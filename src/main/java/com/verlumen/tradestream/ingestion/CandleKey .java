63661abc11ef5f86856f601dab39df1d05979dad
tradestreamhq <tradestream@googlegroups.com>
2024-11-06 23:04:46 -0500

    Create CandleKey .java

---
+++ src/main/java/com/verlumen/tradestream/ingestion/CandleKey .java
@@ -0,0 +1,40 @@
+package com.verlumen.tradestream.ingestion;
+
+import java.util.Objects;
+
+/**
+ * Composite key for identifying unique trades within a candle interval.
+ */
+public final class CandleKey {
+
+    private final String tradeId;
+    private final long minuteTimestamp;
+
+    public CandleKey(String tradeId, long minuteTimestamp) {
+        this.tradeId = tradeId;
+        this.minuteTimestamp = minuteTimestamp;
+    }
+
+    public String getTradeId() {
+        return tradeId;
+    }
+
+    public long getMinuteTimestamp() {
+        return minuteTimestamp;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CandleKey candleKey = (CandleKey) o;
+        return minuteTimestamp == candleKey.minuteTimestamp &&
+                Objects.equals(tradeId, candleKey.tradeId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tradeId, minuteTimestamp);
+    }
+}
