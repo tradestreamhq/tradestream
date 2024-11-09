b5c18efa20275c2603fcc9bd4c8405c91e6e10dd
tradestreamhq <tradestream@googlegroups.com>
2024-11-09 03:13:54 -0500

    Update App.java

--- src/main/java/com/verlumen/tradestream/ingestion/App.java
+++ src/main/java/com/verlumen/tradestream/ingestion/App.java
@@ -4,10 +4,6 @@ import com.google.common.primitives.Ints;
 
 final class App {
 
-  public static int compare(int a, int b) {
-    return Ints.compare(a, b);
-  }
-
   public static void main(String... args) throws Exception {
     App app = new App();
     System.out.println("Starting real-time data ingestion...";
