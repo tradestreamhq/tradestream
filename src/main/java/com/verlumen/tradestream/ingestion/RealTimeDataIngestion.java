0c37d196076b1cb4dfe9f05303c26b7f9ce45b6c
tradestreamhq <tradestream@googlegroups.com>
2024-11-09 04:33:48 -0500

    Update RealTimeDataIngestion.java

--- src/main/java/com/verlumen/tradestream/ingestion/RealTimeDataIngestion.java
+++ src/main/java/com/verlumen/tradestream/ingestion/RealTimeDataIngestion.java
@@ -5,6 +5,12 @@ import com.google.inject.Inject;
 final class RealTimeDataIngestion {
       @Inject
       RealTimeDataIngestion() {}
+
+      @Override
+      public void start() {}
+
+      @Override
+      public void shutdown() {}
       
       public static void main(String[] args) throws Exception {
         System.out.println("Starting real-time data ingestion...");
