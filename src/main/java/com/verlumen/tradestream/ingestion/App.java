48e57cc811f2b649b65b3d2e61e68ca7c2c472b3
tradestreamhq <tradestream@googlegroups.com>
2024-11-09 03:25:47 -0500

    Update `App` Class and Remove Deprecated Test Code for TradeStream Ingestion Module (#30)

--- src/main/java/com/verlumen/tradestream/ingestion/App.java
+++ src/main/java/com/verlumen/tradestream/ingestion/App.java
@@ -1,20 +1,10 @@
 package com.example.myproject;
 
-import com.google.common.primitives.Ints;
-
-/**
- * This application compares two numbers, using the Ints.compare
- * method from Guava.
- */
-public class App {
-
-  public static int compare(int a, int b) {
-    return Ints.compare(a, b);
-  }
+final class App {
 
   public static void main(String... args) throws Exception {
     App app = new App();
-    System.out.println("Success: " + app.compare(2, 1));
+    System.out.println("Starting real-time data ingestion...");
   }
 
 }
