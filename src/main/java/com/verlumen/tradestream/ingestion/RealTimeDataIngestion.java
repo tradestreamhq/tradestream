277a067c23ab841abdb7adcec0fb656b70a9619c
tradestreamhq <tradestream@googlegroups.com>
2024-11-09 00:43:23 -0500

    Update RealTimeDataIngestion.java

--- src/main/java/com/verlumen/tradestream/ingestion/RealTimeDataIngestion.java
+++ src/main/java/com/verlumen/tradestream/ingestion/RealTimeDataIngestion.java
@@ -1,3 +1,7 @@
 package com.verlumen.tradestream.ingestion;
 
-final class RealTimeDataIngestion {}
+final class RealTimeDataIngestion {
+      public static void main(String[] args) throws Exception {
+        System.out.println("Starting real-time data ingestion...");
+    }
+}
