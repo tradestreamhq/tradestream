735ec6c5ca08824dddfca70fc7c504f055bb04e6
tradestreamhq <tradestream@googlegroups.com>
2024-11-13 21:46:14 -0500

    Update RealTimeDataIngestion.java

--- src/main/java/com/verlumen/tradestream/ingestion/RealTimeDataIngestion.java
+++ src/main/java/com/verlumen/tradestream/ingestion/RealTimeDataIngestion.java
@@ -3,16 +3,16 @@ package com.verlumen.tradestream.ingestion;
 import com.google.inject.Inject;
 
 final class RealTimeDataIngestion implements MarketDataIngestion {
-      @Inject
-      RealTimeDataIngestion() {}
+    @Inject
+    RealTimeDataIngestion() {}
 
-      @Override
-      public void start() {}
-
-      @Override
-      public void shutdown() {}
+    @Override
+    public void start() {}
+      
+    @Override
+    public void shutdown() {}
       
       public static void main(String[] args) throws Exception {
         System.out.println("Starting real-time data ingestion...");
-    }
+      }
 }
