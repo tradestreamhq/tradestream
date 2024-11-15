041773e41e061622e3702d7ffeb2214aa3dabae3
tradestreamhq <tradestream@googlegroups.com>
2024-11-15 18:56:47 -0500

    Update CandleKey.java

--- src/main/java/com/verlumen/tradestream/ingestion/CandleKey.java
+++ src/main/java/com/verlumen/tradestream/ingestion/CandleKey.java
@@ -1,37 +1,13 @@
 package com.verlumen.tradestream.ingestion;
 
-import java.util.Objects;
+import com.google.auto.value.AutoValue;
 
-final class CandleKey {
-
-    private final String tradeId;
-    private final long minuteTimestamp;
-
-    CandleKey(String tradeId, long minuteTimestamp) {
-        this.tradeId = tradeId;
-        this.minuteTimestamp = minuteTimestamp;
+@AutoValue
+abstract class CandleKey {
+    static CandleKey create(String tradeId, long minuteTimestamp) {
+        return new AutoValue_CandleKey(tradeId, minuteTimestamp);
     }
-
-    public String getTradeId() {
-        return tradeId;
-    }
-
-    public long getMinuteTimestamp() {
-        return minuteTimestamp;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        CandleKey candleKey = (CandleKey) o;
-        return minuteTimestamp == candleKey.minuteTimestamp &&
-                Objects.equals(tradeId, candleKey.tradeId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(tradeId, minuteTimestamp);
-    }
+    
+    abstract String tradeId();
+    abstract long minuteTimestamp();
 }
