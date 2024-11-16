ae265ae60791edb710d20af2944662122a02a299
tradestreamhq <tradestream@googlegroups.com>
2024-11-15 19:06:15 -0500

    Update CandlePublisher.java

--- src/main/java/com/verlumen/tradestream/ingestion/CandlePublisher.java
+++ src/main/java/com/verlumen/tradestream/ingestion/CandlePublisher.java
@@ -1,8 +1,7 @@
 package com.verlumen.tradestream.ingestion;
 
 import marketdata.Marketdata.Candle;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
+
 import java.time.Duration;
 
 interface CandlePublisher {
