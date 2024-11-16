c6809ab6dd97b4951cce18ba6c84c9298428ce1f
tradestreamhq <tradestream@googlegroups.com>
2024-11-16 16:12:40 -0500

    Update IngestionModule.java

--- src/main/java/com/verlumen/tradestream/ingestion/IngestionModule.java
+++ src/main/java/com/verlumen/tradestream/ingestion/IngestionModule.java
@@ -1,10 +1,15 @@
 package com.verlumen.tradestream.ingestion;
 
+import com.google.inject.assistedinject.FactoryModuleBuilder;
 import com.google.inject.AbstractModule;
 
 final class IngestionModule extends AbstractModule {
   @Override
   protected void configure() {
     bind(MarketDataIngestion.class).to(RealTimeDataIngestion.class);
+
+    install(new FactoryModuleBuilder()
+     .implement(CandlePublisher.class, CandlePublisherImpl.class)
+     .build(CandlePublisher.Factory.class));
   }
 }
