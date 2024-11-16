dc0b3a36c56edaa93b8e5363d9437903c425fa7b
tradestreamhq <tradestream@googlegroups.com>
2024-11-15 19:00:23 -0500

    Update CandlePublisher.java

--- src/main/java/com/verlumen/tradestream/ingestion/CandlePublisher.java
+++ src/main/java/com/verlumen/tradestream/ingestion/CandlePublisher.java
@@ -6,9 +6,9 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import java.time.Duration;
 
 interface CandlePublisher {
-    void publishCandle(Candle candle) {}
+    void publishCandle(Candle candle);
 
-    void close() {}
+    void close();
 
     interface Factory {
         CandlePublisher create(String topic);
