67cd9b423a2085f4bebbd31cd3ff7da066cd3a03
tradestreamhq <tradestream@googlegroups.com>
2024-11-16 18:24:49 -0500

    Update CandleManager.java

--- src/main/java/com/verlumen/tradestream/ingestion/CandleManager.java
+++ src/main/java/com/verlumen/tradestream/ingestion/CandleManager.java
@@ -1,10 +1,7 @@
 package com.verlumen.tradestream.ingestion;
 
 import marketdata.Marketdata.Trade;
-import marketdata.Marketdata.Candle;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 interface CandleManager {
     void processTrade(Trade trade);
