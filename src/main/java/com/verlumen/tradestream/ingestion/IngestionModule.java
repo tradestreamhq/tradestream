6f4028c2de76d289dc862c3504277e4d2a78af7b
tradestreamhq <tradestream@googlegroups.com>
2024-11-16 18:20:15 -0500

    Update IngestionModule.java

--- src/main/java/com/verlumen/tradestream/ingestion/IngestionModule.java
+++ src/main/java/com/verlumen/tradestream/ingestion/IngestionModule.java
@@ -2,14 +2,17 @@ package com.verlumen.tradestream.ingestion;
 
 import com.google.inject.assistedinject.FactoryModuleBuilder;
 import com.google.inject.AbstractModule;
+import com.google.inject.TypeLiteral;
 
 final class IngestionModule extends AbstractModule {
   @Override
   protected void configure() {
+    bind(new TypeLiteral<KafkaProducer<String, byte[]>>() {}).toProvider();
+    bind(KafkaProducer.class).toProvider(KafkaProducerProvider.class);
     bind(MarketDataIngestion.class).to(RealTimeDataIngestion.class);
 
-    // install(new FactoryModuleBuilder()
-    //  .implement(CandlePublisher.class, CandlePublisherImpl.class)
-    //  .build(CandlePublisher.Factory.class));
+    install(new FactoryModuleBuilder()
+     .implement(CandlePublisher.class, CandlePublisherImpl.class)
+     .build(CandlePublisher.Factory.class));
   }
 }
