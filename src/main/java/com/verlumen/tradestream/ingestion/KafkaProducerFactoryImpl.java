27ce343e49899be43699bc90d541c789bece8336
tradestreamhq <tradestream@googlegroups.com>
2024-11-16 18:12:42 -0500

    Create KafkaProducerFactoryImpl.java

---
+++ src/main/java/com/verlumen/tradestream/ingestion/KafkaProducerFactoryImpl.java
@@ -0,0 +1,22 @@
+import java.util.Properties;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+
+class KafkaProducerFactoryImpl {
+  @Inject
+  KafkaProducerFactoryImpl() {}
+
+  public static Producer<String, String> createProducer() {
+      Properties props = new Properties();
+      props.put("bootstrap.servers", "localhost:9092");
+      props.put("acks", "all");
+      props.put("retries", 0);
+      props.put("batch.size", 16384);
+      props.put("linger.ms", 1);
+      props.put("buffer.memory", 33554432);
+      props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+      props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+      return new KafkaProducer<>(props);
+  }
+}
