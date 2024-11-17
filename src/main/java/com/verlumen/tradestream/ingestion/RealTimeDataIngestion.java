46e37beb8232c3acd6f4ef141714056381b65b13
tradestreamhq <tradestream@googlegroups.com>
2024-11-17 17:48:47 -0500

    Integrate StreamingExchange Support for Real-Time Market Data Ingestion (#146)

--- src/main/java/com/verlumen/tradestream/ingestion/RealTimeDataIngestion.java
+++ src/main/java/com/verlumen/tradestream/ingestion/RealTimeDataIngestion.java
@@ -1,18 +1,24 @@
 package com.verlumen.tradestream.ingestion;
 
 import com.google.inject.Inject;
+import com.google.inject.Provider;
+import info.bitrich.xchangestream.core.StreamingExchange;
+import info.bitrich.xchangestream.core.StreamingMarketDataService;
 
 final class RealTimeDataIngestion implements MarketDataIngestion {
     private final CandleManager.Factory candleManagerFactory;
     private final CandlePublisher.Factory candlePublisherFactory;
+    private final Provider<StreamingExchange> exchange;
     
     @Inject
     RealTimeDataIngestion(
         CandleManager.Factory candleManagerFactory,
-        CandlePublisher.Factory candlePublisherFactory
+        CandlePublisher.Factory candlePublisherFactory,
+        Provider<StreamingExchange> exchange
     ) {
         this.candleManagerFactory = candleManagerFactory;
         this.candlePublisherFactory = candlePublisherFactory;
+        this.exchange = exchange;
     }
 
     @Override
