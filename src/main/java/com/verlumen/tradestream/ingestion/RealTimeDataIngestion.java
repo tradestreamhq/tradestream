8611a0ebb1b2641df9e72f9285a881fad9fed7c2
tradestreamhq <tradestream@googlegroups.com>
2024-11-16 20:44:40 -0500

    Update RealTimeDataIngestion.java

--- src/main/java/com/verlumen/tradestream/ingestion/RealTimeDataIngestion.java
+++ src/main/java/com/verlumen/tradestream/ingestion/RealTimeDataIngestion.java
@@ -3,10 +3,15 @@ package com.verlumen.tradestream.ingestion;
 import com.google.inject.Inject;
 
 final class RealTimeDataIngestion implements MarketDataIngestion {
+    private final CandleManager.Factory candleManagerFactory;
     private final CandlePublisher.Factory candlePublisherFactory;
     
     @Inject
-    RealTimeDataIngestion(CandlePublisher.Factory candlePublisherFactory) {
+    RealTimeDataIngestion(
+        CandleManager.Factory candleManagerFactory,
+        CandlePublisher.Factory candlePublisherFactory,
+    ) {
+        this.candleManagerFactory = candleManagerFactory;
         this.candlePublisherFactory = candlePublisherFactory;
     }
 
