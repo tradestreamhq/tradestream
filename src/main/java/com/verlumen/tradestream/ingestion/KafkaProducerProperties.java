2ce9a30895ad90a19a451bfa4d0ea53984fcfb52
tradestreamhq <tradestream@googlegroups.com>
2024-11-17 15:40:38 -0500

    Update KafkaProducerProperties.java

--- src/main/java/com/verlumen/tradestream/ingestion/KafkaProducerProperties.java
+++ src/main/java/com/verlumen/tradestream/ingestion/KafkaProducerProperties.java
@@ -3,6 +3,8 @@ package com.verlumen.tradestream.ingestion;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 
+import java.util.Properties;
+
 final class KafkaProducerPropertiesProvider impements Provider<Properties> {
   private final Properties properties;
 
