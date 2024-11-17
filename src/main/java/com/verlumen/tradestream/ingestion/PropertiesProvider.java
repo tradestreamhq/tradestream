8267a05423880d7a8c60e142c19093cc07903433
tradestreamhq <tradestream@googlegroups.com>
2024-11-17 12:34:26 -0500

    Update PropertiesProvider.java

--- src/main/java/com/verlumen/tradestream/ingestion/PropertiesProvider.java
+++ src/main/java/com/verlumen/tradestream/ingestion/PropertiesProvider.java
@@ -7,28 +7,30 @@ import java.io.IOException;
 import java.util.Properties;
 
 final class PropertiesProvider implements Provider<Properties> {
-  private static final String CONFIG_FILE_PATH = "/src/main/java/com/verlumen/tradestream/ingestion/config.properties";
+  private static final String CONFIG_FILE_PATH = "src/main/java/com/verlumen/tradestream/ingestion/config.properties";
   private static final String WORKSPACE_NAME = "tradestream";
 
   @Inject
   PropertiesProvider() {}
-  
-  @Override  
+
+  @Override
   public Properties get() {
     try {
       // Step 1: Preload Runfiles
       Runfiles.Preloaded runfilesPreloaded = Runfiles.preload();
 
+      // Resolve the path to the properties file in the runfiles directory
       String propertiesPath = runfilesPreloaded.unmapped()
-        .rlocation(WORKSPACE_NAME + CONFIG_FILE_PATH);
+        .rlocation(WORKSPACE_NAME + "/" + CONFIG_FILE_PATH);
 
       // Step 2: Load properties from the file
       Properties props = new Properties();
       try (FileInputStream inputStream = new FileInputStream(propertiesPath)) {
         props.load(inputStream);
       }
-        return props;
-      } catch (IOException e) {
-        throw new RuntimeException(e);
+      return props;
+    } catch (IOException e) {
+      throw new RuntimeException("Failed to load properties file", e);
     }
+  }
 }
