f8600cb152351e47c3862985bb9e05f41612171e
tradestreamhq <tradestream@googlegroups.com>
2024-11-17 11:00:34 -0500

    Update App.java

--- src/main/java/com/verlumen/tradestream/ingestion/App.java
+++ src/main/java/com/verlumen/tradestream/ingestion/App.java
@@ -1,8 +1,15 @@
 package com.verlumen.tradestream.ingestion;
 
+import com.google.common.flogger.backend.system.DefaultPlatform;
 import com.google.common.flogger.FluentLogger;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
+
+static {
+  System.setProperty(
+      "flogger.backend_factory",
+      "com.google.common.flogger.backend.system.DefaultPlatform$SystemBackendFactory");
+}
 
 final class App {
   private static final FluentLogger logger = FluentLogger.forEnclosingClass();
