dc54e010cadda79b2b0779b68a582222439d9817
tradestreamhq <tradestream@googlegroups.com>
2024-11-20 22:40:24 -0500

    Create ThinMarketTimerImpl.java

---
+++ src/main/java/com/verlumen/tradestream/ingestion/ThinMarketTimerImpl.java
@@ -0,0 +1,30 @@
+package com.verlumen.tradestream.ingestion;
+
+final class ThinMarketTimerImpl implements ThinMarketTimer {
+  private final Timer timer;
+  private final TimerTask timerTask;
+
+  @Inject
+  ThinMarketTimerImpl(CandleManager candleManager, CurrencyPairSupplier currencyPairSupplier) {
+    this.timer = new Timer();
+    this.timerTask = ThinMarketTimerTask.create(candleManager, currencyPairSupplier);
+  }
+   
+  void start() {
+      timer().scheduleAtFixedRate(timerTask(), 0, ONE_MINUTE_IN_MILLISECONDS);            
+  }
+}
+
+abstract static class ThinMarketTimerTask extends TimerTask {
+    private static ThinMarketTimerTask create(CandleManager candleManager, CurrencyPairSupplier currencyPairSupplier) {
+        return new AutoValue_RealTimeDataIngestion_ThinMarketTimerTask(candleManager, currencyPairSupplier);
+    }
+
+    abstract CandleManager candleManager();
+    abstract CurrencyPairSuppier currencyPairSupplier();
+
+    @Override
+    public void run() {
+        candleManager().handleThinlyTradedMarkets(currencyPairSupplier().currencyPairs());
+    }
+}
