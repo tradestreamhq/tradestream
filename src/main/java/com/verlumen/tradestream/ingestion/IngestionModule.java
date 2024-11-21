09cef9ad46a79865f19f996f9f85863af040df71
tradestreamhq <tradestream@googlegroups.com>
2024-11-20 19:00:59 -0500

    Update IngestionModule.java

--- src/main/java/com/verlumen/tradestream/ingestion/IngestionModule.java
+++ src/main/java/com/verlumen/tradestream/ingestion/IngestionModule.java
@@ -4,6 +4,7 @@ import com.google.auto.value.AutoValue;
 import com.google.common.collect.ImmutableList;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
 import com.google.inject.TypeLiteral;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import info.bitrich.xchangestream.core.StreamingExchange;
@@ -35,5 +36,11 @@ abstract class IngestionModule extends AbstractModule {
     install(new FactoryModuleBuilder()
         .implement(CandlePublisher.class, CandlePublisherImpl.class)
         .build(CandlePublisher.Factory.class));
+  }
+
+  @Provides
+  TradeProcessor provideTradeProcessor(Namespace namespace) {
+    long candleIntervalMillis = namespace.getInt("candleIntervalSeconds") * 1000;
+    return TradeProcessor.create(candleIntervalMillis);
   }
 }
