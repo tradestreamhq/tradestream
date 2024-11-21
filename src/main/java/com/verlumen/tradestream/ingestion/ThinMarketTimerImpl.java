35df923038719f0d92fa2214b0379be7a61706fb
tradestreamhq <tradestream@googlegroups.com>
2024-11-21 01:40:55 -0500

    Create ThinMarketTimerImpl.java

---
+++ src/main/java/com/verlumen/tradestream/ingestion/ThinMarketTimerImpl.java
@@ -0,0 +1,29 @@
+package com.verlumen.tradestream.ingestion;
+
+import com.google.inject.Inject;
+import org.knowm.xchange.currency.CurrencyPair;
+
+import java.util.Timer;
+
+final class ThinMarketTimerImpl implements ThinMarketTimer {
+  private static final int ONE_MINUTE_IN_MILLISECONDS = 60_000;
+
+  private final ThinMarketTimerTask timerTask;
+  private final Timer timer;
+
+  @Inject
+  ThinMarketTimerImpl(ThinMarketTimerTask timerTask, Timer timer) {
+    this.timer = timer;
+    this.timerTask = timerTask;
+  }
+
+  @Override
+  public void start() {
+    timer.scheduleAtFixedRate(task, 0, ONE_MINUTE_IN_MILLISECONDS);            
+  }
+
+  @Override
+  public void stop() {
+    timer.cancel();
+  }
+}
