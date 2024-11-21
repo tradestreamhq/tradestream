0c4ca384e4ba205b198130505e5f4ec1d7226d23
tradestreamhq <tradestream@googlegroups.com>
2024-11-21 01:38:52 -0500

    Update ThinMarketTimerTask.java

--- src/main/java/com/verlumen/tradestream/ingestion/ThinMarketTimerTask.java
+++ src/main/java/com/verlumen/tradestream/ingestion/ThinMarketTimerTask.java
@@ -7,11 +7,4 @@ import java.util.TimerTask;
  * This can be used in conjunction with the ThinMarketTimer to perform specific actions,
  * such as forcing the generation of candles when there is little to no trading activity.
  */
-abstract class ThinMarketTimerTask extends TimerTask {
-
-  /**
-   * Executes the task associated with thin market conditions.
-   * This method is invoked by the ThinMarketTimer when a predefined interval elapses without significant market activity.
-   */
-  void execute();
-}
+abstract class ThinMarketTimerTask extends TimerTask {}
