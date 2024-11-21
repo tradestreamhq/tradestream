7dc66667d73899386ce525a63498e0361e276905
tradestreamhq <tradestream@googlegroups.com>
2024-11-20 21:11:01 -0500

    Update CurrencyPairSupplier.java

--- src/main/java/com/verlumen/tradestream/ingestion/CurrencyPairSupplier.java
+++ src/main/java/com/verlumen/tradestream/ingestion/CurrencyPairSupplier.java
@@ -1,5 +1,7 @@
 package com.verlumen.tradestream.ingestion;
 
+import com.google.common.collect.ImmutableSet;
+
 import java.util.function.Supplier;
 
 interface CurrencyPairSupplier extends Supplier<ImmutableSet<CurrencyPairMetadata>> {}
