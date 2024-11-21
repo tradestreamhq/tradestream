f6a9b7fc211c1545e8afda8806e79b3423d30462
tradestreamhq <tradestream@googlegroups.com>
2024-11-21 01:16:22 -0500

    Update ThinMarketTimerImpl.java

--- src/main/java/com/verlumen/tradestream/ingestion/ThinMarketTimerImpl.java
+++ src/main/java/com/verlumen/tradestream/ingestion/ThinMarketTimerImpl.java
@@ -6,18 +6,18 @@ import org.knowm.xchange.currency.CurrencyPair;
 final class ThinMarketTimerImpl implements ThinMarketTimer {
   private static final int ONE_MINUTE_IN_MILLISECONDS = 60_000;
 
+  private final ThinMarketTimerTask task;
   private final Timer timer;
-  private final ThinMarketTimerTask timerTask;
 
   @Inject
-  ThinMarketTimerImpl(CandleManager candleManager, ThinMarketTimerTask timerTask) {
+  ThinMarketTimerImpl(ThinMarketTimerTask timerTask) {
+    this.task = thinMarketTimerTask;
     this.timer = new Timer();
-    this.timerTask = thinMarketTimerTask;
   }
 
   @Override
   public void start() {
-    timer.scheduleAtFixedRate(timerTask, 0, ONE_MINUTE_IN_MILLISECONDS);            
+    timer.scheduleAtFixedRate(task, 0, ONE_MINUTE_IN_MILLISECONDS);            
   }
 
   @Override
