3c37d0b86891fd92721b2a13c9435a9b37324828
tradestreamhq <tradestream@googlegroups.com>
2024-11-20 20:00:58 -0500

    Update RealTimeDataIngestion.java

--- src/main/java/com/verlumen/tradestream/ingestion/RealTimeDataIngestion.java
+++ src/main/java/com/verlumen/tradestream/ingestion/RealTimeDataIngestion.java
@@ -6,20 +6,20 @@ import info.bitrich.xchangestream.core.StreamingExchange;
 import info.bitrich.xchangestream.core.StreamingMarketDataService;
 
 final class RealTimeDataIngestion implements MarketDataIngestion {
-    private final CandleManager.Factory candleManagerFactory;
-    private final CandlePublisher.Factory candlePublisherFactory;
+    private final CandleManager candleManager;
+    private final CandlePublisher candlePublisher;
     private final Provider<StreamingExchange> exchange;
     private final TradeProcessor tradeProcessor;
     
     @Inject
     RealTimeDataIngestion(
-        CandleManager.Factory candleManagerFactory,
-        CandlePublisher.Factory candlePublisherFactory,
+        CandleManager.Factory candleManager,
+        CandlePublisher.Factory candlePublisher,
         Provider<StreamingExchange> exchange,
         TradeProcessor tradeProcessor
     ) {
-        this.candleManagerFactory = candleManagerFactory;
-        this.candlePublisherFactory = candlePublisherFactory;
+        this.candleManager = candleManager;
+        this.candlePublisher = candlePublisher;
         this.exchange = exchange;
         this.tradeProcessor = tradeProcessor;
     }
