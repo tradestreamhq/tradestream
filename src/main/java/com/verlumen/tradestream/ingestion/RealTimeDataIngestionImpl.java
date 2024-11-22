aa497a7c5d378355edb1c1a7a575291f8f8c3aad
tradestreamhq <tradestream@googlegroups.com>
2024-11-21 19:23:25 -0500

    Update RealTimeDataIngestionImpl.java

--- src/main/java/com/verlumen/tradestream/ingestion/RealTimeDataIngestionImpl.java
+++ src/main/java/com/verlumen/tradestream/ingestion/RealTimeDataIngestionImpl.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-final class RealTimeDataIngestionImpl implements MarketDataIngestion {
+final class RealTimeDataIngestionImpl implements RealTimeDataIngestion {
     private final CandleManager candleManager;
     private final CandlePublisher candlePublisher;
     private final CurrencyPairSupplier currencyPairSupplier;
