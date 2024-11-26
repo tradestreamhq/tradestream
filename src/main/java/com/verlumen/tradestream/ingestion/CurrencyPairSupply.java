37f442e2df05202396af3b7e0a491fee74532d45
tradestreamhq <tradestream@googlegroups.com>
2024-11-26 15:21:31 -0500

    Update CurrencyPairSupply.java

--- src/main/java/com/verlumen/tradestream/ingestion/CurrencyPairSupply.java
+++ src/main/java/com/verlumen/tradestream/ingestion/CurrencyPairSupply.java
@@ -2,9 +2,11 @@ package com.verlumen.tradestream.ingestion;
 
 import static com.google.common.collect.ImmutableList.toImmutableList;
 
+import com.google.auto.value.AutoValue;
 import com.google.common.collect.ImmutableList;
 import org.knowm.xchange.currency.CurrencyPair;
 
+@AutoValue
 abstract CurrencyPairSupply {
   static CurrencyPairSupply create(ImmutableList<CurrencyPairMetadata> metadataList) {
     return AutoValue_CurrencyPairSupply(metadataList);
