18df1ceb27fea5419f3caa319bbba71e4290ce1c
tradestreamhq <tradestream@googlegroups.com>
2024-11-26 20:01:19 -0500

    Refactor CurrencyPairSupply to an Interface with Implementation (#203)

--- src/main/java/com/verlumen/tradestream/ingestion/CurrencyPairSupply.java
+++ src/main/java/com/verlumen/tradestream/ingestion/CurrencyPairSupply.java
@@ -2,19 +2,13 @@ package com.verlumen.tradestream.ingestion;
 
 import static com.google.common.collect.ImmutableList.toImmutableList;
 
-import com.google.auto.value.AutoValue;
 import com.google.common.collect.ImmutableList;
 import org.knowm.xchange.currency.CurrencyPair;
 
-@AutoValue
-abstract class CurrencyPairSupply {
-  static CurrencyPairSupply create(ImmutableList<CurrencyPairMetadata> metadataList) {
-    return new AutoValue_CurrencyPairSupply(metadataList);
-  }
-
-  abstract ImmutableList<CurrencyPairMetadata> metadataList();
+interface CurrencyPairSupply {
+  ImmutableList<CurrencyPairMetadata> metadataList();
   
-  ImmutableList<CurrencyPair> currencyPairs() {
+  default ImmutableList<CurrencyPair> currencyPairs() {
     return metadataList()
       .stream()
       .map(CurrencyPairMetadata::currencyPair)
