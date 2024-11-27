abd67275579068734d1e48e390265745592ea889
tradestreamhq <tradestream@googlegroups.com>
2024-11-26 21:37:50 -0500

    Create HttpClientImpl.java

---
+++ src/main/java/com/verlumen/tradestream/ingestion/HttpClientImpl.java
@@ -0,0 +1,38 @@
+package com.verlumen.tradestream.ingestion;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Map;
+
+final class HttpClientImpl implements HttpClient {
+    @Override
+    public String get(String url, Map<String, String> headers) throws IOException {
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        for (Map.Entry<String, String> header : headers.entrySet()) {
+            con.setRequestProperty(header.getKey(), header.getValue());
+        }
+
+        int responseCode = con.getResponseCode();
+        if (responseCode != HttpURLConnection.HTTP_OK) {
+            throw new IOException(
+                "Failed to fetch data: HTTP code " + responseCode);
+        }
+
+        BufferedReader in =
+            new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder responseStrBuilder = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null) {
+            responseStrBuilder.append(inputLine);
+        }
+        in.close();
+
+        return responseStrBuilder.toString();
+    }
+}
