c742192de852f4629a72f7234de45f2bee13c82e
tradestreamhq <tradestream@googlegroups.com>
2024-11-27 10:41:30 -0500

    Update HttpURLConnectionFactory.java

--- src/main/java/com/verlumen/tradestream/ingestion/HttpURLConnectionFactory.java
+++ src/main/java/com/verlumen/tradestream/ingestion/HttpURLConnectionFactory.java
@@ -1,14 +1,8 @@
 package com.verlumen.tradestream.ingestion;
 
-import com.google.inject.Inject;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Map;
 
 interface HttpURLConnectionFactory {
-  String create(String url) throws IOException;
+  HttpURLConnection create(String url) throws IOException;
 }
