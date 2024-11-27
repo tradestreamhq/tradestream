95c722033a661a4fb287d67ce38abb616157fdc1
tradestreamhq <tradestream@googlegroups.com>
2024-11-26 20:48:02 -0500

    Update ThinMarketTimerImplTest.java

--- src/test/java/com/verlumen/tradestream/ingestion/ThinMarketTimerImplTest.java
+++ src/test/java/com/verlumen/tradestream/ingestion/ThinMarketTimerImplTest.java
@@ -26,7 +26,7 @@ public class ThinMarketTimerImplTest {
     @Rule public MockitoRule mocks = MockitoJUnit.rule();
 
     @Mock @Bind private CandleManager candleManager;
-    @Mock @Bind private CurrencyPairSupplier currencyPairSupplier;
+    @Mock @Bind private CurrencyPairSupply currencyPairSupply;
     @Mock @Bind private Timer timer;
     @Mock @Bind private ThinMarketTimerTask task;
     @Inject private ThinMarketTimerImpl thinMarketTimer;
