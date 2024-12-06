2281f22796895e9b896a52a43ed8e269ddb0ffd6
tradestreamhq <tradestream@googlegroups.com>
2024-12-05 22:53:26 -0500

    Update IngestionModule.java

--- src/main/java/com/verlumen/tradestream/ingestion/IngestionModule.java
+++ src/main/java/com/verlumen/tradestream/ingestion/IngestionModule.java
@@ -5,6 +5,7 @@ import com.google.common.collect.ImmutableList;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
+import com.google.inject.Singleton;
 import com.google.inject.TypeLiteral;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import info.bitrich.xchangestream.core.ProductSubscription;
