5f3298817e01204a8ca16eeb23dad74e4cc75ff8
tradestreamhq <tradestream@googlegroups.com>
2024-12-07 04:43:20 -0500

    Create ExchangeStreamingClientFactory.java

---
+++ src/main/java/com/verlumen/tradestream/ingestion/ExchangeStreamingClientFactory.java
@@ -0,0 +1,27 @@
+package com.verlumen.tradestream.ingestion;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Strings.isNullOrEmpty;
+
+import com.google.common.collect.ImmutableList;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.verlumen.tradestream.marketdata.Trade;
+
+import java.util.function.Consumer;
+
+final class ExchangeStreamingClientFactory implements ExchangeStreamingClient.Factory {
+  private final ImmutableMap<String, Provider<ExchangeStreamingClient>> clientMap;
+
+  @Inject
+  ExchangeStreamingClient(Provider<CoinbaseStreamingClient> coinbaseStreamingClient) {
+    this.clientMap = ImmutableMap.builder()
+      .put("coinbase", coinbaseStreamingClient)
+      .buildOrThrow();
+  }
+    
+  ExchangeStreamingClient create(String exchangeName) {
+    checkArgument(clientMap.contains(exchangeName));
+    return clientMap.get(exchangeName);
+  }
+}
