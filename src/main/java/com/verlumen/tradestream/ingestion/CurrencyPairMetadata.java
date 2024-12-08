fff4ef5be43b7a5721cc282d3306ad5b8b4c4ab8
tradestreamhq <tradestream@googlegroups.com>
2024-12-08 16:02:06 -0500

    Update CurrencyPairMetadata.java

--- src/main/java/com/verlumen/tradestream/ingestion/CurrencyPairMetadata.java
+++ src/main/java/com/verlumen/tradestream/ingestion/CurrencyPairMetadata.java
@@ -1,29 +1,59 @@
 package com.verlumen.tradestream.ingestion;
 
+import static com.google.common.collect.MoreCollectors.onlyElement;
+
 import com.google.auto.value.AutoValue;
-import org.knowm.xchange.currency.Currency;
-import org.knowm.xchange.currency.CurrencyPair;
+import com.google.common.base.Splitter;
 
 import java.math.BigDecimal;
+import java.util.Stream;
 
 @AutoValue
 abstract class CurrencyPairMetadata {
-  static CurrencyPairMetadata create(String pair, BigDecimal marketCapValue) {
-    return create(new CurrencyPair(pair), marketCapValue);
+  private static final String FORWARD_SLASH = "/";
+  private static final String HYPHEN = "-";
+
+  static CurrencyPairMetadata create(String symbol, BigDecimal marketCapValue) {
+    String delimitter = Stream.of(FORWARD_SLASH, HYPHEN)
+      .filter(symbol::contains)
+      .collect(onlyElement());
+    Splitter splitter = Splitter.on(delimiter)
+      .trimResults()
+      .omitEmptyStrings();
+    Currency base = Currency.create(symbol.split(delimiter)[0]);
+    Currency counter = Currency.create(symbol.split(delimiter)[1]);
+    CurrencyPair currencyPair = CurrencyPair.create(base, counter);
+    MarketCap marketCap = MarketCap.create(marketCapValue, counter);
+    return create(currencyPair, marketCap);
   }
 
-  static CurrencyPairMetadata create(CurrencyPair currencyPair, long marketCapValue) {
-    return create(currencyPair, BigDecimal.valueOf(marketCapValue));
-  }
-
-  static CurrencyPairMetadata create(CurrencyPair currencyPair, BigDecimal marketCapValue) {
-    MarketCap marketCap = MarketCap.create(marketCapValue, Currency.USD);
-    return new AutoValue_CurrencyPairMetadata(currencyPair, marketCap);
-  }
+  private static CurrencyPairMetadata create(CurrencyPair currencyPair, MarketCap marketCap) {
+    return new AutoValue_CurrencyPairMetadata(base, counter, marketCap);
+  }  
 
   abstract CurrencyPair currencyPair();
 
   abstract MarketCap marketCap();
+
+  @AutoValue
+  abstract static class Currency {
+    private static Currency create(String name) {
+      return new AutoValue_CurrencyPairMetadata_Currency(name);
+    }
+
+    abstract String name();
+  }
+
+  @AutoValue
+  abstract static class CurrencyPair {
+    private static Currency create(Currency base, Currency counter) {
+      return new AutoValue_CurrencyPairMetadata_CurrencyPair(base, counter);
+    }
+
+    abstract Currency base();
+
+    abstract Currency counter();
+  }
 
   @AutoValue
   abstract static class MarketCap {
