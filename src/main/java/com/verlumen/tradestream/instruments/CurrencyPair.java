317857a251852cc0e38bafb0e663434cc2b237f7
tradestreamhq <tradestream@googlegroups.com>
2024-12-08 16:40:41 -0500

    Update CurrencyPair.java

--- src/main/java/com/verlumen/tradestream/instruments/CurrencyPair.java
+++ src/main/java/com/verlumen/tradestream/instruments/CurrencyPair.java
@@ -6,6 +6,18 @@ import java.util.Currency;
 
 @AutoValue
 abstract static class CurrencyPair {
+  public static CurrencyPair fromSymbol(String symbol) {
+    String delimitter = Stream.of(FORWARD_SLASH, HYPHEN)
+      .filter(symbol::contains)
+      .collect(onlyElement());
+    Splitter splitter = Splitter.on(delimiter)
+      .trimResults()
+      .omitEmptyStrings();
+    Currency base = Currency.create(symbol.split(delimiter)[0]);
+    Currency counter = Currency.create(symbol.split(delimiter)[1]);
+    return create(base, counter);
+  }
+
   private static Currency create(Currency base, Currency counter) {
     return new AutoValue_CurrencyPair(base, counter);
   }
