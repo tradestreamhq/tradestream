9aaff017a381f33d980b2dce14dac76739450d37
tradestreamhq <tradestream@googlegroups.com>
2024-12-08 18:16:38 -0500

    Update CurrencyPairSupply.java

--- src/main/java/com/verlumen/tradestream/ingestion/CurrencyPairSupply.java
+++ src/main/java/com/verlumen/tradestream/ingestion/CurrencyPairSupply.java
@@ -1,7 +1,17 @@
 package com.verlumen.tradestream.ingestion;
 
+import static com.google.common.collect.ImmutableList.toImmutableList;
+
 import com.google.common.collect.ImmutableList;
+import com.verlumen.tradestream.instruments.CurrencyPair;
 
 interface CurrencyPairSupply {
   ImmutableList<CurrencyPairMetadata> metadataList();
+
+  default ImmutableList<CurrencyPair> currencyPairs() {
+    return metadataList()
+      .stream()
+      .map(CurrencyPairMetadata::currencyPair)
+      .collect(toImmutableList());
+  }
 }
