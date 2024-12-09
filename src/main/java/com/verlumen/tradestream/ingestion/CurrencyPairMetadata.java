19b14b8a33f60e54710bd264a4f531ade0a752e5
tradestreamhq <tradestream@googlegroups.com>
2024-12-08 22:03:05 -0500

    Update CurrencyPairMetadata.java

--- src/main/java/com/verlumen/tradestream/ingestion/CurrencyPairMetadata.java
+++ src/main/java/com/verlumen/tradestream/ingestion/CurrencyPairMetadata.java
@@ -9,7 +9,7 @@ import java.math.BigDecimal;
 @AutoValue
 abstract class CurrencyPairMetadata {
   static CurrencyPairMetadata create(String pair, BigDecimal marketCapValue) {
-    return create(new CurrencyPair(pair), marketCapValue);
+    return create(CurrencyPair.fromSymbol(pair), marketCapValue);
   }
 
   static CurrencyPairMetadata create(CurrencyPair currencyPair, long marketCapValue) {
