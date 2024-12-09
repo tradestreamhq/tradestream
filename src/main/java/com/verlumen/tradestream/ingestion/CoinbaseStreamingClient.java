9202c3c4bb9e1f9b4db6407340967391caed9dcc
tradestreamhq <tradestream@googlegroups.com>
2024-12-09 02:37:03 -0500

    Update CoinbaseStreamingClient.java

--- src/main/java/com/verlumen/tradestream/ingestion/CoinbaseStreamingClient.java
+++ src/main/java/com/verlumen/tradestream/ingestion/CoinbaseStreamingClient.java
@@ -27,6 +27,7 @@ import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.function.Consumer;
+import java.util.stream.StreamSupport;
 
 final class CoinbaseStreamingClient implements ExchangeStreamingClient {
     private static final FluentLogger logger = FluentLogger.forEnclosingClass();
