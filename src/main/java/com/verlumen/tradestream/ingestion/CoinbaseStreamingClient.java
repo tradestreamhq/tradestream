0e4338c552626b2300f5873401fb0672fc519e95
tradestreamhq <tradestream@googlegroups.com>
2024-12-08 20:49:34 -0500

    Update CoinbaseStreamingClient.java

--- src/main/java/com/verlumen/tradestream/ingestion/CoinbaseStreamingClient.java
+++ src/main/java/com/verlumen/tradestream/ingestion/CoinbaseStreamingClient.java
@@ -50,7 +50,7 @@ final class CoinbaseStreamingClient implements ExchangeStreamingClient {
 
         // Convert currency pairs to Coinbase product IDs
         ImmutableList<String> productIds = currencyPairs.stream()
-            .map(pair -> pair.symbolWithCustomDelimeter("-"))
+            .map(pair -> pair.symbolWithCustomDelimiter("-"))
             .collect(toImmutableList());
 
         logger.atInfo().log("Starting Coinbase streaming for %d products: %s", 
