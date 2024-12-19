3d0309565a84dea4d3d49695e8a10d13a9099484
tradestreamhq <tradestream@googlegroups.com>
2024-12-18 23:57:22 -0500

    Update StrategyFactory.java

--- src/main/java/com/verlumen/tradestream/strategies/StrategyFactory.java
+++ src/main/java/com/verlumen/tradestream/strategies/StrategyFactory.java
@@ -2,9 +2,9 @@ package com.verlumen.tradestream.strategies;
 
 import com.google.protobuf.Any;
 import com.google.protobuf.InvalidProtocolBufferException;
+import com.verlumen.tradestream.strategies.StrategyType;
 import org.ta4j.core.BarSeries;
 import org.ta4j.core.Strategy;
-import com.verlumen.tradestream.strategies.StrategyType;
 
 interface StrategyFactory {
   /**
