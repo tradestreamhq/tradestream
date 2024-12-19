30147b2fcd0f63bcdc762e96b9fec1ac3c0e0d8d
tradestreamhq <tradestream@googlegroups.com>
2024-12-18 23:53:52 -0500

    Create StrategyFactory.java

---
+++ src/main/java/com/verlumen/tradestream/strategies/StrategyFactory.java
@@ -0,0 +1,35 @@
+package trading.backtesting;
+
+import com.google.protobuf.Any;
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.ta4j.core.BarSeries;
+import org.ta4j.core.Strategy;
+import trading.StrategyType;
+
+public interface StrategyFactory {
+
+  /**
+   * Creates a Ta4j Strategy object from the provided parameters
+   *
+   * @param strategyParameters the parameters for the strategy
+   * @param series bar series for the strategy
+   * @return Strategy object
+   * @throws InvalidProtocolBufferException If there is an error when unpacking the `Any` type
+   */
+  Strategy createStrategy(Any strategyParameters, BarSeries series)
+      throws InvalidProtocolBufferException;
+
+    /**
+     * Gets the `StrategyType` this factory handles.
+     *
+     * @return The StrategyType this factory handles.
+     */
+  StrategyType getStrategyType();
+
+    /**
+     * Returns the Class that this strategy unpacks the parameters into
+     *
+     * @return the Class that this factory unpacks `Any` into.
+     */
+  Class<?> getParameterClass();
+}
