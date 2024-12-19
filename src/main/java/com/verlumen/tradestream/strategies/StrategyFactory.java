37c987f02e9aa8ebf30c1a70490c95a3e0b6d678
tradestreamhq <tradestream@googlegroups.com>
2024-12-19 00:47:48 -0500

    Update StrategyFactory.java

--- src/main/java/com/verlumen/tradestream/strategies/StrategyFactory.java
+++ src/main/java/com/verlumen/tradestream/strategies/StrategyFactory.java
@@ -2,11 +2,12 @@ package com.verlumen.tradestream.strategies;
 
 import com.google.protobuf.Any;
 import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.Message;
 import com.verlumen.tradestream.strategies.StrategyType;
 import org.ta4j.core.BarSeries;
 import org.ta4j.core.Strategy;
 
-interface StrategyFactory {
+interface StrategyFactory<T extends Message> {
   /**
    * Creates a Ta4j Strategy object from the provided parameters
    *
@@ -24,4 +25,11 @@ interface StrategyFactory {
    * @return The StrategyType this factory handles.
    */
   StrategyType getStrategyType();
+
+    /**
+     * Returns the Class that this strategy unpacks the parameters into
+     *
+     * @return the Class that this factory unpacks `Any` into.
+     */
+    Class<T> getParameterClass();
 }
