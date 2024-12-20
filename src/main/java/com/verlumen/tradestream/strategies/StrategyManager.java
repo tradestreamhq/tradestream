e55e855b83c420e951c3507011674e31d813935f
tradestreamhq <tradestream@googlegroups.com>
2024-12-20 00:51:58 -0500

    Update StrategyManager.java

--- src/main/java/com/verlumen/tradestream/strategies/StrategyManager.java
+++ src/main/java/com/verlumen/tradestream/strategies/StrategyManager.java
@@ -1,23 +1,10 @@
 package com.verlumen.tradestream.strategies;
 
-import com.google.auto.value.AutoValue;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 import com.google.protobuf.Any;
 import com.google.protobuf.InvalidProtocolBufferException;
-import com.google.protobuf.Message;
 import com.verlumen.tradestream.strategies.StrategyType;
 import org.ta4j.core.Strategy;
 
 interface StrategyManager {
   Strategy createStrategy(StrategyType strategyType, Any strategyParameters) throws InvalidProtocolBufferException;
-  
-  @AutoValue
-  abstract class Config {
-    static Config create(ImmutableList<StrategyFactory> factories) {
-      return new AutoValue_Config();
-    }
-    
-    abstract ImmutableMap<StrategyType, StrategyFactory> factoryMap();
-  }
 }
