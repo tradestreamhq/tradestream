bfa87107549e4fa65dcbb20401a6d5bedf1ab56d
tradestreamhq <tradestream@googlegroups.com>
2024-12-20 00:37:49 -0500

    Update StrategyManager.java

--- src/main/java/com/verlumen/tradestream/strategies/StrategyManager.java
+++ src/main/java/com/verlumen/tradestream/strategies/StrategyManager.java
@@ -6,7 +6,6 @@ import com.google.protobuf.Any;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.Message;
 import org.ta4j.core.Strategy;
-import java.util.Map;
 
 interface StrategyManager {
   Strategy createStrategy(StrategyType strategyType, Any strategyParameters) throws InvalidProtocolBufferException;
