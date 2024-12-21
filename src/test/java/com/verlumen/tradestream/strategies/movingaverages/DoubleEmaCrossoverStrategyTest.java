4057f510d52805ec533e9041bc160dec728205c8
tradestreamhq <tradestream@googlegroups.com>
2024-12-21 01:11:21 -0500

    Create DoubleEmaCrossoverStrategyTest.java

---
+++ src/test/java/com/verlumen/tradestream/strategies/movingaverages/DoubleEmaCrossoverStrategyTest.java
@@ -0,0 +1,40 @@
+package com.verlumen.tradestream.strategies.movingaverages;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.verlumen.tradestream.strategies.StrategyType;
+import com.verlumen.tradestream.strategies.DoubleEmaCrossoverParameters;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.ta4j.core.Strategy;
+
+@RunWith(JUnit4.class)
+public class DoubleEmaCrossoverStrategyTest {
+
+  private DoubleEmaCrossoverStrategy.Factory factory;
+
+  @Before
+  public void setUp() {
+    factory = new DoubleEmaCrossoverStrategy.Factory();
+  }
+
+  @Test
+  public void getStrategyType() {
+    assertThat(factory.getStrategyType()).isEqualTo(StrategyType.DOUBLE_EMA_CROSSOVER);
+  }
+
+  @Test
+  public void getParameterClass() {
+      assertThat(factory.getParameterClass()).isEqualTo(DoubleEmaCrossoverParameters.class);
+  }
+
+  @Test
+  public void createStrategy_withParams() throws InvalidProtocolBufferException {
+      DoubleEmaCrossoverParameters params = DoubleEmaCrossoverParameters.newBuilder().setShortEmaPeriod(5).setLongEmaPeriod(20).build();
+    Strategy strategy = factory.createStrategy(params);
+    assertThat(strategy).isNotNull();
+  }
+}
