d17c1c96383e28586713312ee81246c9d9e04bae
tradestreamhq <tradestream@googlegroups.com>
2024-12-21 00:50:55 -0500

    Update StrategyManagerImpl.java

--- src/main/java/com/verlumen/tradestream/strategies/StrategyManagerImpl.java
+++ src/main/java/com/verlumen/tradestream/strategies/StrategyManagerImpl.java
@@ -9,7 +9,6 @@ import com.google.inject.Inject;
 import com.google.mu.util.stream.BiStream;
 import com.google.protobuf.Any;
 import com.google.protobuf.InvalidProtocolBufferException;
-import com.google.protobuf.Message;
 import org.ta4j.core.Strategy;
 
 final class StrategyManagerImpl implements StrategyManager {
@@ -28,24 +27,8 @@ final class StrategyManagerImpl implements StrategyManager {
       throw new IllegalArgumentException("Unsupported strategy type: " + strategyType);
     }
   
-    // The factory returns a parameter class with a known Message subtype.
-    Class<? extends Message> parameterClass = factory.getParameterClass();
-  
-    // Cast the parameter class to Message, since we know it's safe from how we build the map.
-    @SuppressWarnings("unchecked")
-    Class<Message> castedParameterClass = (Class<Message>) parameterClass;
-  
-    // Unpack the parameters using the casted parameter class.
-    Message paramMessage = parameters.unpack(castedParameterClass);
-  
-    // Cast the factory to a StrategyFactory<Message> so createStrategy can accept paramMessage.
-    @SuppressWarnings("unchecked")
-    StrategyFactory<Message> castedFactory = (StrategyFactory<Message>) factory;
-  
-    // Now call createStrategy with the correctly typed message.
-    return castedFactory.createStrategy(paramMessage);
+    return castedFactory.createStrategy(parameters);
   }
-
 
   @AutoValue
   abstract static class Config {
