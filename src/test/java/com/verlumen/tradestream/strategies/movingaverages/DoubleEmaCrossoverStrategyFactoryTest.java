c3510eadd184a9f7d624136679698394d34b262e
tradestreamhq <tradestream@googlegroups.com>
2024-12-21 14:36:45 -0500

    Update DoubleEmaCrossoverStrategyFactoryTest.java

--- src/test/java/com/verlumen/tradestream/strategies/movingaverages/DoubleEmaCrossoverStrategyFactoryTest.java
+++ src/test/java/com/verlumen/tradestream/strategies/movingaverages/DoubleEmaCrossoverStrategyFactoryTest.java
@@ -22,6 +22,8 @@ import org.ta4j.core.indicators.EMAIndicator;
 import org.ta4j.core.indicators.helpers.ClosePriceIndicator;
 import org.ta4j.core.rules.CrossedDownIndicatorRule;
 import org.ta4j.core.rules.CrossedUpIndicatorRule;
+import org.ta4j.core.BarSeriesManager;
+import org.ta4j.core.TradingRecord;
 
 @RunWith(JUnit4.class)
 public class DoubleEmaCrossoverStrategyFactoryTest {
