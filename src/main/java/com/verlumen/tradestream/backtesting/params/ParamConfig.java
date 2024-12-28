c76aa813bbab3aa47ac02a7889412d6940c0a027
tradestreamhq <tradestream@googlegroups.com>
2024-12-28 01:09:02 -0500

    Introduce Parameter Configuration for Backtesting Strategies (#496)

---
+++ src/main/java/com/verlumen/tradestream/backtesting/params/ParamConfig.java
@@ -0,0 +1,18 @@
+package com.verlumen.tradestream.backtesting.params;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Range;
+import com.google.protobuf.Any;
+import io.jenetics.Genotype;
+import io.jenetics.NumericGene;
+
+/**
+ * Interface for strategy-specific parameter configuration.
+ * Defines parameter ranges and conversion logic.
+ */
+interface ParamConfig<N extends Number & Comparable<N>, G extends NumericGene<N,G>> {
+    ImmutableList<Range<N>> getChromosomes();
+    Any createParameters(Genotype<G> genotype);
+}
