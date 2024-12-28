aa0152f3364a66ecb969091c8f505ab04565d743
tradestreamhq <tradestream@googlegroups.com>
2024-12-28 00:47:30 -0500

    Update ParamConfig.java

--- src/main/java/com/verlumen/tradestream/backtesting/params/ParamConfig.java
+++ src/main/java/com/verlumen/tradestream/backtesting/params/ParamConfig.java
@@ -4,6 +4,7 @@ import static com.google.common.base.Preconditions.checkArgument;
 
 import com.google.auto.value.AutoValue;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Range;
 import com.google.protobuf.Any;
 import io.jenetics.Genotype;
 import io.jenetics.DoubleGene;
@@ -13,20 +14,6 @@ import io.jenetics.DoubleGene;
  * Defines parameter ranges and conversion logic.
  */
 interface ParamConfig {
-    ImmutableList<ParamRange> getChromosomes();
+    ImmutableList<Range> getChromosomes();
     Any createParameters(Genotype<DoubleGene> genotype);
-
-    /**
-     * Represents a parameter's valid range for genetic optimization.
-     */
-    @AutoValue
-    abstract class ParamRange<T extends Comparable> {
-        abstract T min();
-        abstract T max();
-
-        static ParamRange create(T min, T max) {
-            checkArgument(min.compareTo(max) < 0);
-            return new AutoValue_ParamConfig_ParamRange(min, max);
-        }
-    }
 }
