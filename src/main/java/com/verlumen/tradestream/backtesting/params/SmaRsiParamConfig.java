9fd62acc5a0be13717ca876dca1329f99c7b2170
tradestreamhq <tradestream@googlegroups.com>
2024-12-28 01:38:12 -0500

    Update SmaRsiParamConfig.java

--- src/main/java/com/verlumen/tradestream/backtesting/params/SmaRsiParamConfig.java
+++ src/main/java/com/verlumen/tradestream/backtesting/params/SmaRsiParamConfig.java
@@ -1,4 +1,4 @@
-package com.verlumen.tradestream.backtesting;
+package com.verlumen.tradestream.backtesting.params;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Range;
