6a04d7dffea655cf58378fe4e4cf201ff103189b
tradestreamhq <tradestream@googlegroups.com>
2024-12-28 03:17:47 -0500

    Update ParamConfig.java

--- src/main/java/com/verlumen/tradestream/backtesting/params/ParamConfig.java
+++ src/main/java/com/verlumen/tradestream/backtesting/params/ParamConfig.java
@@ -2,8 +2,8 @@ package com.verlumen.tradestream.backtesting.params;
 
 import com.google.common.collect.ImmutableList;
 import com.google.protobuf.Any;
-import io.jenetics.Chromosome;
 import io.jenetics.Gene;
+import io.jenetics.NumericChromosome;
 
 /**
  * Represents configuration for a strategy's parameters that supports multiple parameter types.
@@ -13,18 +13,18 @@ public interface ParamConfig {
     /**
      * Returns a list of chromosome specifications, each defining a parameter's type and constraints.
      */
-    ImmutableList<ChromosomeSpec<?>> getChromosomeSpecs();
+    ImmutableList<NumericChromosomeSpec<?>> getNumericChromosomeSpecs();
 
     /**
      * Creates strategy parameters from a list of chromosomes.
      * @param chromosomes The chromosomes containing optimized parameter values
      * @return Protocol buffer message containing the parameters 
      */
-    Any createParameters(ImmutableList<? extends Chromosome<? extends Gene<?, ?>>> chromosomes);
+    Any createParameters(ImmutableList<? extends NumericChromosome<? extends Gene<?, ?>>> chromosomes);
 
     /**
      * Creates initial chromosomes for this parameter configuration.
      * @return List of initial chromosomes for optimization
      */
-    ImmutableList<? extends Chromosome<? extends Gene<?, ?>>> initialChromosomes();
+    ImmutableList<? extends NumericChromosome<? extends Gene<?, ?>>> initialNumericChromosomes();
 }
