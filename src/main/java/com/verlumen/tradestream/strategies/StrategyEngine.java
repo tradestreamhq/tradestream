e45b9dd57470c4aaf57cf23308120f31caf369b7
tradestreamhq <tradestream@googlegroups.com>
2024-12-28 22:37:01 -0500

    Create StrategyEngine.java

---
+++ src/main/java/com/verlumen/tradestream/strategies/StrategyEngine.java
@@ -0,0 +1,20 @@
+package com.verlumen.tradestream.strategies;
+
+import com.google.tradestream.marketdata.Candle;
+
+interface StrategyEngine {
+    /**
+     * Handles new candle data from the market
+     */
+    void handleCandle(Candle candle);
+    
+    /**
+     * Requests optimization of strategy parameters
+     */
+    void optimizeStrategy();
+    
+    /**
+     * Gets the current active strategy
+     */
+    Strategy getCurrentStrategy();
+}
