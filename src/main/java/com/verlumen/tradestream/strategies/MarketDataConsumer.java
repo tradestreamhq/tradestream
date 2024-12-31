eb742fbb17d22e482eb184b6c6ca00d5ea813b86
tradestreamhq <tradestream@googlegroups.com>
2024-12-30 21:27:26 -0500

    Introduce `MarketDataConsumer` Interface for Handling Market Data (#509)
    * Create MarketDataConsumer.java
    
    * Update BUILD

---
+++ src/main/java/com/verlumen/tradestream/strategies/MarketDataConsumer.java
@@ -0,0 +1,22 @@
+package com.verlumen.tradestream.strategies;
+
+import com.verlumen.tradestream.marketdata.Candle;
+import java.util.function.Consumer;
+
+/**
+ * Interface for consuming market data from Kafka and forwarding it to
+ * registered handlers.
+ */
+interface MarketDataConsumer {
+    /**
+     * Starts consuming market data and forwards it to the provided handler.
+     *
+     * @param handler Consumer that will process each candle
+     */
+    void startConsuming(Consumer<Candle> handler);
+
+    /**
+     * Stops consuming market data and performs cleanup.
+     */
+    void stopConsuming();
+}
