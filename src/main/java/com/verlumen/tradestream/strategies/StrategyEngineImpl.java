011545c0fb35cdb9a6713f658d67aba01f5a758f
tradestreamhq <tradestream@googlegroups.com>
2024-12-31 01:12:10 -0500

    Update StrategyEngineImpl.java

--- src/main/java/com/verlumen/tradestream/strategies/StrategyEngineImpl.java
+++ src/main/java/com/verlumen/tradestream/strategies/StrategyEngineImpl.java
@@ -5,25 +5,26 @@ import com.verlumen.tradestream.marketdata.Candle;
 import org.ta4j.core.Strategy;
 
 /**
- * Core implementation of the Strategy Engine that coordinates strategy optimization,
- * candlestick processing, and trade signal generation.
+ * Core implementation of the Strategy Engine that coordinates strategy optimization, candlestick
+ * processing, and trade signal generation.
  */
 final class StrategyEngineImpl implements StrategyEngine {
-    @Inject
-    StrategyEngineImpl() {}
 
-    @Override
-    public synchronized void handleCandle(Candle candle) {
-        throw new UnsupportedOperationException();
-    }
+  @Inject
+  StrategyEngineImpl() {}
 
-    @Override
-    public synchronized void optimizeStrategy() {
-        throw new UnsupportedOperationException();
-    }
+  @Override
+  public synchronized void handleCandle(Candle candle) {
+    throw new UnsupportedOperationException();
+  }
 
-    @Override
-    public Strategy getCurrentStrategy() {
-        throw new UnsupportedOperationException();
-    }
+  @Override
+  public synchronized void optimizeStrategy() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public Strategy getCurrentStrategy() {
+    throw new UnsupportedOperationException();
+  }
 }
