298b5a498e2bce8d4fd8579b20504b305464cf50
tradestreamhq <tradestream@googlegroups.com>
2024-12-31 01:24:11 -0500

    Update StrategyEngineImpl.java

--- src/main/java/com/verlumen/tradestream/strategies/StrategyEngineImpl.java
+++ src/main/java/com/verlumen/tradestream/strategies/StrategyEngineImpl.java
@@ -1,6 +1,7 @@
 package com.verlumen.tradestream.strategies;
 
 import com.google.inject.Inject;
+import com.verlumen.tradestream.backtesting.GAServiceClient;
 import com.verlumen.tradestream.marketdata.Candle;
 import org.ta4j.core.Strategy;
 
