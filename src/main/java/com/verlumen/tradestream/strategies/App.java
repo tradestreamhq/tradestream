27534d75221fdeca944ad29b7b3e873dc8ffad83
tradestreamhq <tradestream@googlegroups.com>
2024-12-31 21:33:07 -0500

    Update App.java

--- src/main/java/com/verlumen/tradestream/strategies/App.java
+++ src/main/java/com/verlumen/tradestream/strategies/App.java
@@ -3,6 +3,12 @@ package com.verlumen.tradestream.strategies;
 import com.google.common.flogger.FluentLogger;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
+import com.google.inject.assistedinject.Assisted;
+import com.verlumen.tradestream.execution.RunMode;
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
 
 /**
  * Main entry point for the Strategy Module. Coordinates data flow between components and manages the
@@ -11,8 +17,12 @@ import com.google.inject.Inject;
 final class App {
   private static final FluentLogger logger = FluentLogger.forEnclosingClass();
 
+  private final RunMode runMode;
+
   @Inject
-  App() {}
+  App(@Assisted RunMode runMode) {
+    this.runMode = runMode;
+  }
 
   /** Starts all strategy module components */
   public void start() {
@@ -22,11 +32,34 @@ final class App {
   /** Gracefully shuts down all strategy module components */
   public void shutdown() {}
 
-  public static void main(String[] args) throws Exception {
+  interface Factory {
+    App create(RunMode runMode);
+  }
+
+  public static void main(String[] args) throws ArgumentParserException {
     logger.atInfo().log("TradeStream application starting up with %d arguments", args.length);
-    App app = Guice.createInjector(StrategiesModule.create(args)).getInstance(App.class);
+
+    ArgumentParser argumentParser = createArgumentParser();
+    Namespace namespace = argumentParser.parseArgs(args);
+    RunMode runMode = RunMode.fromString(namespace.getString("runMode"));
+    App.Factory appFactory =
+        Guice.createInjector(StrategiesModule.create(args)).getInstance(App.Factory.class);
+    App app = appFactory.create(runMode);
 
     // Start the service
     app.start();
+  }
+
+  private static ArgumentParser createArgumentParser() {
+    ArgumentParser parser =
+        ArgumentParsers.newFor("TradeStreamDataIngestion")
+            .build()
+            .defaultHelp(true)
+            .description("Configuration for Kafka producer and exchange settings");
+
+    // Run mode configuration
+    parser.addArgument("--runMode").choices("wet", "dry").help("Run mode: wet or dry");
+
+    return parser;
   }
 }
