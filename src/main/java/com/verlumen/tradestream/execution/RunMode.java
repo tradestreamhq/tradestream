6b452ca32cfe5d771da20571c7fc5c5c763b2ed0
tradestreamhq <tradestream@googlegroups.com>
2024-12-31 22:16:54 -0500

    Add `fromString` Method to `RunMode` Enum (#539)
    * Update RunMode.java

--- src/main/java/com/verlumen/tradestream/execution/RunMode.java
+++ src/main/java/com/verlumen/tradestream/execution/RunMode.java
@@ -3,4 +3,8 @@ package com.verlumen.tradestream.execution;
 public enum RunMode {
   WET,
   DRY;
+
+  public static RunMode fromString(String name) {
+    return RunMode.valueOf(name.toUpperCase());
+  }
 }
