952e36e1e1b7475374b7cae66eb9be8608768ecd
tradestreamhq <tradestream@googlegroups.com>
2024-12-31 22:05:21 -0500

    Update RunMode.java

--- src/main/java/com/verlumen/tradestream/execution/RunMode.java
+++ src/main/java/com/verlumen/tradestream/execution/RunMode.java
@@ -1,10 +1,10 @@
 package com.verlumen.tradestream.execution;
 
 public enum RunMode {
+  WET,
+  DRY;
+
   public static RunMode fromString(String name) {
     return RunMode.valueOf(name.toUpperCase());
   }
-  
-  WET,
-  DRY;
 }
