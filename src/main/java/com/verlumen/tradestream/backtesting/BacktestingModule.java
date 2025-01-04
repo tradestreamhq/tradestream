532e7dfdf1c67f08b99af9c385491b3bd5288c4c
tradestreamhq <tradestream@googlegroups.com>
2025-01-03 22:28:51 -0500

    Update BacktestingModule.java

--- src/main/java/com/verlumen/tradestream/backtesting/BacktestingModule.java
+++ src/main/java/com/verlumen/tradestream/backtesting/BacktestingModule.java
@@ -2,7 +2,7 @@ package com.verlumen.tradestream.backtesting;
 
 import com.google.inject.AbstractModule;
 
-final class BacktestingModule implements AbstractModule {
+final class BacktestingModule extends AbstractModule {
   @Override
   protected void configure() {}
 }
