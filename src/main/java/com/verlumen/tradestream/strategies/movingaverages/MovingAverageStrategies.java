2343221734d7cf1aec2557db8c8a4f9288aae392
tradestreamhq <tradestream@googlegroups.com>
2025-01-03 21:10:41 -0500

    Create MovingAverageStrategies.java

---
+++ src/main/java/com/verlumen/tradestream/strategies/movingaverages/MovingAverageStrategies.java
@@ -0,0 +1,23 @@
+package com.verlumen.tradestream.strategies.movingaverages;
+
+import com.google.common.collect.ImmutableList;
+import com.verlumen.tradestream.strategies.StrategyFactory;
+
+/**
+ * Provides a centralized collection of all available moving average-based strategy factories.
+ * This class is immutable and thread-safe.
+ */
+public final class MovingAverageStrategies {
+    /**
+     * An immutable list of all moving average strategy factories.
+     */
+    public static final ImmutableList<StrategyFactory<?>> ALL_FACTORIES = ImmutableList.of(
+        new DoubleEmaCrossoverStrategyFactory(),
+        new MomentumSmaCrossoverStrategyFactory(),
+        new SmaEmaCrossoverStrategyFactory(),
+        new TripleEmaCrossoverStrategyFactory()
+    );
+
+    // Prevent instantiation
+    private MovingAverageStrategies() {}
+}
