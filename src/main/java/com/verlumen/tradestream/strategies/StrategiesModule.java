20560683cc92f6f84a45f82acc337a7bdd78528f
tradestreamhq <tradestream@googlegroups.com>
2025-01-03 21:33:45 -0500

    Update StrategiesModule.java

--- src/main/java/com/verlumen/tradestream/strategies/StrategiesModule.java
+++ src/main/java/com/verlumen/tradestream/strategies/StrategiesModule.java
@@ -1,16 +1,13 @@
 package com.verlumen.tradestream.strategies;
 
 import com.google.auto.value.AutoValue;
-import com.google.common.collect.ImmutableList;
 import com.google.inject.AbstractModule;
 
 @AutoValue
 abstract class StrategiesModule extends AbstractModule {
-  static StrategiesModule create(String[] commandLineArgs) {
-    return new AutoValue_StrategiesModule(ImmutableList.copyOf(commandLineArgs));
+  static StrategiesModule create() {
+    return new StrategiesModule();
   }
-
-  abstract ImmutableList<String> commandLineArgs();
   
   @Override
   protected void configure() {
