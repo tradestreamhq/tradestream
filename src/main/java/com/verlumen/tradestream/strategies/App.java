2ef88a39861add65844da5e7ccee3cda03587374
tradestreamhq <tradestream@googlegroups.com>
2025-01-06 00:20:46 -0500

    Enhance Kafka Configuration in `App.java` with Extended Argument Parser   (#583)
    * Update App.java

--- src/main/java/com/verlumen/tradestream/strategies/App.java
+++ src/main/java/com/verlumen/tradestream/strategies/App.java
@@ -68,8 +68,66 @@ final class App {
             .defaultHelp(true)
             .description("Configuration for Kafka producer and exchange settings");
 
+    parser.addArgument("--candleTopic")
+      .type(String.class)
+      .help("Kafka topic to subscribe to candle data");
+
+    // Kafka configuration
+    parser.addArgument("--kafka.bootstrap.servers")
+      .setDefault("localhost:9092")
+      .help("Kafka bootstrap servers");
+
+    parser.addArgument("--kafka.acks")
+      .setDefault("all")
+      .help("Kafka acknowledgment configuration");
+
+    parser.addArgument("--kafka.retries")
+      .type(Integer.class)
+      .setDefault(0)
+      .help("Number of retries");
+
+    parser.addArgument("--kafka.batch.size")
+      .type(Integer.class)
+      .setDefault(16384)
+      .help("Batch size in bytes");
+
+    parser.addArgument("--kafka.linger.ms")
+      .type(Integer.class)
+      .setDefault(1)
+      .help("Linger time in milliseconds");
+
+    parser.addArgument("--kafka.buffer.memory")
+      .type(Integer.class)
+      .setDefault(33554432)
+      .help("Buffer memory in bytes");
+
+    parser.addArgument("--kafka.key.serializer")
+      .setDefault("org.apache.kafka.common.serialization.StringSerializer")
+      .help("Key serializer class");
+
+    parser.addArgument("--kafka.value.serializer")
+      .setDefault("org.apache.kafka.common.serialization.ByteArraySerializer")
+      .help("Value serializer class");
+
+    // SASL configuration
+    parser.addArgument("--kafka.security.protocol")
+      .setDefault("PLAINTEXT")
+      .help("Protocol used to communicate with brokers (e.g., PLAINTEXT, SASL_SSL)");
+
+    parser.addArgument("--kafka.sasl.mechanism")
+      .setDefault("")
+      .help("SASL mechanism used for authentication (e.g., PLAIN, SCRAM-SHA-256)");
+
+    parser.addArgument("--kafka.sasl.jaas.config")
+      .setDefault("")
+      .help("SASL JAAS configuration");
+
     // Run mode configuration
     parser.addArgument("--runMode").choices("wet", "dry").help("Run mode: wet or dry");
+
+    parser.addArgument("--tradeSignalTopic")
+      .type(String.class)
+      .help("Kafka topic to publish signal data");
 
     return parser;
   }
