923a7e0dbaa49e07464af605cc0030fcdac8c321
tradestreamhq <tradestream@googlegroups.com>
2025-01-06 01:47:25 -0500

    Update KafkaPropertiesTest.java

--- src/test/java/com/verlumen/tradestream/kafka/KafkaPropertiesTest.java
+++ src/test/java/com/verlumen/tradestream/kafka/KafkaPropertiesTest.java
@@ -2,15 +2,14 @@ package com.verlumen.tradestream.kafka;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
 
 @RunWith(JUnit4.class)
 public class KafkaPropertiesTest {
