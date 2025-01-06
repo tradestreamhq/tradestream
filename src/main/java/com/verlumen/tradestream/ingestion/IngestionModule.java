21826d330aacf16746780b1762276734464f4fec
tradestreamhq <tradestream@googlegroups.com>
2025-01-06 03:19:41 -0500

    Refactor Argument Parsing in Ingestion Module with Enhanced Namespace Integration (#585)

--- src/main/java/com/verlumen/tradestream/ingestion/IngestionModule.java
+++ src/main/java/com/verlumen/tradestream/ingestion/IngestionModule.java
@@ -1,7 +1,6 @@
 package com.verlumen.tradestream.ingestion;
 
 import com.google.auto.value.AutoValue;
-import com.google.common.collect.ImmutableList;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
@@ -16,15 +15,15 @@ import net.sourceforge.argparse4j.inf.Namespace;
 
 @AutoValue
 abstract class IngestionModule extends AbstractModule {
-  static IngestionModule create(String[] commandLineArgs) {
-    return new AutoValue_IngestionModule(ImmutableList.copyOf(commandLineArgs));
+  static IngestionModule create(Namespace namespace) {
+    return new AutoValue_IngestionModule(namespace);
   }
 
-  abstract ImmutableList<String> commandLineArgs();
+  abstract Namespace namespace();
   
   @Override
   protected void configure() {
-    bind(Namespace.class).toProvider(ConfigArguments.create(commandLineArgs()));
+    bind(Namespace.class).toProvider(this::namespace);
     bind(CurrencyPairSupply.class).toProvider(CurrencyPairSupplyProvider.class);
     bind(ExchangeStreamingClient.Factory.class).to(ExchangeStreamingClientFactory.class);
     bind(HttpClient.class).to(HttpClientImpl.class);
