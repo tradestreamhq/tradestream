f0b0f7a90b3a9f505a19d1f0b2801743f9e95eaf
tradestreamhq <tradestream@googlegroups.com>
2025-01-10 18:46:16 -0500

    Revert "Update App.java (#600)" (#603)
    This reverts commit 84f80db7db58841d38e5ad8ed1aebb4b468c11a4.

--- src/main/java/com/verlumen/tradestream/ingestion/App.java
+++ src/main/java/com/verlumen/tradestream/ingestion/App.java
@@ -104,6 +104,7 @@ final class App {
       .help("Candle interval in seconds");
 
     parser.addArgument("--candlePublisherTopic")
+      .setDefault("candles")
       .help("Kafka topic to publish candle data");
 
     // Kafka configuration
