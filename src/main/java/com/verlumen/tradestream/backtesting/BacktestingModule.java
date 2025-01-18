282405aa7a92769ad2350b9fe285790d97a9fc3e
tradestreamhq <tradestream@googlegroups.com>
2025-01-18 12:14:47 -0500

    Update BacktestingModule.java

--- src/main/java/com/verlumen/tradestream/backtesting/BacktestingModule.java
+++ src/main/java/com/verlumen/tradestream/backtesting/BacktestingModule.java
@@ -12,15 +12,17 @@ public final class BacktestingModule extends AbstractModule {
   private static final int DEFAULT_GA_SERVICE_PORT = 50052;
 
   public static BacktestingModule create() {
-    return new BacktestingModule();
+    return new BacktestingModule(
+      new Endpoint(DEFAULT_BACKTEST_SERVICE_HOST, DEFAULT_BACKTEST_SERVICE_PORT),
+      new Endpoint(DEFAULT_GA_SERVICE_HOST, DEFAULT_GA_SERVICE_PORT));
   }
 
   private final Endpoint backtestService;
   private final Endpoint gaService;
 
-  BacktestingModule() {
-    this.backtestService = new Endpoint(DEFAULT_BACKTEST_SERVICE_HOST, DEFAULT_BACKTEST_SERVICE_PORT);
-    this.gaService = new Endpoint(DEFAULT_GA_SERVICE_HOST, DEFAULT_GA_SERVICE_PORT);
+  BacktestingModule(Endpoint backtestService, Endpoint gaService) {
+    this.backtestService = backtestService;
+    this.gaService = gaService;
   }
 
   @Override
@@ -40,6 +42,4 @@ public final class BacktestingModule extends AbstractModule {
   GAServiceGrpc.GAServiceBlockingStub provideGAServiceStub() {
     return GAServiceGrpc.newBlockingStub(gaService.createChannel());
   }
-
-  private BacktestingModule() {}
 }
