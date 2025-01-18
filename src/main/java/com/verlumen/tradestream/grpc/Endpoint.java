f1967af06a9c9345a4cedd7e008b1a0839889812
tradestreamhq <tradestream@googlegroups.com>
2025-01-18 12:08:48 -0500

    Update Endpoint.java

--- src/main/java/com/verlumen/tradestream/grpc/Endpoint.java
+++ src/main/java/com/verlumen/tradestream/grpc/Endpoint.java
@@ -3,7 +3,7 @@ package com.verlumen.tradestream.grpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
-public record Endpoint(String host, String port) {
+public record Endpoint(String host, int port) {
   public ManagedChannel createChannel() {
     return ManagedChannelBuilder
         .forAddress(host, port)
