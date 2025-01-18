18ec3bd101dd6a7beb382c014ec1bbb38a7e91fb
tradestreamhq <tradestream@googlegroups.com>
2025-01-18 17:37:58 -0500

    Update GAServer.java

--- src/main/java/com/verlumen/tradestream/backtesting/GAServer.java
+++ src/main/java/com/verlumen/tradestream/backtesting/GAServer.java
@@ -1,26 +1,28 @@
 package com.verlumen.tradestream.backtesting;
 
-import io.grpc.Server;
-import io.grpc.ServerBuilder;
+import com.google.common.flogger.FluentLogger;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
 
 public class GAServer {
+    private static final FluentLogger logger = FluentLogger.forEnclosingClass();
     public static void main(String[] args) throws Exception {
         // Create Guice injector
         Injector injector = Guice.createInjector(BacktestingModule.create());
-        
+
         // Get GAServiceImpl instance from Guice
         GAServiceImpl gaService = injector.getInstance(GAServiceImpl.class);
-        
+
         // Create and start gRPC server
         Server server = ServerBuilder.forPort(50051)
             .addService(gaService)
             .build();
-        
+
         server.start();
         System.out.println("GA Server started on port 50051");
-        
+
         // Keep server running
         server.awaitTermination();
     }
