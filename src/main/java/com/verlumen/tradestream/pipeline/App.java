cbaa5fc361b8d0b6681e7995b674e12cf6ebbe58
tradestreamhq <tradestream@googlegroups.com>
2025-01-25 16:43:55 -0500

    Update App.java

--- src/main/java/com/verlumen/tradestream/pipeline/App.java
+++ src/main/java/com/verlumen/tradestream/pipeline/App.java
@@ -2,7 +2,6 @@ package com.verlumen.tradestream.pipeline;
 
 import java.util.Arrays;
 import java.util.List;
-
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.coders.StringUtf8Coder;
 import org.apache.beam.sdk.options.Default;
@@ -15,28 +14,31 @@ import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.TypeDescriptors;
 
 public class App {
-	public interface Options extends StreamingOptions {
-		@Description("Input text to print.")
-		@Default.String("My input text")
-		String getInputText();
+  public interface Options extends StreamingOptions {
+    @Description("Input text to print.")
+    @Default.String("My input text")
+    String getInputText();
 
-		void setInputText(String value);
-	}
+    void setInputText(String value);
+  }
 
-	public static PCollection<String> buildPipeline(Pipeline pipeline, String inputText) {
-		return pipeline
-				.apply("Create elements", Create.of(Arrays.asList("Hello", "World!", inputText)))
-				.apply("Print elements",
-						MapElements.into(TypeDescriptors.strings()).via(x -> {
-							System.out.println(x);
-							return x;
-						}));
-	}
+  public static PCollection<String> buildPipeline(Pipeline pipeline, String inputText) {
+    return pipeline
+        .apply("Create elements", Create.of(Arrays.asList("Hello", "World!", inputText)))
+        .apply(
+            "Print elements",
+            MapElements.into(TypeDescriptors.strings())
+                .via(
+                    x -> {
+                      System.out.println(x);
+                      return x;
+                    }));
+  }
 
-	public static void main(String[] args) {
-		var options = PipelineOptionsFactory.fromArgs(args).withValidation().as(Options.class);
-		var pipeline = Pipeline.create(options);
-		App.buildPipeline(pipeline, options.getInputText());
-		pipeline.run().waitUntilFinish();
-	}
+  public static void main(String[] args) {
+    var options = PipelineOptionsFactory.fromArgs(args).withValidation().as(Options.class);
+    var pipeline = Pipeline.create(options);
+    App.buildPipeline(pipeline, options.getInputText());
+    pipeline.run().waitUntilFinish();
+  }
 }
