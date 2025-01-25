38ceb74281b4dbdefeb136ae284b54876e7404d3
tradestreamhq <tradestream@googlegroups.com>
2025-01-25 16:44:52 -0500

    Add PipelineModule for dependency injection (#631)
    * Create PipelineModule.java
    
    * Update BUILD

---
+++ src/main/java/com/verlumen/tradestream/pipeline/PipelineModule.java
@@ -0,0 +1,8 @@
+package com.verlumen.tradestream.pipeline;
+
+import com.google.inject.AbstractModule;
+
+class PipelineModule extends AbstractModule {
+  @Override
+  protected void configure() {}
+}
