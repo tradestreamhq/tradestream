6b2d184c65cee2c100c2d44eabd5df64c55dbb56
tradestreamhq <tradestream@googlegroups.com>
2025-01-25 19:43:11 -0500

    Update App.java

--- src/main/java/com/verlumen/tradestream/pipeline/App.java
+++ src/main/java/com/verlumen/tradestream/pipeline/App.java
@@ -42,7 +42,7 @@ public class App {
   }
 
   public static void main(String[] args) {
-    App app = Guice.createInjector().getInstance(App.class);
+    App app = Guice.createInjector(PipelineModule.create()).getInstance(App.class);
     app.runPipeline(args);
   }
 }
