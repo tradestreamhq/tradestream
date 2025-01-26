8e9fcc834fd98e8c0cc9d6ce8483c811935728b9
tradestreamhq <tradestream@googlegroups.com>
2025-01-25 19:54:30 -0500

    Update PipelineModule.java

--- src/main/java/com/verlumen/tradestream/pipeline/PipelineModule.java
+++ src/main/java/com/verlumen/tradestream/pipeline/PipelineModule.java
@@ -3,6 +3,10 @@ package com.verlumen.tradestream.pipeline;
 import com.google.inject.AbstractModule;
 
 class PipelineModule extends AbstractModule {
+  static PipelineModule create() {
+    return new PipelineModule();
+  }
+
   @Override
   protected void configure() {}
 }
