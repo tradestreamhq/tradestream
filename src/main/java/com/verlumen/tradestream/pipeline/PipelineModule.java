33d5b60cdc57200b0adc1ba78219e9098c6b1a32
tradestreamhq <tradestream@googlegroups.com>
2025-01-25 20:21:58 -0500

    Update PipelineModule.java

--- src/main/java/com/verlumen/tradestream/pipeline/PipelineModule.java
+++ src/main/java/com/verlumen/tradestream/pipeline/PipelineModule.java
@@ -1,12 +1,30 @@
 package com.verlumen.tradestream.pipeline;
 
+import com.google.auto.value.AutoValue;
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import package com.verlumen.tradestream.kafka.KafkaReadTransform;
 
-class PipelineModule extends AbstractModule {
-  static PipelineModule create() {
-    return new PipelineModule();
+@AutoValue
+abstract class PipelineModule extends AbstractModule {
+  static PipelineModule create(
+    String bootstrapServers, String candleTopic, int intervalHours) {
+    return new AutoValue_PipelineModule();
   }
+
+  abstract String bootstrapServers();
+  abstract String candleTopic();
+  abstract int dynamicReadIntervalHours();
 
   @Override
   protected void configure() {}
+
+  @Provides
+  KafkaReadTransform provideKafkaReadTransform() {
+    return KafkaReadTransform.builder()
+      .setBootstrapServers(bootstrapServers())
+      .setTopic(candleTopic())
+      .setDynamicReadIntervalHours(dynamicReadIntervalHours())
+      .build();
+  }
 }
