499cef03d352ce536b614242945ed9c546a26a61
tradestreamhq <tradestream@googlegroups.com>
2025-01-25 22:02:55 -0500

    Update App.java

--- src/main/java/com/verlumen/tradestream/pipeline/App.java
+++ src/main/java/com/verlumen/tradestream/pipeline/App.java
@@ -7,7 +7,6 @@ import java.util.Arrays;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.options.Default;
 import org.apache.beam.sdk.options.Description;
-import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.options.StreamingOptions;
 import org.apache.beam.sdk.transforms.Create;
