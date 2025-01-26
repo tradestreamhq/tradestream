eabda9ccda9a4b419d1e9d4c9ff9015a103c1e45
tradestreamhq <tradestream@googlegroups.com>
2025-01-26 00:03:34 -0500

    Update KafkaReadTransform.java

--- src/main/java/com/verlumen/tradestream/kafka/KafkaReadTransform.java
+++ src/main/java/com/verlumen/tradestream/kafka/KafkaReadTransform.java
@@ -10,7 +10,6 @@ import org.apache.beam.sdk.values.TypeDescriptors;
 import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.beam.sdk.io.kafka.KafkaRecord;
-import org.joda.time.Duration;
 
 import java.util.Collections;
 import java.util.Map;
@@ -20,7 +19,6 @@ public abstract class KafkaReadTransform extends PTransform<PBegin, PCollection<
 
   abstract String bootstrapServers();
   abstract String topic();
-  abstract int dynamicReadIntervalHours();
   abstract Map<String, Object> consumerConfig();
 
   public static Builder builder() {
@@ -32,7 +30,6 @@ public abstract class KafkaReadTransform extends PTransform<PBegin, PCollection<
   public abstract static class Builder {
     public abstract Builder setBootstrapServers(String bootstrapServers);
     public abstract Builder setTopic(String topic);
-    public abstract Builder setDynamicReadIntervalHours(int hours);
     public abstract Builder setConsumerConfig(Map<String, Object> consumerConfig);
     public abstract KafkaReadTransform build();
   }
@@ -48,8 +45,7 @@ public abstract class KafkaReadTransform extends PTransform<PBegin, PCollection<
             .withTopic(topic())
             .withKeyDeserializer(LongDeserializer.class)
             .withValueDeserializer(StringDeserializer.class)
-            .withConsumerConfigUpdates(consumerConfig())
-            .withDynamicRead(interval);
+            .withConsumerConfigUpdates(consumerConfig());
 
     // Apply the read, then map each KafkaRecord<Long,String> to just the String value
     return input
