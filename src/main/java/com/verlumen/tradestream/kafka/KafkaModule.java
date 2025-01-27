61bcdf033b90e1df42e1971147161c6aa7e8c3cd
tradestreamhq <tradestream@googlegroups.com>
2025-01-27 11:18:19 -0500

    Update

--- src/main/java/com/verlumen/tradestream/kafka/KafkaModule.java
+++ src/main/java/com/verlumen/tradestream/kafka/KafkaModule.java
@@ -3,6 +3,7 @@ package com.verlumen.tradestream.kafka;
 import com.google.auto.value.AutoValue;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
 import com.google.inject.TypeLiteral;
 import org.apache.kafka.clients.producer.KafkaProducer;
 
@@ -16,8 +17,15 @@ public abstract class KafkaModule extends AbstractModule {
   
   @Override
   protected void configure() {
-    bind(KafkaProperties.class).toProvider(this::kafkaProperties);
     bind(new TypeLiteral<KafkaProducer<String, byte[]>>() {})
         .toProvider(KafkaProducerProvider.class);
+    bind(KafkaProperties.class).toProvider(this::kafkaProperties);
+    bind(KafkaReadTransform.class).to(KafkaReadTransformImpl.class);
+  }
+
+  @Provides
+  KafkaReadTransform provideKafkaReadTransform(KafkaProperties kafkaProperties) {
+    return KafkaReadTransformImpl.builder()
+      .set();
   }
 }
