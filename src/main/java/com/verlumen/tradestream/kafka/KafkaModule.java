f55b13282d8ff8ecc4359f223e8f7ca7d959f497
tradestreamhq <tradestream@googlegroups.com>
2025-01-27 12:24:51 -0500

    Update KafkaModule.java

--- src/main/java/com/verlumen/tradestream/kafka/KafkaModule.java
+++ src/main/java/com/verlumen/tradestream/kafka/KafkaModule.java
@@ -8,17 +8,15 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 
 @AutoValue
 public abstract class KafkaModule extends AbstractModule {
-  public static KafkaModule create(KafkaProperties kafkaProperties) {
-    return new AutoValue_KafkaModule(kafkaProperties);
+  public static KafkaModule create() {
+    return new AutoValue_KafkaModule();
   }
-
-  abstract KafkaProperties kafkaProperties();
   
   @Override
   protected void configure() {
     bind(new TypeLiteral<KafkaProducer<String, byte[]>>() {})
         .toProvider(KafkaProducerProvider.class);
-    bind(KafkaProperties.class).toProvider(this::kafkaProperties);
+    bind(KafkaProperties.class).toProvider(KafkaProperties::create);
     bind(KafkaReadTransform.Factory.class).to(KafkaReadTransformFactory.class);
   }
 }
