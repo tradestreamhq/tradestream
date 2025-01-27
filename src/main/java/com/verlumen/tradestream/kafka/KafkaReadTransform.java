dfde0c46f7bade99961f15faf5f0a9d1c3547a65
tradestreamhq <tradestream@googlegroups.com>
2025-01-27 11:54:27 -0500

    Update KafkaReadTransform.java

--- src/main/java/com/verlumen/tradestream/kafka/KafkaReadTransform.java
+++ src/main/java/com/verlumen/tradestream/kafka/KafkaReadTransform.java
@@ -5,7 +5,7 @@ import org.apache.beam.sdk.values.PBegin;
 import org.apache.beam.sdk.values.PCollection;
 
 public abstract class KafkaReadTransform extends PTransform<PBegin, PCollection<String>> {
-  static interface Factory {
+  public static interface Factory {
     KafkaReadTransform create(String topic);
   }
 }
