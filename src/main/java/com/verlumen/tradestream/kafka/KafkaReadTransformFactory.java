a54595bb58cf9a7a200916599a0ade7f33f3998e
tradestreamhq <tradestream@googlegroups.com>
2025-01-27 17:39:35 -0500

    Update KafkaReadTransformFactory.java

--- src/main/java/com/verlumen/tradestream/kafka/KafkaReadTransformFactory.java
+++ src/main/java/com/verlumen/tradestream/kafka/KafkaReadTransformFactory.java
@@ -4,14 +4,23 @@ import com.google.inject.Inject;
 
 final class KafkaReadTransformFactory implements KafkaReadTransform.Factory {
   private final KafkaProperties kafkaProperties;
+  private final RunMode runMode;
 
   @Inject
-  KafkaReadTransformFactory(KafkaProperties kafkaProperties) {
+  KafkaReadTransformFactory(KafkaProperties kafkaProperties, RunMode runMode) {
     this.kafkaProperties = kafkaProperties;
+    this.runMode = runMode;
   }
 
   @Override
   public KafkaReadTransform create(String topic) {
+    if (runMode.equals(runMode.DRY)) {
+      return DryRunKafkaReadTransform.builder()
+        .setBootstrapServers(kafkaProperties.bootstrapServers())
+        .setTopic(topic)
+        .build();
+    }
+
     return KafkaReadTransformImpl.builder()
       .setBootstrapServers(kafkaProperties.bootstrapServers())
       .setTopic(topic)
