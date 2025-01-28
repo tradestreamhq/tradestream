3c194a54e944a7dced40354ab4699b7136cfcf8b
tradestreamhq <tradestream@googlegroups.com>
2025-01-28 10:29:15 -0500

    Update

--- src/main/java/com/verlumen/tradestream/ingestion/IngestionModule.java
+++ src/main/java/com/verlumen/tradestream/ingestion/IngestionModule.java
@@ -38,7 +38,7 @@ abstract class IngestionModule extends AbstractModule {
             .implement(CandlePublisher.class, CandlePublisherImpl.class)
             .build(CandlePublisher.Factory.class));
 
-    install(KafkaModule.create(ingestionConfig.kafkaBootstrapServers()));
+    install(KafkaModule.create(ingestionConfig().kafkaBootstrapServers()));
   }
 
   @Provides
