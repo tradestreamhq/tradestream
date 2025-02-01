a5984278ef216cd87e333c92541ec3d2f8c1684c
tradestreamhq <tradestream@googlegroups.com>
2025-02-01 13:34:59 -0500

    Update MarketDataModule.java

--- src/main/java/com/verlumen/tradestream/marketdata/MarketDataModule.java
+++ src/main/java/com/verlumen/tradestream/marketdata/MarketDataModule.java
@@ -3,7 +3,9 @@ package com.verlumen.tradestream.marketdata;
 import com.google.inject.AbstractModule;
 
 public final class MarketDataModule extends AbstractModule {
-  public static MarketDataModule create() {}
+  public static MarketDataModule create() {
+    return new MarketDataModule();
+  }
 
   private MarketDataModule() {}
 
