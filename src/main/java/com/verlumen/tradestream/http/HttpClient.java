af418d5fc93f4c028dc9a65febba704231696aa5
tradestreamhq <tradestream@googlegroups.com>
2025-02-01 15:19:51 -0500

    Refactor HTTP Client Code into `http` Package (#683)

--- src/main/java/com/verlumen/tradestream/http/HttpClient.java
+++ src/main/java/com/verlumen/tradestream/http/HttpClient.java
@@ -1,8 +1,8 @@
-package com.verlumen.tradestream.ingestion;
+package com.verlumen.tradestream.http;
 
 import java.io.IOException;
 import java.util.Map;
 
-interface HttpClient {
+public interface HttpClient {
     String get(String url, Map<String, String> headers) throws IOException;
 }
