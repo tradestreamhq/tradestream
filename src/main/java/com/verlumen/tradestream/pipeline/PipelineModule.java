d698284d9c641972e4e64631aef19cd47f9df0a7
tradestreamhq <tradestream@googlegroups.com>
2025-02-01 11:51:39 -0500

    Update PipelineModule.java

--- src/main/java/com/verlumen/tradestream/pipeline/PipelineModule.java
+++ src/main/java/com/verlumen/tradestream/pipeline/PipelineModule.java
@@ -1,5 +1,7 @@
 package com.verlumen.tradestream.pipeline;
 
+import static com.google.protobuf.util.Timestamps.fromMillis;
+
 import com.google.auto.value.AutoValue;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
@@ -8,12 +10,22 @@ import com.verlumen.tradestream.execution.RunMode;
 import com.verlumen.tradestream.kafka.DryRunKafkaReadTransform;
 import com.verlumen.tradestream.kafka.KafkaModule;
 import com.verlumen.tradestream.kafka.KafkaReadTransform;
+import com.verlumen.tradestream.marketdata.Trade;
 import java.nio.charset.StandardCharsets;
 import org.apache.kafka.common.serialization.ByteArrayDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
 @AutoValue
 abstract class PipelineModule extends AbstractModule {
+  private static final Trade DRY_RUN_TRADE = Trade.newBuilder()
+      .setExchange("FakeExhange")
+      .setCurrencyPair("DRY/RUN")
+      .setTradeId("trade-123")
+      .setTimestamp(fromMillis(1738428532166))
+      .setPrice(50000.0)
+      .setVolume(0.1)
+      .build();
+
   static PipelineModule create(
     String bootstrapServers, String candleTopic, String runMode) {
     return new AutoValue_PipelineModule(bootstrapServers, candleTopic, runMode);
