67ee87980f1368e76fd7902579ddd1a21ab105aa
tradestreamhq <tradestream@googlegroups.com>
2025-02-01 16:45:08 -0500

    Update MarketDataModule.java

--- src/main/java/com/verlumen/tradestream/marketdata/MarketDataModule.java
+++ src/main/java/com/verlumen/tradestream/marketdata/MarketDataModule.java
@@ -6,7 +6,7 @@ import com.google.inject.Provides;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
 
 @AutoValue
-public final class MarketDataModule extends AbstractModule {
+public abstract class MarketDataModule extends AbstractModule {
   public static MarketDataModule create(MarketDataConfig config) {
     return new AutoValue_MarketDataModule(config);
   }
