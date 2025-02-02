841e69619d6795fb5e442e8cf1c188ebd28c6a3c
tradestreamhq <tradestream@googlegroups.com>
2025-02-02 13:10:20 -0500

    Update PipelineConfig.java

--- src/main/java/com/verlumen/tradestream/pipeline/PipelineConfig.java
+++ src/main/java/com/verlumen/tradestream/pipeline/PipelineConfig.java
@@ -1,3 +1,7 @@
 package com.verlumen.tradestream.pipeline;
 
-record PipelineConfig(String bootstrapServers, String tradeTopic, String runMode) {}
+record PipelineConfig(String bootstrapServers, String tradeTopic, String runMode) {
+  static PipelineConfig create(String bootstrapServers, String tradeTopic, String runMode) {
+    return new PipelineConfig(bootstrapServers, tradeTopic, runMode);
+  }
+}
