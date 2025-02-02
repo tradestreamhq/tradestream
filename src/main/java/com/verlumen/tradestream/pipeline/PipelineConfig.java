ab5071ef39dbeefb248a0aa3c23faa3c44ab1e93
tradestreamhq <tradestream@googlegroups.com>
2025-02-02 16:37:10 -0500

    Revert "Revert "Update PipelineConfig.java (#735)" (#736)"
    This reverts commit 0e4fa742b9a2845e5c65382e02f24269c550cf7e.

--- src/main/java/com/verlumen/tradestream/pipeline/PipelineConfig.java
+++ src/main/java/com/verlumen/tradestream/pipeline/PipelineConfig.java
@@ -9,11 +9,11 @@ record PipelineConfig(
   Duration allowedLateness,
   Duration windowDuration,
   Duration allowedTimestampSkew) {
+  private static final Duration FIVE_MINUTES = Duration.standardMinutes(5);
   private static final Duration FIVE_SECONDS = Duration.standardSeconds(5);
-  private static final Duration ONE_MINUTE = Duration.standardMinutes(1);
   private static final Duration ONE_SECOND = Duration.standardSeconds(1);
 
   static PipelineConfig create(String bootstrapServers, String tradeTopic, String runMode) {
-    return new PipelineConfig(bootstrapServers, tradeTopic, runMode, FIVE_SECONDS, ONE_MINUTE, ONE_SECOND);
+    return new PipelineConfig(bootstrapServers, tradeTopic, runMode, FIVE_SECONDS, ONE_MINUTE, FIVE_MINUTES);
   }
 }
