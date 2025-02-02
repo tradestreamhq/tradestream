4f69842ff7fd25b66a2825424d5f4a1bcb36e8ff
tradestreamhq <tradestream@googlegroups.com>
2025-02-02 15:19:41 -0500

    Update CreateCandles.java

--- src/main/java/com/verlumen/tradestream/marketdata/CreateCandles.java
+++ src/main/java/com/verlumen/tradestream/marketdata/CreateCandles.java
@@ -29,6 +29,8 @@ public class CreateCandles extends PTransform<PCollection<KV<String, Trade>>, PC
 
   @Override
   public PCollection<Candle> expand(PCollection<KV<String, Trade>> input) {
+    // IMPORTANT: This transform assumes that the input elements have valid event timestamps
+    // (e.g. they have been stamped using WithTimestamps upstream in the pipeline).
     // Note: Make sure the input is windowed. For example:
     //   input.apply(Window.into(FixedWindows.of(windowDuration))
     //         .withAllowedLateness(allowedLateness)
