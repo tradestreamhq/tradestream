88f54a757d4f76f4750f7d13ff533a27eb3f53e6
tradestreamhq <tradestream@googlegroups.com>
2025-02-01 22:26:42 -0500

    Update IngestionModule.java

--- src/main/java/com/verlumen/tradestream/ingestion/IngestionModule.java
+++ src/main/java/com/verlumen/tradestream/ingestion/IngestionModule.java
@@ -6,7 +6,6 @@ import com.google.inject.Provides;
 import com.verlumen.tradestream.execution.RunMode;
 import com.verlumen.tradestream.http.HttpModule;
 import com.verlumen.tradestream.kafka.KafkaModule;
-import com.verlumen.tradestream.marketdata.MarketDataConfig;
 import com.verlumen.tradestream.marketdata.MarketDataModule;
 
 @AutoValue
