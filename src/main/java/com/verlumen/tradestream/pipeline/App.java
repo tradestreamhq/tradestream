d0241cab01afb248a5ac2eb6a431ee8e6b2c80f3
tradestreamhq <tradestream@googlegroups.com>
2025-02-08 16:50:00 -0500

    Update App.java

--- src/main/java/com/verlumen/tradestream/pipeline/App.java
+++ src/main/java/com/verlumen/tradestream/pipeline/App.java
@@ -1,5 +1,6 @@
 package com.verlumen.tradestream.pipeline;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.flogger.FluentLogger;
 import com.google.protobuf.util.Timestamps;
 import com.google.inject.Guice;
