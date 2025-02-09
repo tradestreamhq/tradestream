fdcc554d2e8e84cf789767ca7724b24e7290c0d8
tradestreamhq <tradestream@googlegroups.com>
2025-02-09 17:14:06 -0500

    Delete src/main/java/com/verlumen/tradestream/marketdata directory

--- src/main/java/com/verlumen/tradestream/marketdata/MarketDataConfig.java
+++
@@ -1,7 +0,0 @@
-package com.verlumen.tradestream.marketdata;
-
-public record MarketDataConfig(String tradeTopic) {
-  public static MarketDataConfig create(String tradeTopic) {
-    return new MarketDataConfig(tradeTopic);
-  }
-}
