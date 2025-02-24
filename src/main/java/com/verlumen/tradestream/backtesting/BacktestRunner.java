bda7a78a07e093b589bb017613430f3e460055f7
tradestreamhq <tradestream@googlegroups.com>
2025-02-23 19:19:04 -0500

    Update BacktestRunner.java

--- src/main/java/com/verlumen/tradestream/backtesting/BacktestRunner.java
+++ src/main/java/com/verlumen/tradestream/backtesting/BacktestRunner.java
@@ -2,6 +2,7 @@ package com.verlumen.tradestream.backtesting;
 
 import com.google.auto.value.AutoValue;
 import com.verlumen.tradestream.strategies.StrategyType;
+import java.io.Serializable;
 import org.ta4j.core.BarSeries;
 import org.ta4j.core.Strategy;
 
