b73c31bf9f1ce3913407dae4990037fa99fee92b
tradestreamhq <tradestream@googlegroups.com>
2025-02-25 23:22:11 -0500

    Update BacktestingModule.java

--- src/main/java/com/verlumen/tradestream/backtesting/BacktestingModule.java
+++ src/main/java/com/verlumen/tradestream/backtesting/BacktestingModule.java
@@ -1,11 +1,29 @@
 package com.verlumen.tradestream.backtesting;
 
+import com.google.auto.value.AutoValue;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
 
-public final class BacktestingModule extends AbstractModule {
+public abstract class BacktestingModule extends AbstractModule {
   public static BacktestingModule create() {
     return new BacktestingModule();
+  }
+
+  @Override
+  protected void configure() {
+    install(GenotypeConverterModule.create());
+  }
+
+  @AutoValue
+  abstract static class GenotypeConverterModule extends AbstractModule  
+    public static BacktestingModule create() {
+      return new AutoValue_BacktestingModule_GenotypeConverterModule();
+    }
+
+    @Override
+    protected void configure() {
+      bind(GenotypeConverter.class).to(GenotypeConverterImpl.class); 
+    } 
   }
 }
