26a68dae063df7e013086f407e4f5850dbf003a0
tradestreamhq <tradestream@googlegroups.com>
2025-02-25 22:58:28 -0500

    Update GAEngineFactory.java

--- src/main/java/com/verlumen/tradestream/backtesting/GAEngineFactory.java
+++ src/main/java/com/verlumen/tradestream/backtesting/GAEngineFactory.java
@@ -11,8 +11,6 @@ interface GAEngineFactory {
      * Creates a genetic algorithm engine configured for the given request.
      *
      * @param request the GA optimization request
-     * @param series the bar series for backtesting
-     * @param backtestRunner the backtest runner for fitness evaluation
      * @return a configured GA engine
      */
     Engine<DoubleGene, Double> createEngine(GAOptimizationRequest request);
