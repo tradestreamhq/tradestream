dfb4d0a729ceef9de77112ee77118e3a519edff1
tradestreamhq <tradestream@googlegroups.com>
2025-03-01 18:30:15 -0500

    Update StrategyRecord.java

--- src/main/java/com/verlumen/tradestream/strategies/StrategyRecord.java
+++ src/main/java/com/verlumen/tradestream/strategies/StrategyRecord.java
@@ -1,16 +1,24 @@
 package com.verlumen.tradestream.strategies;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import com.google.protobuf.Any;
 import java.io.Serializable;
-import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * Immutable record of a strategy's parameters and performance score.
  */
 record StrategyRecord(StrategyType strategyType, Any parameters, double score)
     implements Serializable {
-  
-  public StrategyRecord {
-    checkNotNull(strategyType, "Strategy type cannot be null");
+  static StrategyRecord create(StrategyType strategyType, Any parameters) {
+      return create(strategyType, parameters, Double.NEGATIVE_INFINITY);    
+  }
+
+  static StrategyRecord create(StrategyType strategyType, Any parameters, double score) {
+      return new StrategyRecord(strategyType, parameters, score);    
+  }
+
+  StrategyRecord {
+    checkNotNull(strategyType, "Strategy strategyType cannot be null");
   }
 }
