6a07ab1388d221f9c969ac1b8dba9bafde14dc63
tradestreamhq <tradestream@googlegroups.com>
2025-03-05 21:47:59 -0500

    Update StrategiesModule.java

--- src/main/java/com/verlumen/tradestream/strategies/StrategiesModule.java
+++ src/main/java/com/verlumen/tradestream/strategies/StrategiesModule.java
@@ -25,7 +25,6 @@ abstract class StrategiesModule extends AbstractModule {
   @Override
   protected void configure() {
     bind(CandleBuffer.class).to(CandleBufferImpl.class);
-    bind(ExecutorService.class).toProvider(Executors::newSingleThreadExecutor);
     bind(StrategyEngine.class).to(StrategyEngineImpl.class);
     bind(new TypeLiteral<ImmutableList<StrategyFactory<?>>>() {})
         .toInstance(StrategyFactories.ALL_FACTORIES);
