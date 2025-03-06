014e922b2a7deb848b0ff4b63c8e2ed90c405079
tradestreamhq <tradestream@googlegroups.com>
2025-03-05 21:43:49 -0500

    Remove `MarketDataConsumer` and related dependencies (#817)

--- src/main/java/com/verlumen/tradestream/strategies/StrategiesModule.java
+++ src/main/java/com/verlumen/tradestream/strategies/StrategiesModule.java
@@ -6,7 +6,6 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
 import com.google.inject.TypeLiteral;
-import com.google.inject.assistedinject.FactoryModuleBuilder;
 import com.verlumen.tradestream.backtesting.BacktestingModule;
 import com.verlumen.tradestream.signals.SignalsModule;
 import com.verlumen.tradestream.signals.TradeSignalPublisher;
@@ -34,15 +33,7 @@ abstract class StrategiesModule extends AbstractModule {
     bind(StrategyState.Factory.class).to(StrategyStateFactoryImpl.class);
 
     install(BacktestingModule.create());
-    install(new FactoryModuleBuilder()
-        .implement(MarketDataConsumer.class, MarketDataConsumerImpl.class)
-        .build(MarketDataConsumer.Factory.class));
     install(SignalsModule.create());
-  }
-
-  @Provides
-  MarketDataConsumer provideMarketDataConsumer(MarketDataConsumer.Factory factory) {
-    return factory.create(candleTopic());
   }
 
   @Provides
