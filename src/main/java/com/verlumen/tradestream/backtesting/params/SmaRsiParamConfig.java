b4faf0376ce915c1b0f46426bccbbdb4bcbefb51
tradestreamhq <tradestream@googlegroups.com>
2025-03-06 22:25:58 -0500

    Update SmaRsiParamConfig.java

--- src/main/java/com/verlumen/tradestream/backtesting/params/SmaRsiParamConfig.java
+++ src/main/java/com/verlumen/tradestream/backtesting/params/SmaRsiParamConfig.java
@@ -3,6 +3,7 @@ package com.verlumen.tradestream.backtesting.params;
 import com.google.common.collect.ImmutableList;
 import com.google.protobuf.Any;
 import com.verlumen.tradestream.backtesting.ChromosomeSpec;
+import com.verlumen.tradestream.backtesting.ParamConfig;
 import com.verlumen.tradestream.strategies.SmaRsiParameters;
 import com.verlumen.tradestream.strategies.StrategyType;
 import io.jenetics.IntegerChromosome;
