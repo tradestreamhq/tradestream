fe40b433d5a8d4caaa3196167ebad0ee5c1cf7c1
tradestreamhq <tradestream@googlegroups.com>
2025-03-10 00:39:20 -0400

    Update StrategyEnginePipeline.java

--- src/main/java/com/verlumen/tradestream/strategies/StrategyEnginePipeline.java
+++ src/main/java/com/verlumen/tradestream/strategies/StrategyEnginePipeline.java
@@ -1,9 +1,10 @@
-package com.verlumen.tradestream.pipeline.strategies;
+package com.verlumen.tradestream.strategies;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.flogger.FluentLogger;
 import com.google.inject.Inject;
 import com.verlumen.tradestream.marketdata.Candle;
+import com.verlumen.tradestream.signals.GenerateTradeSignals;
 import com.verlumen.tradestream.signals.TradeSignal;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.Filter;
