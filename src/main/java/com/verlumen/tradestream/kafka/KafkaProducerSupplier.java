cfe2a5a793d850978d8273d66a92e582ed329b51
tradestreamhq <tradestream@googlegroups.com>
2025-03-11 01:47:17 -0400

    feat(MINOR): Integrate Strategy Engine Pipeline and Ensure Serializable Kafka Components (#816)

--- src/main/java/com/verlumen/tradestream/kafka/KafkaProducerSupplier.java
+++ src/main/java/com/verlumen/tradestream/kafka/KafkaProducerSupplier.java
@@ -1,10 +1,11 @@
 package com.verlumen.tradestream.kafka;
 
 import com.google.inject.Inject;
+import java.io.Serializable;
 import java.util.function.Supplier;
 import org.apache.kafka.clients.producer.KafkaProducer;
 
-final class KafkaProducerSupplier implements Supplier<KafkaProducer<String, byte[]>> {
+final class KafkaProducerSupplier implements Serializable, Supplier<KafkaProducer<String, byte[]>> {
   private final KafkaProperties properties;
 
   @Inject
