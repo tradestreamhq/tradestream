4836e16c186e9a2f07cf70ae6841dd900ca508db
tradestreamhq <tradestream@googlegroups.com>
2025-03-11 01:56:27 -0400

    Create ParamConfigs.java

---
+++ src/main/java/com/verlumen/tradestream/backtesting/ParamConfigs.java
@@ -0,0 +1,14 @@
+package com.verlumen.tradestream.backtesting;
+
+import com.google.common.collect.ImmutableList;
+import com.verlumen.tradestream.backtesting.oscillators.OscillatorStrategies;
+
+public final class ParamConfigs {
+    public static final ImmutableList<ParamConfig> ALL_CONFIGS = 
+        ImmutableList.<StrategyFactory<?>>builder()
+            .addAll(OscillatorParams.ALL_CONFIGS)
+            .build();
+
+    // Prevent instantiation
+    private ParamConfigs() {}
+}
