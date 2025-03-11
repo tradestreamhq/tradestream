de51d47958de9df6d39ae63ba6098c49fafea459
tradestreamhq <tradestream@googlegroups.com>
2025-03-11 17:37:47 -0400

    Update BacktestingModule.java

--- src/main/java/com/verlumen/tradestream/backtesting/BacktestingModule.java
+++ src/main/java/com/verlumen/tradestream/backtesting/BacktestingModule.java
@@ -3,8 +3,6 @@ package com.verlumen.tradestream.backtesting;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
-import com.google.inject.TypeLiteral;
-
 
 public final class BacktestingModule extends AbstractModule {
   public static BacktestingModule create() {
@@ -18,8 +16,6 @@ public final class BacktestingModule extends AbstractModule {
     bind(GAEngineFactory.class).to(GAEngineFactoryImpl.class);
     bind(GeneticAlgorithmOrchestrator.class).to(GeneticAlgorithmOrchestratorImpl.class);
     bind(GenotypeConverter.class).to(GenotypeConverterImpl.class);
-    bind(new TypeLiteral<ImmutableList<ParamConfig>>() {})
-        .toInstance(ParamConfigs.ALL_FACTORIES);
     bind(ParamConfigManager.class).to(ParamConfigManagerImpl.class);
   }
 }
