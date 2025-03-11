e90d4da5ff17481ed0797947375a3036670a037c
tradestreamhq <tradestream@googlegroups.com>
2025-03-11 16:13:15 -0400

    Update ParamConfigs.java

--- src/main/java/com/verlumen/tradestream/backtesting/ParamConfigs.java
+++ src/main/java/com/verlumen/tradestream/backtesting/ParamConfigs.java
@@ -3,8 +3,8 @@ package com.verlumen.tradestream.backtesting;
 import com.google.common.collect.ImmutableList;
 import com.verlumen.tradestream.backtesting.oscillators.OscillatorParams;
 
-public final class ParamConfigs {
-    public static final ImmutableList<ParamConfig> ALL_CONFIGS = 
+final class ParamConfigs {
+    static final ImmutableList<ParamConfig> ALL_CONFIGS = 
         ImmutableList.<ParamConfig<?>>builder()
             .addAll(OscillatorParams.ALL_CONFIGS)
             .build();
