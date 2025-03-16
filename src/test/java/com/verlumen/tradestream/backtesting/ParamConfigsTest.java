70c0af7db146eb49e6243bbb5064a269db971ac8
tradestreamhq <tradestream@googlegroups.com>
2025-03-16 17:00:40 -0400

    Create ParamConfigsTest.java

---
+++ src/test/java/com/verlumen/tradestream/backtesting/ParamConfigsTest.java
@@ -0,0 +1,28 @@
+package com.verlumen.tradestream.backtesting;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assume.assumeTrue;
+
+import com.google.testing.junit.testparameterinjector.TestParameter;
+import com.google.testing.junit.testparameterinjector.TestParameterInjector;
+import java.util.Optional;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(TestParameterInjector.class)
+public class ParamConfigsTest {
+
+    @Test
+    public void testParamConfigNotNullForStrategyType(@TestParameter StrategyType strategyType) {
+        // Arrange: Skip this test if the strategy type is unspecified.
+        assumeTrue("Skipping test for unspecified strategy type", !StrategyType.UNSPECIFIED.equals(strategyType));
+
+        // Act: Find the ParamConfig corresponding to the provided strategy type.
+        ParamConfig configOptional = ParamConfigs.ALL_CONFIGS.stream()
+                .filter(pc -> strategyType.equals(pc.getStrategyType()))
+                .collect(onlyElement());
+
+        // Assert: Verify that the configuration is not null.
+        assertThat(configOptional).isNotNull();
+    }
+}
