721a437a31d859f9f05463ba5b2c3bf8521a83df
tradestreamhq <tradestream@googlegroups.com>
2025-03-17 23:50:17 -0400

    Update PipelineModule.kt

--- src/main/java/com/verlumen/tradestream/pipeline/PipelineModule.kt
+++ src/main/java/com/verlumen/tradestream/pipeline/PipelineModule.kt
@@ -1,7 +1,6 @@
-@file:JvmName("PipelineModuleUtility")
 package com.verlumen.tradestream.pipeline
 
-import com.google.protobuf.util.Timestamps.fromMillis
+import com.google.auto.value.AutoValue
 import com.google.inject.AbstractModule
 import com.google.inject.Provides
 import com.verlumen.tradestream.backtesting.BacktestingModule
@@ -14,56 +13,59 @@ import com.verlumen.tradestream.marketdata.Trade
 import com.verlumen.tradestream.signals.SignalsModule
 import com.verlumen.tradestream.strategies.StrategiesModule
 import com.verlumen.tradestream.ta4j.Ta4jModule
+import java.nio.charset.StandardCharsets
 import org.apache.kafka.common.serialization.ByteArrayDeserializer
 import org.apache.kafka.common.serialization.StringDeserializer
 
-// In Java, this would be equivalent to a package-private class with static factory method
-class PipelineModule private constructor(private val config: PipelineConfig) : AbstractModule() {
-    private companion object {
-        private val DRY_RUN_TRADE = Trade.newBuilder()
-            .setExchange("FakeExhange")
-            .setCurrencyPair("DRY/RUN")
-            .setTradeId("trade-123")
-            .setTimestamp(fromMillis(1234567))
-            .setPrice(50000.0)
-            .setVolume(0.1)
+@AutoValue
+abstract class PipelineModule : AbstractModule() {
+  private val DRY_RUN_TRADE = Trade.newBuilder()
+      .setExchange("FakeExhange")
+      .setCurrencyPair("DRY/RUN")
+      .setTradeId("trade-123")
+      .setTimestamp(fromMillis(1234567))
+      .setPrice(50000.0)
+      .setVolume(0.1)
+      .build()
+
+  companion object {
+    fun create(config: PipelineConfig): PipelineModule {
+      return AutoValue_PipelineModule(config)
+    }
+  }
+
+  abstract fun config(): PipelineConfig
+
+  override fun configure() {
+      install(BacktestingModule.create())
+      install(ExecutionModule.create(config().runMode()))
+      install(KafkaModule.create(config().bootstrapServers()))
+      install(SignalsModule.create(config().signalTopic()))
+      install(StrategiesModule.create())
+      install(Ta4jModule.create())
+  }
+
+  @Provides
+  fun provideKafkaReadTransform(factory: KafkaReadTransform.Factory, runMode: RunMode): KafkaReadTransform<String, ByteArray> {
+      if (runMode == RunMode.DRY) {
+        return DryRunKafkaReadTransform
+            .<String, ByteArray>builder()
+            .setBootstrapServers(config().bootstrapServers())
+            .setTopic(config().tradeTopic())
+            .setKeyDeserializerClass(StringDeserializer::class.java)
+            .setValueDeserializerClass(ByteArrayDeserializer::class.java)
+            .setDefaultValue(DRY_RUN_TRADE.toByteArray())
             .build()
-    }
+      }
 
-    override fun configure() {
-        install(BacktestingModule.create())
-        install(ExecutionModule.create(config.runMode()))
-        install(KafkaModule.create(config.bootstrapServers()))
-        install(SignalsModule.create(config.signalTopic()))
-        install(StrategiesModule.create())
-        install(Ta4jModule.create())
-    }
+      return factory.create(
+          config().tradeTopic(),
+          StringDeserializer::class.java,
+          ByteArrayDeserializer::class.java)
+  }
 
-    @Provides
-    fun provideKafkaReadTransform(factory: KafkaReadTransform.Factory, runMode: RunMode): KafkaReadTransform<String, ByteArray> =
-        if (runMode == RunMode.DRY) {
-            DryRunKafkaReadTransform.builder<String, ByteArray>()
-                .setBootstrapServers(config.bootstrapServers())
-                .setTopic(config.tradeTopic())
-                .setKeyDeserializerClass(StringDeserializer::class.java)
-                .setValueDeserializerClass(ByteArrayDeserializer::class.java)
-                .setDefaultValue(DRY_RUN_TRADE.toByteArray())
-                .build()
-        } else {
-            factory.create(
-                config.tradeTopic(),
-                StringDeserializer::class.java,
-                ByteArrayDeserializer::class.java
-            )
-        }
-
-    @Provides
-    fun providePipelineConfig(): PipelineConfig = config
+  @Provides
+  fun providePipelineConfig(): PipelineConfig {
+    return config()
+  }
 }
-
-// This is the Java-accessible static factory method
-@JvmName("create")
-fun createPipelineModule(config: PipelineConfig): PipelineModule = 
-    PipelineModule::class.java.getDeclaredConstructor(PipelineConfig::class.java).apply {
-        isAccessible = true 
-    }.newInstance(config)
