20ae6610eb31a22f06f3103c0855bcba75c4441e
tradestreamhq <tradestream@googlegroups.com>
2025-03-20 00:29:17 -0400

    Update ParamConfigs.java

--- src/main/java/com/verlumen/tradestream/backtesting/ParamConfigs.java
+++ src/main/java/com/verlumen/tradestream/backtesting/ParamConfigs.java
@@ -2,11 +2,13 @@ package com.verlumen.tradestream.backtesting;
 
 import com.google.common.collect.ImmutableList;
 import com.verlumen.tradestream.backtesting.momentumoscillators.MomentumOscillatorParams;
+import com.verlumen.tradestream.backtesting.trendfollowing.TrendFollowingParams;
 
 final class ParamConfigs {
     static final ImmutableList<ParamConfig> ALL_CONFIGS = 
         ImmutableList.<ParamConfig>builder()
             .addAll(MomentumOscillatorParams.ALL_CONFIGS)
+            .addAll(TrendFollowingParams.allConfigs)
             .build();
 
     // Prevent instantiation
