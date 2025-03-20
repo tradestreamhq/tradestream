00e8d2998d1acae6885a4d942c6077b8388c7186
tradestreamhq <tradestream@googlegroups.com>
2025-03-19 23:31:36 -0400

    Create TrendFollowingParams.kt

---
+++ src/main/java/com/verlumen/tradestream/backtesting/trendfollowing/TrendFollowingParams.kt
@@ -0,0 +1,7 @@
+package com.verlumen.tradestream.backtesting.trendfollowing
+
+import com.verlumen.tradestream.backtesting.ParamConfig
+
+object TrendFollowingParams {
+    public val ALL_CONFIGS: List<ParamConfig> = emptyList()
+}
