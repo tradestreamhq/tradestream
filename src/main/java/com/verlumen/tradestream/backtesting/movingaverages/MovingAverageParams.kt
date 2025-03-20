de9ecc1eb35e8cac549698695b6b026504bda37f
tradestreamhq <tradestream@googlegroups.com>
2025-03-20 00:14:00 -0400

    Update MovingAverageParams.kt

--- src/main/java/com/verlumen/tradestream/backtesting/movingaverages/MovingAverageParams.kt
+++ src/main/java/com/verlumen/tradestream/backtesting/movingaverages/MovingAverageParams.kt
@@ -3,6 +3,5 @@ package com.verlumen.tradestream.backtesting.movingaverages
 import com.verlumen.tradestream.backtesting.ParamConfig
 
 object MovingAverageParams {
-    @JvmField
-    val ALL_CONFIGS: List<ParamConfig> = emptyList()
+    val allConfigs: List<ParamConfig> = emptyList()
 }
