6e21226bc187733a5ce9baa8b5219be7b16dd0ef
tradestreamhq <tradestream@googlegroups.com>
2025-03-20 00:07:27 -0400

    Update PatternRecognitionParams.kt

--- src/main/java/com/verlumen/tradestream/backtesting/patternrecognition/PatternRecognitionParams.kt
+++ src/main/java/com/verlumen/tradestream/backtesting/patternrecognition/PatternRecognitionParams.kt
@@ -3,5 +3,6 @@ package com.verlumen.tradestream.backtesting.patternrecognition
 import com.verlumen.tradestream.backtesting.ParamConfig
 
 object PatternRecognitionParams {
-    val ALL_CONFIGS: List<ParamConfig> = emptyList()
+    @JvmField
+    val allConfigs: List<ParamConfig> = emptyList()
 }
