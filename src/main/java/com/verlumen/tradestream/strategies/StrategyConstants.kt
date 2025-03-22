72dfdb1af165c11c755041dac6995bc9f02181d1
tradestreamhq <tradestream@googlegroups.com>
2025-03-22 18:30:09 -0400

    Add `StrategyConstants` Kotlin library and test coverage for strategy-specific param configs (MINOR) (#866)

---
+++ src/main/java/com/verlumen/tradestream/strategies/StrategyConstants.kt
@@ -0,0 +1,8 @@
+package com.verlumen.tradestream.strategies;
+
+object StrategyConstants {
+    @JvmField
+    val supportedStrategyTypes: Set<StrategyType> = setOf(
+        StrategyType.ICHIMOKU_CLOUD
+    );
+}
