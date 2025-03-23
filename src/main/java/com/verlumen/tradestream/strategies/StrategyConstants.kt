60ed4948fe3bb9d019620bb08941fd6fb06950e4
tradestreamhq <tradestream@googlegroups.com>
2025-03-23 15:33:18 -0400

    Update StrategyConstants.kt

--- src/main/java/com/verlumen/tradestream/strategies/StrategyConstants.kt
+++ src/main/java/com/verlumen/tradestream/strategies/StrategyConstants.kt
@@ -3,6 +3,7 @@ package com.verlumen.tradestream.strategies;
 object StrategyConstants {
     @JvmField
     val supportedStrategyTypes: Set<StrategyType> = setOf(
-        StrategyType.SMA_RSI
+        StrategyType.SMA_RSI,
+        StrategyType.EMA_MACD
     );
 }
