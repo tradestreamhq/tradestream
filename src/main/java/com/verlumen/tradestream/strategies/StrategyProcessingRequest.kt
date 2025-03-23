545435e30e6c21b7c243efb0c78cc1c1b839f67a
tradestreamhq <tradestream@googlegroups.com>
2025-03-22 22:56:25 -0400

    Create StrategyProcessingRequest.kt

---
+++ src/main/java/com/verlumen/tradestream/strategies/StrategyProcessingRequest.kt
@@ -0,0 +1,8 @@
+package com.verlumen.tradestream.strategies;
+
+import com.verlumen.tradestream.marketdata.Candle;
+
+/**
+ * Helper class representing a request to process a single strategy type.
+ */
+data class StrategyProcessingRequest(val strategyType: StrategyType, val candles: List<Candle>)
