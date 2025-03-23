8329a491e785456d23278832b012fd9497d30c73
tradestreamhq <tradestream@googlegroups.com>
2025-03-23 00:34:07 -0400

    Update OptimizeStrategies.java

--- src/main/java/com/verlumen/tradestream/strategies/OptimizeStrategies.java
+++ src/main/java/com/verlumen/tradestream/strategies/OptimizeStrategies.java
@@ -1,13 +1,12 @@
 package com.verlumen.tradestream.strategies;
 
 import com.google.common.collect.ImmutableList;
+import com.google.inject.Inject;
 import com.verlumen.tradestream.marketdata.Candle;
 import org.apache.beam.sdk.transforms.PTransform;
 import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
-
-import javax.inject.Inject;
 
 /**
  * Composite PTransform that chains the splitting of input records per strategy type
