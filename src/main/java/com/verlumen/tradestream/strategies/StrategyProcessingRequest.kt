af7268a292919b3f327d95a0160c258480b16977
tradestreamhq <tradestream@googlegroups.com>
2025-03-23 00:24:14 -0400

    Revert "Add support for isolated strategy optimization via per-strategy split…" (#893)
    This reverts commit c5d99a5eb744b7dd47fa37f6c9102d3cb2882552.

--- src/main/java/com/verlumen/tradestream/strategies/StrategyProcessingRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.verlumen.tradestream.strategies;
-
-import com.verlumen.tradestream.marketdata.Candle;
-
-/**
- * Helper class representing a request to process a single strategy type.
- */
-data class StrategyProcessingRequest(val strategyType: StrategyType, val candles: List<Candle>)
