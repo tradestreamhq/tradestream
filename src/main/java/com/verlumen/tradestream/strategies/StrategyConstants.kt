e89cd5be62f5ff454a098d71665b3b66270c0173
tradestreamhq <tradestream@googlegroups.com>
2025-03-23 23:24:59 -0400

    Update StrategyConstants.kt (#898)

--- src/main/java/com/verlumen/tradestream/strategies/StrategyConstants.kt
+++ src/main/java/com/verlumen/tradestream/strategies/StrategyConstants.kt
@@ -4,7 +4,6 @@ object StrategyConstants {
     @JvmField
     val supportedStrategyTypes: Set<StrategyType> = setOf(
         StrategyType.SMA_RSI,
-        StrategyType.EMA_MACD,
-        StrategyType.ADX_STOCHASTIC
+        StrategyType.EMA_MACD
     );
 }
