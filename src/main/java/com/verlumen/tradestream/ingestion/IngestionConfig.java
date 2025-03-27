b78f522e85477cdd56e35f5bbff616413015fd80
tradestreamhq <tradestream@googlegroups.com>
2025-03-26 22:12:51 -0400

    Update IngestionConfig.java

--- src/main/java/com/verlumen/tradestream/ingestion/IngestionConfig.java
+++ src/main/java/com/verlumen/tradestream/ingestion/IngestionConfig.java
@@ -5,7 +5,6 @@ import com.verlumen.tradestream.execution.RunMode;
 record IngestionConfig(
     String coinMarketCapApiKey,
     int topCryptocurrencyCount,
-    String exchangeName,
     RunMode runMode,
     String kafkaBootstrapServers,
     String tradeTopic) {}
