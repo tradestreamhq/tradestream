48a0f9258cf6a1e0d9fe1702b6d8389d420e4e33
tradestreamhq <tradestream@googlegroups.com>
2025-03-26 22:28:05 -0400

    Update MarketDataConfig.java

--- src/main/java/com/verlumen/tradestream/marketdata/MarketDataConfig.java
+++ src/main/java/com/verlumen/tradestream/marketdata/MarketDataConfig.java
@@ -1,7 +1,7 @@
 package com.verlumen.tradestream.marketdata;
 
-public record MarketDataConfig(String exchangeName) {
-  public static MarketDataConfig create(String exchangeName) {
-    return new MarketDataConfig(exchangeName);
+public record MarketDataConfig(String exchangeName, String tradeTopic) {
+  public static MarketDataConfig create(String exchangeName, String tradeTopic) {
+    return new MarketDataConfig(exchangeName, tradeTopic);
   }
 }
