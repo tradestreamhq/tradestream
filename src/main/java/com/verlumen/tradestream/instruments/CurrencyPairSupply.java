07ed08c736b5d079904f398652444c73533808fc
tradestreamhq <tradestream@googlegroups.com>
2025-03-27 01:00:49 -0400

    Update CurrencyPairSupply.java

--- src/main/java/com/verlumen/tradestream/instruments/CurrencyPairSupply.java
+++ src/main/java/com/verlumen/tradestream/instruments/CurrencyPairSupply.java
@@ -2,5 +2,6 @@ package com.verlumen.tradestream.instruments;
 
 import com.google.common.collect.ImmutableList;
 import com.verlumen.tradestream.instruments.CurrencyPair;
+import java.util.function.Supplier;
 
 interface CurrencyPairSupply extends Supplier<ImmutableList<CurrencyPair>> {}
