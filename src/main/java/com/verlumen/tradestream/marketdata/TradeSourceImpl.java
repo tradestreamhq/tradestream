7a7e444eae200e307b5d64e6dd073782759aadc6
tradestreamhq <tradestream@googlegroups.com>
2025-03-29 19:51:20 -0400

    Update TradeSourceImpl.java

--- src/main/java/com/verlumen/tradestream/marketdata/TradeSourceImpl.java
+++ src/main/java/com/verlumen/tradestream/marketdata/TradeSourceImpl.java
@@ -1,30 +1,31 @@
 package com.verlumen.tradestream.kafka;
 
+import com.google.inject.Inject;
+import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.transforms.PTransform;
 import org.apache.beam.sdk.values.PBegin;
 import org.apache.beam.sdk.values.PCollection;
 
-/**
- * Now generic on K (key) and V (value).
- */
 final class TradeSourceImpl extends TradeSource {
   private final KafkaReadTransform<String, byte[]> kafkaReadTransform;
   private final ParseTrades parseTrades;
+  private final Pipeline pipeline;
 
   @Inject
   TradeSourceImpl(
+      Pipeline pipeline,
       KafkaReadTransform<String, byte[]> kafkaReadTransform,
       ParseTrades parseTrades) {
+    this.pipeline = pipeline;
     this.kafkaReadTransform = kafkaReadTransform;
     this.parseTrades = parseTrades;
   }
 
   @Override
-  public PCollection<Trade> expand() {
+  public PCollection<Trade> expand(PBegin input) {
     // 1. Read from Kafka.
-    PCollection<byte[]> input = pipeline.apply("ReadFromKafka", kafkaReadTransform);
-
+    PCollection<byte[]> kafkaData = input.apply("ReadFromKafka", kafkaReadTransform);
     // 2. Parse the byte stream into Trade objects.
-    return input.apply("ParseTrades", parseTrades);
+    return kafkaData.apply("ParseTrades", parseTrades);
   }
 }
