9abacace86b0282d23334a8ff36efc37d3d7fd7e
tradestreamhq <tradestream@googlegroups.com>
2025-03-29 14:12:34 -0400

    Update MarketDataModule.java

--- src/main/java/com/verlumen/tradestream/marketdata/MarketDataModule.java
+++ src/main/java/com/verlumen/tradestream/marketdata/MarketDataModule.java
@@ -4,17 +4,19 @@ import com.google.auto.value.AutoValue;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
+import com.verlumen.tradestream.execution.RunMode;
 
 public class MarketDataModule extends AbstractModule {
-  private static class BaseModule extends AbstractModule {
-    @Override
-    protected void configure() {
-      bind(ExchangeClientUnboundedSource.class).to(ExchangeClientUnboundedSourceImpl.class);
+  public static MarketDataModule create(RunMode runMode) {
+    switch(runMode) {
+      case DRY: return DryRunModule.create();
+      case WET: return ProdModule.create();
+      default: throw new IllegalStateException();
     }
   }
 
   @AutoValue
-  public abstract static class ProdModule extends AbstractModule {
+  public abstract static class ProdModule extends MarketDataModule {
     public static ProdModule create(String exchangeName) {
       return new AutoValue_MarketDataModule_ProdModule(exchangeName);
     }
@@ -23,9 +25,9 @@ public class MarketDataModule extends AbstractModule {
 
     @Override
     protected void configure() {
+      bind(ExchangeClientUnboundedSource.class).to(ExchangeClientUnboundedSourceImpl.class);
       bind(ExchangeStreamingClient.Factory.class).to(ExchangeStreamingClientFactory.class);
 
-      install(new BaseModule());
       install(new FactoryModuleBuilder()
               .implement(ExchangeClientUnboundedReader.class, ExchangeClientUnboundedReaderImpl.class)
               .build(ExchangeClientUnboundedReader.Factory.class));
@@ -38,7 +40,7 @@ public class MarketDataModule extends AbstractModule {
     }
   }
 
-  public static class DryRunModule extends AbstractModule {
+  public static class DryRunModule extends MarketDataModule {
     public static DryRunModule create() {
       return new DryRunModule();
     }
@@ -47,8 +49,6 @@ public class MarketDataModule extends AbstractModule {
     protected void configure() {
       bind(ExchangeClientUnboundedReader.class).to(DryRunExchangeClientUnboundedReader.class);
       bind(ExchangeClientUnboundedSource.class).to(DryRunExchangeClientUnboundedSource.class);
-
-      install(new BaseModule());
     }
   }
 }
