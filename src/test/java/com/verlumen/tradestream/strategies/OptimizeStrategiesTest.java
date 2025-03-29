2a38bb5fe9bbba6d645e685c1de487474d6bbf73
tradestreamhq <tradestream@googlegroups.com>
2025-03-28 23:34:41 -0400

    Update OptimizeStrategiesTest.java

--- src/test/java/com/verlumen/tradestream/strategies/OptimizeStrategiesTest.java
+++ src/test/java/com/verlumen/tradestream/strategies/OptimizeStrategiesTest.java
@@ -144,7 +144,7 @@ public class OptimizeStrategiesTest {
     GAOptimizationRequest actualRequest = mockOrchestrator.getLastRequest();
     assertThat(actualRequest.getCandlesList()).containsExactlyElementsIn(candles).inOrder();
     assertThat(actualRequest.getStrategyType())
-        .isEqualTo(StrategyType.EMA_MACD);
+        .isEqualTo(StrategyType.ADX_STOCHASTIC);
   }
 
   @Test
@@ -187,7 +187,7 @@ public class OptimizeStrategiesTest {
 
     assertThat(mockOrchestrator.wasRunOptimizationCalled()).isTrue();
     assertThat(mockOrchestrator.getLastRequest().getStrategyType())
-        .isEqualTo(StrategyType.EMA_MACD);
+        .isEqualTo(StrategyType.ADX_STOCHASTIC);
   }
 
   // Fake implementation of StrategyState.Factory.
