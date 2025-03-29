207b237a01b4af989f7cbb95f9170214dcf77a35
tradestreamhq <tradestream@googlegroups.com>
2025-03-29 12:47:37 -0400

    Update PipelineModule.java

--- src/main/java/com/verlumen/tradestream/pipeline/PipelineModule.java
+++ src/main/java/com/verlumen/tradestream/pipeline/PipelineModule.java
@@ -3,9 +3,13 @@ package com.verlumen.tradestream.pipeline;
 import com.google.auto.value.AutoValue;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
+import com.google.inject.Singleton;
 import com.verlumen.tradestream.backtesting.BacktestingModule;
 import com.verlumen.tradestream.execution.ExecutionModule;
 import com.verlumen.tradestream.execution.RunMode;
+import com.verlumen.tradestream.marketdata.DryRunExchangeClientUnboundedSource;
+import com.verlumen.tradestream.marketdata.ExchangeClientUnboundedSource;
+import com.verlumen.tradestream.marketdata.ExchangeClientUnboundedSourceImpl;
 import com.verlumen.tradestream.marketdata.MarketDataModule;
 import com.verlumen.tradestream.signals.SignalsModule;
 import com.verlumen.tradestream.strategies.StrategiesModule;
@@ -23,11 +27,25 @@ abstract class PipelineModule extends AbstractModule {
   protected void configure() {
       install(BacktestingModule.create());
       install(ExecutionModule.create(config().runMode()));
-      // Install MarketDataModule to provide ExchangeClientUnboundedSource
-      install(MarketDataModule.create("coinbase", config().tradeTopic())); // Provide exchangeName and a tradeTopic (might be unused by source)
+      // Install MarketDataModule - provides dependencies for ExchangeClientUnboundedSourceImpl
+      install(MarketDataModule.create("coinbase", config().tradeTopic()));
       install(SignalsModule.create(config().signalTopic()));
       install(StrategiesModule.create());
       install(Ta4jModule.create());
+  }
+
+  @Provides
+  @Singleton
+  ExchangeClientUnboundedSource provideExchangeClientUnboundedSource(
+      RunMode runMode,
+      Provider<DryRunExchangeClientUnboundedSource> fakeSourceProvider,
+      Provider<ExchangeClientUnboundedSourceImpl> realSourceProvider) {
+    if (runMode.equals(RunMode.DRY)) {
+      return fakeSourceProvider.get();
+    }
+
+    // Get an instance of the real source implementation via its provider
+    return realSourceProvider.get();
   }
 
   @Provides
