f9bb3b19500cc24b46f448ca2f4cc3e6556424b1
tradestreamhq <tradestream@googlegroups.com>
2025-03-28 23:49:57 -0400

    (MINOR) Add ADX Stochastic Strategy Support (#899)

--- src/main/java/com/verlumen/tradestream/strategies/StrategyConstants.kt
+++ src/main/java/com/verlumen/tradestream/strategies/StrategyConstants.kt
@@ -4,6 +4,7 @@ object StrategyConstants {
     @JvmField
     val supportedStrategyTypes: Set<StrategyType> = setOf(
         StrategyType.SMA_RSI,
-        StrategyType.EMA_MACD
+        StrategyType.EMA_MACD,
+        StrategyType.ADX_STOCHASTIC
     );
 }
