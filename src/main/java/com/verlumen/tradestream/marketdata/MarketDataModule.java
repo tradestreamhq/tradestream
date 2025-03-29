8ec53d1287bbeee20fcd60e9db0bf59f9d01d7b1
tradestreamhq <tradestream@googlegroups.com>
2025-03-29 13:36:48 -0400

    Update MarketDataModule.java

--- src/main/java/com/verlumen/tradestream/marketdata/MarketDataModule.java
+++ src/main/java/com/verlumen/tradestream/marketdata/MarketDataModule.java
@@ -5,36 +5,50 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
 
-@AutoValue
-public abstract class MarketDataModule extends AbstractModule {
-  public static MarketDataModule create(String exchangeName, String tradeTopic) {
-    return new AutoValue_MarketDataModule(MarketDataConfig.create(exchangeName, tradeTopic));
+public class MarketDataModule extends AbstractModule {
+  private static class BaseModule extends AbstractModule {
+    @Override
+    protected void configure() {
+      bind(ExchangeClientUnboundedSource.class).to(ExchangeClientUnboundedSourceImpl.class);
+    }
   }
 
-  abstract MarketDataConfig config();
+  @AutoValue
+  public abstract static class ProdModule extends AbstractModule {
+    public static ProdModule create(String exchangeName) {
+      return new AutoValue_MarketDataModule_ProdModule(exchangeName);
+    }
 
-  @Override
-  protected void configure() {
-    bind(ExchangeClientUnboundedSource.class).to(ExchangeClientUnboundedSourceImpl.class);
-    bind(ExchangeStreamingClient.Factory.class).to(ExchangeStreamingClientFactory.class);
+    abstract String exchangeName();
 
-    install(new FactoryModuleBuilder()
-            .implement(ExchangeClientUnboundedReader.class, ExchangeClientUnboundedReaderImpl.class)
-            .build(ExchangeClientUnboundedReader.Factory.class));
-    install(
-        new FactoryModuleBuilder()
-            .implement(TradePublisher.class, TradePublisherImpl.class)
-            .build(TradePublisher.Factory.class));
+    @Override
+    protected void configure() {
+      bind(ExchangeStreamingClient.Factory.class).to(ExchangeStreamingClientFactory.class);
+
+      install(new BaseModule());
+      install(new FactoryModuleBuilder()
+              .implement(ExchangeClientUnboundedReader.class, ExchangeClientUnboundedReaderImpl.class)
+              .build(ExchangeClientUnboundedReader.Factory.class));
+    }
+
+    @Provides
+    ExchangeStreamingClient provideExchangeStreamingClient(
+        ExchangeStreamingClient.Factory exchangeStreamingClientFactory) {
+      return exchangeStreamingClientFactory.create(exchangeName());
+    }
   }
 
-  @Provides
-  ExchangeStreamingClient provideExchangeStreamingClient(
-      ExchangeStreamingClient.Factory exchangeStreamingClientFactory) {
-    return exchangeStreamingClientFactory.create(config().exchangeName());
-  }
+  public static class DryRunModule extends AbstractModule {
+    public static DryRunModule create() {
+      return new DryRunModule();
+    }
 
-  @Provides
-  TradePublisher provideTradePublisher(TradePublisher.Factory tradePublisherFactory) {
-    return tradePublisherFactory.create(config().tradeTopic());
+    @Override
+    protected void configure() {
+      bind(ExchangeClientUnboundedReader.class).to(DryRunExchangeClientUnboundedReader.class);
+      bind(ExchangeClientUnboundedSource.class).to(DryRunExchangeClientUnboundedSource.class);
+
+      install(new BaseModule());
+    }
   }
 }
