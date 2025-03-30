bdc9aec466dcd671c7a6591e2dc9cef6b6cb545f
tradestreamhq <tradestream@googlegroups.com>
2025-03-29 20:10:52 -0400

    (MINOR) Add KafkaTradeSource to read and parse trade data from Kafka (#912)

---
+++ src/main/java/com/verlumen/tradestream/marketdata/KafkaTradeSource.java
@@ -0,0 +1,32 @@
+package com.verlumen.tradestream.marketdata;
+
+import com.google.inject.Inject;
+import com.verlumen.tradestream.kafka.KafkaReadTransform;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.transforms.PTransform;
+import org.apache.beam.sdk.values.PBegin;
+import org.apache.beam.sdk.values.PCollection;
+
+final class KafkaTradeSource extends TradeSource {
+  private final KafkaReadTransform<String, byte[]> kafkaReadTransform;
+  private final ParseTrades parseTrades;
+  private final Pipeline pipeline;
+
+  @Inject
+  KafkaTradeSource(
+      Pipeline pipeline,
+      KafkaReadTransform<String, byte[]> kafkaReadTransform,
+      ParseTrades parseTrades) {
+    this.pipeline = pipeline;
+    this.kafkaReadTransform = kafkaReadTransform;
+    this.parseTrades = parseTrades;
+  }
+
+  @Override
+  public PCollection<Trade> expand(PBegin input) {
+    // 1. Read from Kafka.
+    PCollection<byte[]> kafkaData = input.apply("ReadFromKafka", kafkaReadTransform);
+    // 2. Parse the byte stream into Trade objects.
+    return kafkaData.apply("ParseTrades", parseTrades);
+  }
+}
