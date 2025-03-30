fed8b1e8fb165e2543db9195086fdf62a371ec8b
tradestreamhq <tradestream@googlegroups.com>
2025-03-29 23:48:33 -0400

    Create TimingConfig.java

---
+++ src/main/java/com/verlumen/tradestream/pipeline/TimingConfig.java
@@ -0,0 +1,20 @@
+package com.verlumen.tradestream.pipeline;
+
+import com.verlumen.tradestream.execution.RunMode;
+import org.joda.time.Duration;
+
+record TimingConfig(
+    Duration allowedLateness,
+    Duration windowDuration,
+    Duration allowedTimestampSkew) {
+  private static final Duration FIVE_SECONDS = Duration.standardSeconds(5);
+  private static final Duration ONE_MINUTE = Duration.standardMinutes(1);
+  private static final Duration THIRTY_MINUTES = Duration.standardMinutes(30);
+
+  static PipelineConfig create() {
+    return new TimingConfig(
+        FIVE_SECONDS,
+        ONE_MINUTE,
+        THIRTY_MINUTES);
+  }
+}
