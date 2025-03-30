fe15efb28e8a65ea5ff43a26581910b443f24656
tradestreamhq <tradestream@googlegroups.com>
2025-03-30 17:00:47 -0400

    Update InstrumentsModule.java

--- src/main/java/com/verlumen/tradestream/instruments/InstrumentsModule.java
+++ src/main/java/com/verlumen/tradestream/instruments/InstrumentsModule.java
@@ -4,12 +4,14 @@ import com.google.auto.value.AutoValue;
 import com.google.common.base.Suppliers;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
+import java.util.concurrent.TimeUnit;
 
 @AutoValue
 public abstract class InstrumentsModule extends AbstractModule {
   public static InstrumentsModule create(String coinMarketCapApiKey, int topCryptocurrencyCount) {
     return new AutoValue_InstrumentsModule(coinMarketCapApiKey, topCryptocurrencyCount);
   }
+
   private static final long ONE = 1L;
 
   abstract String coinMarketCapApiKey();
@@ -28,6 +30,6 @@ public abstract class InstrumentsModule extends AbstractModule {
 
   @Provides
   Supplier<CurrencyPair> provideCurrencyPairSupply(CurrencyPairSupplyProvider provider) {
-    return Suppliers.memoizeWithExpiration(provider.get(), ONE, TimeUnit.Minutes);
+    return Suppliers.memoizeWithExpiration(provider.get(), ONE, TimeUnit.DAYS);
   }
 }
