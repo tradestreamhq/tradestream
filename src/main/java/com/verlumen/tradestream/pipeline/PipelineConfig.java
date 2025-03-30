5d61314aeb1dcf120488eea6770c857ea5b54c02
tradestreamhq <tradestream@googlegroups.com>
2025-03-30 00:17:56 -0400

    Update PipelineConfig.java

--- src/main/java/com/verlumen/tradestream/pipeline/PipelineConfig.java
+++ src/main/java/com/verlumen/tradestream/pipeline/PipelineConfig.java
@@ -1,17 +1,12 @@
 package com.verlumen.tradestream.pipeline;
 
 import com.verlumen.tradestream.execution.RunMode;
-import org.joda.time.Duration;
 
 record PipelineConfig(
     String bootstrapServers,
     String tradeTopic,
     String signalTopic,
     RunMode runMode) {
-  private static final Duration FIVE_SECONDS = Duration.standardSeconds(5);
-  private static final Duration ONE_MINUTE = Duration.standardMinutes(1);
-  private static final Duration THIRTY_MINUTES = Duration.standardMinutes(30);
-
   static PipelineConfig create(
       String bootstrapServers,
       String tradeTopic,
