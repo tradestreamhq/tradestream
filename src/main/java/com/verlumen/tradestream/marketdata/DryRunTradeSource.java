161df56defbfb16aae72641b3c6dd2fa0f48c27c
tradestreamhq <tradestream@googlegroups.com>
2025-03-29 21:00:52 -0400

    Update DryRunTradeSource.java

--- src/main/java/com/verlumen/tradestream/marketdata/DryRunTradeSource.java
+++ src/main/java/com/verlumen/tradestream/marketdata/DryRunTradeSource.java
@@ -1,8 +1,12 @@
 package com.verlumen.tradestream.marketdata;
 
+import org.apache.beam.sdk.transforms.Create;
+
 final class DryRunTradeSource extends TradeSource {
+  private final ImmutableList<Trade> trades;
+
   @Override
   public PCollection<Trade> expand(PBegin input) {
-    
+   return input.apply(Create.of(trades));
   }
 }
