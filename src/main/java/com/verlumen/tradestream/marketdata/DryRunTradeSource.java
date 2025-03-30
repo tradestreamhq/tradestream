14ef512ba990af55dfbbdc4c2c112e18396c2eb0
tradestreamhq <tradestream@googlegroups.com>
2025-03-29 21:36:01 -0400

    (MINOR) Add DryRunTradeSource for Simulated Trade Data in Market Data Pipeline (#916)

---
+++ src/main/java/com/verlumen/tradestream/marketdata/DryRunTradeSource.java
@@ -0,0 +1,22 @@
+package com.verlumen.tradestream.marketdata;
+
+import com.google.auto.value.AutoValue;
+import com.google.common.collect.ImmutableList;
+import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.transforms.PTransform;
+import org.apache.beam.sdk.values.PBegin;
+import org.apache.beam.sdk.values.PCollection;
+
+@AutoValue
+abstract class DryRunTradeSource extends TradeSource {
+  static DryRunTradeSource create(ImmutableList<Trade> trades) {
+    return new AutoValue_DryRunTradeSource(trades);
+  }
+
+  abstract ImmutableList<Trade> trades();
+  
+  @Override
+  public PCollection<Trade> expand(PBegin input) {
+   return input.apply(Create.of(trades()));
+  }
+}
