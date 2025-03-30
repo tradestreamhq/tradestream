1cd13d0e36e82c8305dfd9cc99f87c6f04fb5725
tradestreamhq <tradestream@googlegroups.com>
2025-03-29 20:33:00 -0400

    Update ExchangeClientTradeSource.java

--- src/main/java/com/verlumen/tradestream/marketdata/ExchangeClientTradeSource.java
+++ src/main/java/com/verlumen/tradestream/marketdata/ExchangeClientTradeSource.java
@@ -1,5 +1,7 @@
 package com.verlumen.tradestream.marketdata;
 
+import com.google.inject.Inject;
+import org.apache.beam.sdk.io.Read;
 import org.apache.beam.sdk.transforms.PTransform;
 import org.apache.beam.sdk.values.PBegin;
 import org.apache.beam.sdk.values.PCollection;
