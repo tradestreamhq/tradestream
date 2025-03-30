158b4c25a74022784d0e86a10d952e16dc5790c1
tradestreamhq <tradestream@googlegroups.com>
2025-03-29 23:53:36 -0400

    Update PipelineConfig.java

--- src/main/java/com/verlumen/tradestream/pipeline/PipelineConfig.java
+++ src/main/java/com/verlumen/tradestream/pipeline/PipelineConfig.java
@@ -7,10 +7,7 @@ record PipelineConfig(
     String bootstrapServers,
     String tradeTopic,
     String signalTopic,
-    RunMode runMode,
-    Duration allowedLateness,
-    Duration windowDuration,
-    Duration allowedTimestampSkew) {
+    RunMode runMode) {
   private static final Duration FIVE_SECONDS = Duration.standardSeconds(5);
   private static final Duration ONE_MINUTE = Duration.standardMinutes(1);
   private static final Duration THIRTY_MINUTES = Duration.standardMinutes(30);
@@ -24,9 +21,6 @@ record PipelineConfig(
         bootstrapServers,
         tradeTopic,
         signalTopic,
-        RunMode.fromString(runMode),
-        FIVE_SECONDS,
-        ONE_MINUTE,
-        THIRTY_MINUTES);
+        RunMode.fromString(runMode));
   }
 }
