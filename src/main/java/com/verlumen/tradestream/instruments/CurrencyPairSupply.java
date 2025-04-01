d6516f39ac4b5ef99e46d3f7e544289e10cf8fce
tradestreamhq <tradestream@googlegroups.com>
2025-03-31 23:05:12 -0400

    Delete src/main/java/com/verlumen/tradestream/instruments/CurrencyPairSupply.java

--- src/main/java/com/verlumen/tradestream/instruments/CurrencyPairSupply.java
+++
@@ -1,7 +0,0 @@
-package com.verlumen.tradestream.instruments;
-
-import com.google.common.collect.ImmutableList;
-import java.io.Serializable;
-import java.util.function.Supplier;
-
-interface CurrencyPairSupply extends Serializable, Supplier<ImmutableList<CurrencyPair>> {}
