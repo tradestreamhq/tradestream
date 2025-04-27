9747088269c694060a63820abb18b85ef1e9a369
tradestreamhq <tradestream@googlegroups.com>
2025-04-27 13:13:53 -0400

    Update CandleAccumulator.kt

--- src/main/java/com/verlumen/tradestream/marketdata/CandleAccumulator.kt
+++ src/main/java/com/verlumen/tradestream/marketdata/CandleAccumulator.kt
@@ -1,16 +1,20 @@
 package com.verlumen.tradestream.marketdata
 
+import com.verlumen.tradestream.protos.marketdata.Candle
+import com.google.protobuf.Timestamp
 import java.io.Serializable
+import org.joda.time.Instant
 
 /**
- * Class to hold the intermediate candle data for aggregation.
+ * Accumulator for creating OHLCV candles.
+ * Mutable state used within a Beam DoFn.
  */
 class CandleAccumulator : Serializable {
-    var currencyPair: String = ""
     var open: Double = 0.0
-    var high: Double = 0.0
-    var low: Double = 0.0
+    var high: Double = Double.MIN_VALUE
+    var low: Double = Double.MAX_VALUE
     var close: Double = 0.0
+    lateinit var currencyPair: String
     var volume: Double = 0.0
     var timestamp: Long = 0 // Used for candle timestamp in final output
     var initialized: Boolean = false
