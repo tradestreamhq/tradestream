f8e3b76c1e16da3121962d434c60cc9601a651fd
tradestreamhq <tradestream@googlegroups.com>
2025-04-27 11:41:14 -0400

    Update MarketDataModule.java

--- src/main/java/com/verlumen/tradestream/marketdata/MarketDataModule.java
+++ src/main/java/com/verlumen/tradestream/marketdata/MarketDataModule.java
@@ -1,19 +1,19 @@
 package com.verlumen.tradestream.marketdata;
 
-import static com.google.protobuf.util.Timestamps.fromMillis;
-
 import com.google.auto.value.AutoValue;
 import com.google.common.collect.ImmutableList;
 import com.google.inject.AbstractModule;
-import com.google.inject.Provider;
 import com.google.inject.Provides;
+import com.google.inject.Provider;
 import com.google.inject.Singleton;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
 import com.verlumen.tradestream.execution.RunMode;
 
+import static com.google.protobuf.util.Timestamps.fromMillis;
+
 @AutoValue
 public abstract class MarketDataModule extends AbstractModule {
-  public static MarketDataModule create(String exchangeName, RunMode runMode) {
+  static MarketDataModule create(String exchangeName, RunMode runMode) {
     return new AutoValue_MarketDataModule(exchangeName, runMode);
   }
 
@@ -24,15 +24,11 @@ public abstract class MarketDataModule extends AbstractModule {
   protected void configure() {
     bind(ExchangeClientUnboundedSource.class).to(ExchangeClientUnboundedSourceImpl.class);
     bind(ExchangeStreamingClient.Factory.class).to(ExchangeStreamingClientFactory.class);
-    // Bind CandleCreatorFn directly if no factory/assisted inject is needed
-    // bind(CandleCreatorFn.class); // Uncomment if needed, but @Inject constructor should suffice
+    
+    // Bind CandleCreatorFn directly
+    bind(CandleCreatorFn.class);
 
-    // Remove FactoryModuleBuilder for CandleCreatorFn as it no longer uses assisted inject
-    // install(new FactoryModuleBuilder()
-    //     .implement(CandleCreatorFn.class, CandleCreatorFn.class)
-    //     .build(CandleCreatorFn.Factory.class));
-
-    // Keep FactoryModuleBuilder for TradeToCandle
+    // Install FactoryModuleBuilder for TradeToCandle
     install(new FactoryModuleBuilder()
         .implement(TradeToCandle.class, TradeToCandle.class)
         .build(TradeToCandle.Factory.class));
