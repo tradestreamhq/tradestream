a9b78ad71740fb3eb5739acef62b471f3194254d
tradestreamhq <tradestream@googlegroups.com>
2025-04-27 23:15:49 -0400

    Update FillForwardCandles.kt

--- src/main/java/com/verlumen/tradestream/marketdata/FillForwardCandles.kt
+++ src/main/java/com/verlumen/tradestream/marketdata/FillForwardCandles.kt
@@ -1,35 +1,35 @@
-    package com.verlumen.tradestream.marketdata
+package com.verlumen.tradestream.marketdata
 
-    import com.google.inject.Inject
-    import com.google.inject.assistedinject.Assisted
-    import org.apache.beam.sdk.transforms.PTransform
-    import org.apache.beam.sdk.transforms.ParDo
-    import org.apache.beam.sdk.values.KV
-    import org.apache.beam.sdk.values.PCollection
-    import org.joda.time.Duration
-    import java.io.Serializable
+import com.google.inject.Inject
+import com.google.inject.assistedinject.Assisted
+import org.apache.beam.sdk.transforms.PTransform
+import org.apache.beam.sdk.transforms.ParDo
+import org.apache.beam.sdk.values.KV
+import org.apache.beam.sdk.values.PCollection
+import org.joda.time.Duration
+import java.io.Serializable
 
-    /**
-     * PTransform that applies the FillForwardCandlesFn to fill gaps in a candle stream.
-     */
-    class FillForwardCandles @Inject constructor(
-        @Assisted private val intervalDuration: Duration,
-        private val fillForwardFnFactory: FillForwardCandlesFn.Factory // Inject the factory
-    ) : PTransform<PCollection<KV<String, Candle>>, PCollection<KV<String, Candle>>>(), Serializable {
+/**
+ * PTransform that applies the FillForwardCandlesFn to fill gaps in a candle stream.
+ */
+class FillForwardCandles @Inject constructor(
+    @Assisted private val intervalDuration: Duration,
+    @Assisted private val maxForwardIntervals: Int = Int.MAX_VALUE,
+    private val fillForwardFnFactory: FillForwardCandlesFn.Factory // Inject the factory
+) : PTransform<PCollection<KV<String, Candle>>, PCollection<KV<String, Candle>>>(), Serializable {
 
-        companion object {
-            private const val serialVersionUID = 1L
-        }
-
-        override fun expand(input: PCollection<KV<String, Candle>>): PCollection<KV<String, Candle>> {
-            // Create the DoFn instance using the injected factory
-            val fillForwardFn = fillForwardFnFactory.create(intervalDuration)
-            return input.apply("FillForwardGaps", ParDo.of(fillForwardFn))
-        }
-
-        // Factory interface for Guice AssistedInject for this PTransform
-        interface Factory {
-            fun create(intervalDuration: Duration): FillForwardCandles
-        }
+    companion object {
+        private const val serialVersionUID = 1L
     }
-    
+
+    override fun expand(input: PCollection<KV<String, Candle>>): PCollection<KV<String, Candle>> {
+        // Create the DoFn instance using the injected factory
+        val fillForwardFn = fillForwardFnFactory.create(intervalDuration, maxForwardIntervals)
+        return input.apply("FillForwardGaps", ParDo.of(fillForwardFn))
+    }
+
+    // Factory interface for Guice AssistedInject for this PTransform
+    interface Factory {
+        fun create(intervalDuration: Duration, maxForwardIntervals: Int = Int.MAX_VALUE): FillForwardCandles
+    }
+}
