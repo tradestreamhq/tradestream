4169f639313dfe5577f8f95cac58ba352c42ab48
tradestreamhq <tradestream@googlegroups.com>
2025-04-27 21:42:58 -0400

    Update MarketDataModule.java

--- src/main/java/com/verlumen/tradestream/marketdata/MarketDataModule.java
+++ src/main/java/com/verlumen/tradestream/marketdata/MarketDataModule.java
@@ -5,9 +5,11 @@ import static com.google.protobuf.util.Timestamps.fromMillis;
 import com.google.auto.value.AutoValue;
 import com.google.common.collect.ImmutableList;
 import com.google.inject.AbstractModule;
+import com.google.inject.Key; // Import Key
 import com.google.inject.Provider;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
+import com.google.inject.assistedinject.FactoryModuleBuilder;
 import com.verlumen.tradestream.execution.RunMode;
 
 @AutoValue
@@ -16,15 +18,6 @@ public abstract class MarketDataModule extends AbstractModule {
     return new AutoValue_MarketDataModule(exchangeName, runMode);
   }
 
-  private static final Trade DRY_RUN_TRADE = Trade.newBuilder()
-      .setExchange("FakeExhange")
-      .setCurrencyPair("DRY/RUN")
-      .setTradeId("trade-123")
-      .setTimestamp(fromMillis(1234567))
-      .setPrice(50000.0)
-      .setVolume(0.1)
-      .build();
-
   abstract String exchangeName();
   abstract RunMode runMode();
 
@@ -32,21 +25,40 @@ public abstract class MarketDataModule extends AbstractModule {
   protected void configure() {
     bind(ExchangeClientUnboundedSource.class).to(ExchangeClientUnboundedSourceImpl.class);
     bind(ExchangeStreamingClient.Factory.class).to(ExchangeStreamingClientFactory.class);
+
+    // Install FactoryModuleBuilder for FillForwardCandlesFn (NEW)
+    install(new FactoryModuleBuilder()
+        .build(FillForwardCandlesFn.Factory.class));
+
+    // Install FactoryModuleBuilder for FillForwardCandles PTransform (NEW)
+    install(new FactoryModuleBuilder()
+        .implement(FillForwardCandles.class, FillForwardCandles.class)
+        .build(FillForwardCandles.Factory.class));
   }
 
   @Provides
+  @Singleton
   ExchangeStreamingClient provideExchangeStreamingClient(
-      ExchangeStreamingClient.Factory exchangeStreamingClientFactory) {
-    return exchangeStreamingClientFactory.create(exchangeName());
+      ExchangeStreamingClient.Factory factory) {
+    return factory.create(exchangeName());
   }
 
   @Provides
   @Singleton
   TradeSource provideTradeSource(Provider<ExchangeClientTradeSource> exchangeClientTradeSource) {
     switch (runMode()) {
-      case DRY: return DryRunTradeSource.create(ImmutableList.of(DRY_RUN_TRADE));
+      case DRY: return DryRunTradeSource.create(
+        ImmutableList.of(
+          Trade.newBuilder()
+          .setExchange(exchangeName())
+          .setCurrencyPair("DRY/RUN")
+          .setTradeId("trade-123")
+          .setTimestamp(fromMillis(1234567))
+          .setPrice(50000.0)
+          .setVolume(0.1)
+          .build()));
       case WET: return exchangeClientTradeSource.get();
-      default: throw new UnsupportedOperationException();
+      default: throw new UnsupportedOperationException("Unsupported RunMode: " + runMode());
     }
   }
 }
