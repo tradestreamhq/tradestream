a4d9620d0748dcf570c5e7af0867d0f1ec0175a7
tradestreamhq <tradestream@googlegroups.com>
2025-04-29 09:10:59 -0400

    Update CoinbaseStreamingClient.java

--- src/main/java/com/verlumen/tradestream/marketdata/CoinbaseStreamingClient.java
+++ src/main/java/com/verlumen/tradestream/marketdata/CoinbaseStreamingClient.java
@@ -23,6 +23,7 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ConcurrentHashMap;
