4dc6717ef6c3b5329bdcde2df4ce0aa8cb03b6a3
tradestreamhq <tradestream@googlegroups.com>
2025-04-30 19:36:38 -0400

    Update TimingConfig.java (#1007)

--- src/main/java/com/verlumen/tradestream/pipeline/TimingConfig.java
+++ src/main/java/com/verlumen/tradestream/pipeline/TimingConfig.java
@@ -4,16 +4,13 @@ import org.joda.time.Duration;
 
 record TimingConfig(
     Duration allowedLateness,
-    Duration windowDuration,
-    Duration allowedTimestampSkew) {
+    Duration windowDuration) {
   private static final Duration TWO_SECONDS = Duration.standardSeconds(2);
   private static final Duration ONE_MINUTE = Duration.standardMinutes(1);
-  private static final Duration TWO_MINUTES = Duration.standardMinutes(2);
 
   static TimingConfig create() {
     return new TimingConfig(
         TWO_SECONDS,
-        ONE_MINUTE,
-        TWO_MINUTES);
+        ONE_MINUTE);
   }
 }
