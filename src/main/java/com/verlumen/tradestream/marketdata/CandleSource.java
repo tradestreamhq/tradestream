54b648a4b3048a446e0211fdc0f07f7903619a5f
tradestreamhq <tradestream@googlegroups.com>
2025-05-10 11:03:01 -0400

    Create CandleSource.java

---
+++ src/main/java/com/verlumen/tradestream/marketdata/CandleSource.java
@@ -0,0 +1,8 @@
+package com.verlumen.tradestream.marketdata;
+
+import org.apache.beam.sdk.transforms.PTransform;
+import org.apache.beam.sdk.values.KV
+import org.apache.beam.sdk.values.PBegin;
+import org.apache.beam.sdk.values.PCollection;
+
+public abstract class CandleSource extends PTransform<PBegin,PCollection<KV<String, Candle>>> {}
