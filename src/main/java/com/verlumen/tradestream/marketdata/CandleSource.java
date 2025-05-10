a6dab476710117c5a5f19618f28f86fa427e7f3f
tradestreamhq <tradestream@googlegroups.com>
2025-05-10 12:13:12 -0400

    Update CandleSource.java

--- src/main/java/com/verlumen/tradestream/marketdata/CandleSource.java
+++ src/main/java/com/verlumen/tradestream/marketdata/CandleSource.java
@@ -1,7 +1,7 @@
 package com.verlumen.tradestream.marketdata;
 
 import org.apache.beam.sdk.transforms.PTransform;
-import org.apache.beam.sdk.values.KV
+import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PBegin;
 import org.apache.beam.sdk.values.PCollection;
 
