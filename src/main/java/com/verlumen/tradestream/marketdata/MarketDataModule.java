cc94a489f743ba8f74fc4a7c02feffcf4125c7bc
tradestreamhq <tradestream@googlegroups.com>
2025-05-10 20:21:21 -0400

    Update MarketDataModule.java

--- src/main/java/com/verlumen/tradestream/marketdata/MarketDataModule.java
+++ src/main/java/com/verlumen/tradestream/marketdata/MarketDataModule.java
@@ -14,14 +14,15 @@ import com.verlumen.tradestream.execution.RunMode;
 
 @AutoValue
 public abstract class MarketDataModule extends AbstractModule {
-  public static MarketDataModule create(String apiKey, String exchangeName, Duration granularity, RunMode runMode) {
-    return new AutoValue_MarketDataModule(apiKey, exchangeName, granularity, runMode);
+  public static MarketDataModule create(String exchangeName, Duration granularity, RunMode runMode, String tiingoApiKey) {
+    return new AutoValue_MarketDataModule(exchangeName, granularity, runMode, tiingoApiKey);
   }
 
-  abstract String apiKey();
   abstract String exchangeName();
   abstract Duration granularity();
   abstract RunMode runMode();
+  abstract String tiingoApiKey();
+
   @Override
   protected void configure() {
     bind(CandleSource.class).to(TradeBackedCandleSource.class);
