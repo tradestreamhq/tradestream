fadba194643d7773243fefb6d782e837e6c7f396
tradestreamhq <tradestream@googlegroups.com>
2025-05-23 23:22:05 -0400

    Update src/test/java/com/verlumen/tradestream/marketdata/CoinbaseStreamingClientTest.java
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/test/java/com/verlumen/tradestream/marketdata/CoinbaseStreamingClientTest.java
+++ src/test/java/com/verlumen/tradestream/marketdata/CoinbaseStreamingClientTest.java
@@ -5,6 +5,7 @@ import static com.google.common.truth.Truth.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
