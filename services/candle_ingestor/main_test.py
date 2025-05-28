75f80d78f9a1fb3cf32aa76a257b708dea20a476
tradestreamhq <tradestream@googlegroups.com>
2025-05-28 18:48:14 -0400

    Update services/candle_ingestor/main_test.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- services/candle_ingestor/main_test.py
+++ services/candle_ingestor/main_test.py
@@ -379,8 +379,8 @@ class MainFunctionTest(BaseIngestorTest):
         )
         self.mock_state_tracker.update_last_processed_timestamp.assert_any_call(
             candle_ingestor_main.SERVICE_IDENTIFIER,
-            f"{self.test_ticker}-backfill", 
-            backfill_candle_ts
+            f"{self.test_ticker}-backfill",
+            backfill_candle_ts,
         )
         self.mock_state_tracker.update_last_processed_timestamp.assert_any_call(
             candle_ingestor_main.SERVICE_IDENTIFIER,
