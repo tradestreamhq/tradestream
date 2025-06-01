506ae3e9f4cf5057d3b562a1f188bf54467b9ecf
tradestreamhq <tradestream@googlegroups.com>
2025-06-01 16:56:00 -0400

    Update GenotypeConverterImplTest.java

--- src/test/java/com/verlumen/tradestream/discovery/GenotypeConverterImplTest.java
+++ src/test/java/com/verlumen/tradestream/discovery/GenotypeConverterImplTest.java
@@ -1,4 +1,4 @@
-package com.verlumen.tradestream.backtesting;
+package com.verlumen.tradestream.discovery;
 
 import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.assertThrows;
