005a2378446c827614b6aa456f906bdf2dbefcdd
tradestreamhq <tradestream@googlegroups.com>
2025-06-01 22:35:01 -0400

    Create InfluxDbModule.java

---
+++ src/main/java/com/verlumen/tradestream/InfluxDbModule.java
@@ -0,0 +1,38 @@
+package com.verlumen.tradestream.marketdata;
+
+import com.google.auto.value.AutoValue;
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import com.influxdb.client.InfluxDBClient;
+import com.influxdb.client.InfluxDBClientFactory;
+
+@AutoValue
+public abstract class InfluxDbModule extends AbstractModule {
+  public static InfluxDbModule create(
+      String influxDbUrl,
+      String influxDbToken,
+      String influxDbOrg,
+      String influxDbBucket) {
+    return new AutoValue_InfluxDbModule(
+        influxDbUrl,
+        influxDbToken,
+        influxDbOrg,
+        influxDbBucket);
+  }
+
+  abstract String influxDbUrl();
+  abstract String influxDbToken();
+  abstract String influxDbOrg();
+  abstract String influxDbBucket();
+
+  @Provides
+  @Singleton
+  InfluxDBClient provideInfluxDBClient() {
+    return InfluxDBClientFactory.create(
+        influxDbUrl(), 
+        influxDbToken().toCharArray(), 
+        influxDbOrg(), 
+        influxDbBucket());
+  }
+}
