a8b8bcbf53404dd76fc02b134a1665749d5a1013
tradestreamhq <tradestream@googlegroups.com>
2025-06-02 00:11:49 -0400

    Update DiscoveryModule.kt

--- src/main/java/com/verlumen/tradestream/discovery/DiscoveryModule.kt
+++ src/main/java/com/verlumen/tradestream/discovery/DiscoveryModule.kt
@@ -1,12 +1,12 @@
 package com.verlumen.tradestream.discovery
 
 import com.google.inject.AbstractModule
-import com.google.inject.TypeLiteral;
+import com.google.inject.TypeLiteral
 
 class DiscoveryModule : AbstractModule() {
     override fun configure() {
         bind(GenotypeConverter::class.java).to(GenotypeConverterImpl::class.java)
         bind(ParamConfigManager::class.java).to(ParamConfigManagerImpl::class.java)
-        bind(new TypeLiteral<ImmutableList<ParamConfig>>() {}).toInstance(ParamConfigs.ALL_CONFIGS)
+        bind(object : TypeLiteral<ImmutableList<ParamConfig>>() {}).toInstance(ParamConfigs.ALL_CONFIGS)
     }
 }
