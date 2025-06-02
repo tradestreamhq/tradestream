eec7b6b46fef8bae756c1a213988dedd67d054eb
tradestreamhq <tradestream@googlegroups.com>
2025-06-01 22:53:53 -0400

    Update InfluxDbModule.java

--- src/main/java/com/verlumen/tradestream/influxdb/InfluxDbModule.java
+++ src/main/java/com/verlumen/tradestream/influxdb/InfluxDbModule.java
@@ -1,27 +1,26 @@
-package com.verlumen.tradestream.marketdata;
+package com.verlumen.tradestream.marketdata
 
-import com.google.auto.value.AutoValue;
-import com.google.inject.AbstractModule;
-import com.google.inject.Provides;
-import com.google.inject.Singleton;
-import com.influxdb.client.InfluxDBClient;
-import com.influxdb.client.InfluxDBClientFactory;
+import com.google.inject.AbstractModule
+import com.google.inject.Provides
+import com.google.inject.Singleton
+import com.influxdb.client.InfluxDBClient
+import com.influxdb.client.InfluxDBClientFactory
 
-@AutoValue
-public abstract class InfluxDbModule extends AbstractModule {
-  public static InfluxDbModule create(
-      String influxDbUrl, String influxDbToken, String influxDbOrg, String influxDbBucket) {
-    return new AutoValue_InfluxDbModule(influxDbUrl, influxDbToken, influxDbOrg, influxDbBucket);
-  }
+class InfluxDbModule(
+    private val influxDbUrl: String,
+    private val influxDbToken: String,
+    private val influxDbOrg: String,
+    private val influxDbBucket: String
+) : AbstractModule() {
 
-  abstract String influxDbUrl();
-
-  abstract String influxDbOrg();
-
-  @Provides
-  @Singleton
-  InfluxDBClient provideInfluxDBClient() {
-    return InfluxDBClientFactory.create(
-        influxDbUrl(), influxDbToken().toCharArray(), influxDbOrg(), influxDbBucket());
-  }
+    @Provides
+    @Singleton
+    fun provideInfluxDBClient(): InfluxDBClient {
+        return InfluxDBClientFactory.create(
+            influxDbUrl,
+            influxDbToken.toCharArray(),
+            influxDbOrg,
+            influxDbBucket
+        )
+    }
 }
