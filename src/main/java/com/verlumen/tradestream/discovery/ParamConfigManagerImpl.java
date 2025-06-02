ae6163c56a15db18d8149f381e742f6847ec549a
tradestreamhq <tradestream@googlegroups.com>
2025-06-01 23:11:30 -0400

    Update and rename ParamConfigManagerImpl.java to ParamConfigManagerImpl.java

--- src/main/java/com/verlumen/tradestream/discovery/ParamConfigManagerImpl.java
+++ src/main/java/com/verlumen/tradestream/discovery/ParamConfigManagerImpl.java
@@ -1,4 +1,4 @@
-package com.verlumen.tradestream.backtesting;
+package com.verlumen.tradestream.discovery;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static java.util.function.Function.identity;
