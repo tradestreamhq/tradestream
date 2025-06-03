89988963798ca8802344f6481b28c480fbf94dd3
tradestreamhq <tradestream@googlegroups.com>
2025-06-03 18:08:00 -0400

    Update and rename FitnessCalculationParams.kt to FitnessFunctionParams.kt

--- src/main/java/com/verlumen/tradestream/discovery/FitnessFunctionParams.kt
+++ src/main/java/com/verlumen/tradestream/discovery/FitnessFunctionParams.kt
@@ -4,12 +4,12 @@ import com.verlumen.tradestream.marketdata.Candle
 import com.verlumen.tradestream.strategies.StrategyType
 
 /**
- * Represents the parameters required for a fitness calculation.
+ * Represents the parameters required to produce a fitness function.
  *
  * @property strategyType The type of trading strategy to be evaluated.
  * @property candles A list of historical market data candles.
  */
-data class FitnessCalculationParams(
+data class FitnessFunctionParams(
     val strategyType: StrategyType,
     val candles: List<Candle>,
 )
