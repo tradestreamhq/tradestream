ff28fe465b663ea95b8fcbcc5edaa5ad0a64cd8d
tradestreamhq <tradestream@googlegroups.com>
2025-06-03 18:46:30 -0400

    Update FitnessFunction.kt

--- src/main/java/com/verlumen/tradestream/discovery/FitnessFunction.kt
+++ src/main/java/com/verlumen/tradestream/discovery/FitnessFunction.kt
@@ -1,6 +1,5 @@
 package com.verlumen.tradestream.discovery
 
 import io.jenetics.Genotype
-import java.util.Function
 
-typealias FitnessFunction = Function<Genotype<*>, Double>
+typealias FitnessFunction = (Genotype<*>) -> Double
