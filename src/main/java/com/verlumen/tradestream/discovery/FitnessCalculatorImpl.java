a78dfbd5efaa42fcc6a9a29fe9ee742f743f05b2
tradestreamhq <tradestream@googlegroups.com>
2025-06-02 22:53:27 -0400

    Update FitnessCalculatorImpl.java

--- src/main/java/com/verlumen/tradestream/discovery/FitnessCalculatorImpl.java
+++ src/main/java/com/verlumen/tradestream/discovery/FitnessCalculatorImpl.java
@@ -2,7 +2,7 @@ package com.verlumen.tradestream.discovery;
 
 import com.google.inject.Inject;
 import com.google.protobuf.Any;
-import com.verlumen.tradestream.discovery.GenotypeConverter;
+import com.verlumen.tradestream.backtesting.GAOptimizationRequest;
 import com.verlumen.tradestream.strategies.Strategy;
 import io.jenetics.Genotype;
 import java.util.function.Function;
