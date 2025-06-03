8842aaea5437b449d8389dc28fd39576363d0315
tradestreamhq <tradestream@googlegroups.com>
2025-06-03 23:29:33 +0000

    run ktlint

--- src/main/java/com/verlumen/tradestream/discovery/FitnessFunctionFactoryImpl.kt
+++ src/main/java/com/verlumen/tradestream/discovery/FitnessFunctionFactoryImpl.kt
@@ -17,39 +17,40 @@ import java.util.function.Function
  * algorithm individuals using backtesting.
  */
 class FitnessFunctionFactoryImpl
-@Inject
-constructor(
-    private val backtestRequestFactory: BacktestRequestFactory,
-    private val backtestRunner: BacktestRunner,
-    private val genotypeConverter: GenotypeConverter,
-) : FitnessFunctionFactory {
-
-    override fun create(strategyType: StrategyType, candles: List<Candle>): Function<Genotype<*>, Double> =
-        Function { genotype ->
-            try {
-                val params: Any = genotypeConverter.convertToParameters(genotype, strategyType)
-                val backtestRequest: BacktestRequest = createBacktestRequest(strategyType, candles, params)
-                val result: BacktestResult = backtestRunner.runBacktest(backtestRequest)
-                result.strategyScore // Assumes Kotlin synthetic property access
-            } catch (e: Exception) {
-                // Penalize any invalid genotype by assigning the lowest possible fitness
-                // Consider logging the exception e here if appropriate
-                Double.NEGATIVE_INFINITY
+    @Inject
+    constructor(
+        private val backtestRequestFactory: BacktestRequestFactory,
+        private val backtestRunner: BacktestRunner,
+        private val genotypeConverter: GenotypeConverter,
+    ) : FitnessFunctionFactory {
+        override fun create(
+            strategyType: StrategyType,
+            candles: List<Candle>,
+        ): Function<Genotype<*>, Double> =
+            Function { genotype ->
+                try {
+                    val params: Any = genotypeConverter.convertToParameters(genotype, strategyType)
+                    val backtestRequest: BacktestRequest = createBacktestRequest(strategyType, candles, params)
+                    val result: BacktestResult = backtestRunner.runBacktest(backtestRequest)
+                    result.strategyScore // Assumes Kotlin synthetic property access
+                } catch (e: Exception) {
+                    // Penalize any invalid genotype by assigning the lowest possible fitness
+                    // Consider logging the exception e here if appropriate
+                    Double.NEGATIVE_INFINITY
+                }
             }
-        }
 
-    private fun createBacktestRequest(
-        strategyType: StrategyType,
-        candles: List<Candle>,
-        params: Any
-    ): BacktestRequest {
-        return backtestRequestFactory.create(
-            candles,
-            Strategy
-                .newBuilder()
-                .setType(strategyType)
-                .setParameters(params)
-                .build(),
-        )
+        private fun createBacktestRequest(
+            strategyType: StrategyType,
+            candles: List<Candle>,
+            params: Any,
+        ): BacktestRequest =
+            backtestRequestFactory.create(
+                candles,
+                Strategy
+                    .newBuilder()
+                    .setType(strategyType)
+                    .setParameters(params)
+                    .build(),
+            )
     }
-}
