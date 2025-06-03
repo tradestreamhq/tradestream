301486de655d1d58b6f115740fb5b9e69bfbee4a
tradestreamhq <tradestream@googlegroups.com>
2025-06-03 16:40:44 -0400

    Update BacktestingModule.kt

--- src/main/java/com/verlumen/tradestream/backtesting/BacktestingModule.kt
+++ src/main/java/com/verlumen/tradestream/backtesting/BacktestingModule.kt
@@ -6,6 +6,5 @@ class BacktestingModule : AbstractModule() {
     override fun configure() {
         bind(BacktestRequestFactory::class.java).to(BacktestRequestFactoryImpl::class.java)
         bind(BacktestRunner::class.java).to(BacktestRunnerImpl::class.java)
-        bind(GAEngineFactory::class.java).to(GAEngineFactoryImpl::class.java)
     }
 }
