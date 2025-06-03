3dc0b242450088750444ead32d90bd24db0426fc
tradestreamhq <tradestream@googlegroups.com>
2025-06-03 19:08:28 -0400

    Revert "Update FitnessFunction.kt" (#1188)
    * Revert "Update FitnessFunction.kt (#1187)"
    
    This reverts commit 6b6fca670809b222172ef38dd3f79a3e6ed4e9b6.
    
    * Update FitnessFunction.kt

--- src/main/java/com/verlumen/tradestream/discovery/FitnessFunction.kt
+++ src/main/java/com/verlumen/tradestream/discovery/FitnessFunction.kt
@@ -2,4 +2,8 @@ package com.verlumen.tradestream.discovery
 
 import io.jenetics.Genotype
 
+/**
+ * Type alias for fitness functions used in genetic algorithms.
+ * Takes a Genotype and returns a Double fitness score.
+ */
 typealias FitnessFunction = (Genotype<*>) -> Double
