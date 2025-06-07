48cdf9b9fa7482d1b3e62f7971913af7b7e7d499
tradestreamhq <tradestream@googlegroups.com>
2025-06-07 18:49:11 -0400

    Update WriteDiscoveredStrategiesToPostgresFnTest.kt

--- src/test/java/com/verlumen/tradestream/discovery/WriteDiscoveredStrategiesToPostgresFnTest.kt
+++ src/test/java/com/verlumen/tradestream/discovery/WriteDiscoveredStrategiesToPostgresFnTest.kt
@@ -6,6 +6,7 @@ import com.google.inject.testing.fieldbinder.Bind
 import com.google.inject.testing.fieldbinder.BoundFieldModule
 import com.google.protobuf.Any
 import com.google.protobuf.Timestamp
+import com.verlumen.tradestream.sql.DataSourceConfig
 import com.verlumen.tradestream.sql.DataSourceFactory
 import com.verlumen.tradestream.strategies.SmaRsiParameters
 import com.verlumen.tradestream.strategies.Strategy
