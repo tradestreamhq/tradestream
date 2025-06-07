c720e795ca80706cb349090930a2877e326c9627
tradestreamhq <tradestream@googlegroups.com>
2025-06-07 18:26:45 -0400

    update

--- src/test/java/com/verlumen/tradestream/postgres/PostgreSQLDataSourceFactoryTest.kt
+++ src/test/java/com/verlumen/tradestream/postgres/PostgreSQLDataSourceFactoryTest.kt
@@ -1,5 +1,6 @@
-package com.verlumen.tradestream.discovery
+package com.verlumen.tradestream.postgres
 
+import com.verlumen.tradestream.sql.DataSourceFactory
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
