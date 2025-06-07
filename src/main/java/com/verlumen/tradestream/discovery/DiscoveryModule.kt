07b8292e4899ee51ca7245a43de4a78975e02f01
tradestreamhq <tradestream@googlegroups.com>
2025-06-07 13:19:53 -0400

    Update DiscoveryModule.kt

--- src/main/java/com/verlumen/tradestream/discovery/DiscoveryModule.kt
+++ src/main/java/com/verlumen/tradestream/discovery/DiscoveryModule.kt
@@ -4,11 +4,9 @@ import com.google.common.collect.ImmutableList
 import com.google.inject.AbstractModule
 import com.google.inject.TypeLiteral
 
-class DiscoveryModule(
-    private val options: StrategyDiscoveryPipelineOptions,
-) : AbstractModule() {
+class DiscoveryModule : AbstractModule() {
     override fun configure() {
-        bind(StrategyDiscoveryPipelineOptions::class.java).toInstance(options)
+        bind(StrategyDiscoveryPipelineFactory::class.java).to(StrategyDiscoveryPipelineFactoryImpl::class.java)
         bind(FitnessFunctionFactory::class.java).to(FitnessFunctionFactoryImpl::class.java)
         bind(GAEngineFactory::class.java).to(GAEngineFactoryImpl::class.java)
         bind(GenotypeConverter::class.java).to(GenotypeConverterImpl::class.java)
