a95b02edc5ccabb2879b6b551620077fae091601
Patrick Selamy <pselamy@gmail.com>
2025-06-09 20:26:26 -0400

    apply updates

--- src/main/java/com/verlumen/tradestream/discovery/DiscoveredStrategySink.kt
+++ src/main/java/com/verlumen/tradestream/discovery/DiscoveredStrategySink.kt
@@ -1,6 +1,5 @@
 package com.verlumen.tradestream.discovery
 
-import com.google.fluentlogging.FluentLogger
 import com.verlumen.tradestream.discovery.proto.DiscoveredStrategy
 import org.apache.beam.sdk.transforms.PTransform
 import org.apache.beam.sdk.values.PCollection
@@ -10,8 +9,6 @@ import org.apache.beam.sdk.values.PDone
  * A sink for discovered strategies. Implementations should handle persisting strategies to their
  * respective storage systems.
  */
-interface DiscoveredStrategySink : PTransform<PCollection<DiscoveredStrategy>, PDone> {
-    companion object {
-        private val logger = FluentLogger.forEnclosingClass()
-    }
+interface DiscoveredStrategySink {
+    fun expand(input: PCollection<DiscoveredStrategy>): PDone
 }
