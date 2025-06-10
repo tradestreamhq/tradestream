dbcc7dd10eece3d579a0785194fda791370d5ee2
tradestreamhq <tradestream@googlegroups.com>
2025-06-10 17:30:11 -0400

    Update PipelineModule.java

--- src/main/java/com/verlumen/tradestream/pipeline/PipelineModule.java
+++ src/main/java/com/verlumen/tradestream/pipeline/PipelineModule.java
@@ -1,8 +1,13 @@
 package com.verlumen.tradestream.pipeline;
 
+import static com.google.protobuf.util.Timestamps.fromMillis;
+
 import com.google.auto.value.AutoValue;
+import com.google.common.collect.ImmutableList;
 import com.google.inject.AbstractModule;
+import com.google.inject.Provider;
 import com.google.inject.Provides;
+import com.google.inject.Singleton;
 import com.verlumen.tradestream.backtesting.BacktestingModule;
 import com.verlumen.tradestream.discovery.DiscoveryModule;
 import com.verlumen.tradestream.execution.RunMode;
@@ -10,8 +15,14 @@ import com.verlumen.tradestream.http.HttpModule;
 import com.verlumen.tradestream.influxdb.InfluxDbModule;
 import com.verlumen.tradestream.instruments.InstrumentsModule;
 import com.verlumen.tradestream.kafka.KafkaModule;
+import com.verlumen.tradestream.marketdata.CandleSource;
+import com.verlumen.tradestream.marketdata.DryRunTradeSource;
 import com.verlumen.tradestream.marketdata.FillForwardCandles;
 import com.verlumen.tradestream.marketdata.MarketDataModule;
+import com.verlumen.tradestream.marketdata.TiingoCryptoCandleSource;
+import com.verlumen.tradestream.marketdata.Trade;
+import com.verlumen.tradestream.marketdata.TradeBackedCandleSource;
+import com.verlumen.tradestream.marketdata.TradeSource;
 import com.verlumen.tradestream.marketdata.TradeToCandle;
 import com.verlumen.tradestream.postgres.PostgresModule;
 import com.verlumen.tradestream.signals.SignalsModule;
@@ -85,11 +96,46 @@ abstract class PipelineModule extends AbstractModule {
     install(new InfluxDbModule(influxDbUrl(), influxDbToken(), influxDbOrg(), influxDbBucket()));
     install(InstrumentsModule.create(runMode(), coinMarketCapApiKey(), topCurrencyCount()));
     install(KafkaModule.create(bootstrapServers()));
-    install(MarketDataModule.create(exchangeName(), candleDuration(), runMode(), tiingoApiKey()));
+    install(MarketDataModule.create()); // No parameters needed anymore
     install(new PostgresModule());
     install(SignalsModule.create(signalTopic()));
     install(new StrategiesModule());
     install(Ta4jModule.create());
+  }
+
+  @Provides
+  @Singleton
+  CandleSource provideCandleSource(
+      Provider<TradeBackedCandleSource> tradeBackedCandleSource,
+      TiingoCryptoCandleSource.Factory tiingoCryptoCandleSourceFactory) {
+    switch (runMode()) {
+      case DRY:
+        return tradeBackedCandleSource.get();
+      case WET:
+        return tiingoCryptoCandleSourceFactory.create(candleDuration(), tiingoApiKey());
+      default:
+        throw new UnsupportedOperationException("Unsupported RunMode: " + runMode());
+    }
+  }
+
+  @Provides
+  @Singleton
+  TradeSource provideTradeSource() {
+    switch (runMode()) {
+      case DRY:
+        return DryRunTradeSource.create(
+            ImmutableList.of(
+                Trade.newBuilder()
+                    .setExchange(exchangeName())
+                    .setCurrencyPair("DRY/RUN")
+                    .setTradeId("trade-123")
+                    .setTimestamp(fromMillis(1259999L))
+                    .setPrice(50000.0)
+                    .setVolume(0.1)
+                    .build()));
+      default:
+        throw new UnsupportedOperationException("Unsupported RunMode: " + runMode());
+    }
   }
 
   @Provides
