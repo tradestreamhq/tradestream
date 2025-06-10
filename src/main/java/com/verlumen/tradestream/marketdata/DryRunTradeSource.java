918d7974991b7141fa64fcd81182835d87964206
Patrick Selamy <pselamy@gmail.com>
2025-06-09 23:19:22 -0400

    sync up

--- src/main/java/com/verlumen/tradestream/marketdata/DryRunTradeSource.java
+++ src/main/java/com/verlumen/tradestream/marketdata/DryRunTradeSource.java
@@ -2,7 +2,6 @@ package com.verlumen.tradestream.marketdata;
 
 import static com.google.protobuf.util.Timestamps.fromMillis;
 
-import com.google.auto.value.AutoValue;
 import com.google.inject.Inject;
 import com.google.common.collect.ImmutableList;
 import com.google.inject.assistedinject.Assisted;
@@ -10,28 +9,25 @@ import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.values.PBegin;
 import org.apache.beam.sdk.values.PCollection;
 
+final class DryRunTradeSource extends TradeSource {
+  private final ImmutableList<Trade> trades;
 
-@AutoValue
-abstract class DryRunTradeSource extends TradeSource {
   @Inject
   DryRunTradeSource(@Assisted String exchangeName) {
-    return new AutoValue_DryRunTradeSource(
-        ImmutableList.of(
-            Trade.newBuilder()
-                .setExchange(exchangeName)
-                .setCurrencyPair("DRY/RUN")
-                .setTradeId("trade-123")
-                .setTimestamp(fromMillis(1259999L))
-                .setPrice(50000.0)
-                .setVolume(0.1)
-                .build()));
+    this.trades = ImmutableList.of(
+        Trade.newBuilder()
+            .setExchange(exchangeName)
+            .setCurrencyPair("DRY/RUN")
+            .setTradeId("trade-123")
+            .setTimestamp(fromMillis(1259999L))
+            .setPrice(50000.0)
+            .setVolume(0.1)
+            .build());
   }
-
-  abstract ImmutableList<Trade> trades();
 
   @Override
   public PCollection<Trade> expand(PBegin input) {
-    return input.apply(Create.of(trades()));
+    return input.apply(Create.of(trades));
   }
 
   public interface Factory {
