2fb31ced059dba90fbee84930edc04d474f5e101
tradestreamhq <tradestream@googlegroups.com>
2025-06-14 09:27:57 -0400

    Update InfluxDbConfig.kt

--- src/main/java/com/verlumen/tradestream/influxdb/InfluxDbConfig.kt
+++ src/main/java/com/verlumen/tradestream/influxdb/InfluxDbConfig.kt
@@ -1,8 +1,8 @@
 package com.verlumen.tradestream.influxdb
 
 data class InfluxDbConfig(
-    private val url: String,
-    private val token: String,
-    private val org: String,
-    private val bucket: String,
+    val url: String,
+    val token: String,
+    val org: String,
+    val bucket: String,
 )
