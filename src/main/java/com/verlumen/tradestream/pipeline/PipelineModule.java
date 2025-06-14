76131b9c8d08b80a28ebc0b374ee2bf837195c67
tradestreamhq <tradestream@googlegroups.com>
2025-06-14 14:30:04 -0400

    Update PipelineModule.java

--- src/main/java/com/verlumen/tradestream/pipeline/PipelineModule.java
+++ src/main/java/com/verlumen/tradestream/pipeline/PipelineModule.java
@@ -94,7 +94,7 @@ abstract class PipelineModule extends AbstractModule {
     install(new DiscoveryModule());
     install(HttpModule.create());
     install(new InfluxDbModule(influxDbUrl(), influxDbToken(), influxDbOrg(), influxDbBucket()));
-    install(InstrumentsModule.create(runMode(), coinMarketCapApiKey(), topCurrencyCount()));
+    install(InstrumentsModule.create());
     install(KafkaModule.create(bootstrapServers()));
     install(MarketDataModule.create()); // No parameters needed anymore
     install(new PostgresModule());
