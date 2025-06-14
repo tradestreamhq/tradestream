6979481bdb5a2ebbf619a1eebebfcde06db8b91c
tradestreamhq <tradestream@googlegroups.com>
2025-06-14 09:22:13 -0400

    Update src/main/java/com/verlumen/tradestream/influxdb/InfluxDbModule.kt
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/com/verlumen/tradestream/influxdb/InfluxDbModule.kt
+++ src/main/java/com/verlumen/tradestream/influxdb/InfluxDbModule.kt
@@ -2,7 +2,7 @@ package com.verlumen.tradestream.influxdb
 
 import com.google.inject.AbstractModule
 
-class InfluxDbModule() : AbstractModule() {
+class InfluxDbModule : AbstractModule() {
     override fun configure() {
         bind(InfluxDbClientFactory::class.java).to(InfluxDbClientFactoryImpl::class.java)
     }
