0bdae366730750432b5523d3b6b8087bf60f7c12
tradestreamhq <tradestream@googlegroups.com>
2025-06-14 22:46:37 -0400

    Update StrategyDiscoveryPipeline.kt

--- src/main/java/com/verlumen/tradestream/discovery/StrategyDiscoveryPipeline.kt
+++ src/main/java/com/verlumen/tradestream/discovery/StrategyDiscoveryPipeline.kt
@@ -2,6 +2,8 @@ package com.verlumen.tradestream.discovery
 
 import com.google.common.flogger.FluentLogger
 import com.google.inject.Inject
+import com.verlumen.tradestream.influxdb.InfluxDbConfig
+import com.verlumen.tradestream.marketdata.CandleFetcher
 import com.verlumen.tradestream.sql.DataSourceConfig
 import org.apache.beam.sdk.Pipeline
 import org.apache.beam.sdk.transforms.ParDo
@@ -18,53 +20,54 @@ import org.apache.beam.sdk.transforms.ParDo
  * All transforms arrive through the factory pattern with Guice.
  */
 class StrategyDiscoveryPipeline
-    @Inject
-    constructor(
-        private val runGADiscoveryFnFactory: RunGADiscoveryFnFactory,
-        private val extractFn: ExtractDiscoveredStrategiesFn,
-        private val sinkFactory: DiscoveredStrategySinkFactory,
-        private val discoveryRequestSourceFactory: DiscoveryRequestSourceFactory,
-    ) {
-        fun run(options: StrategyDiscoveryPipelineOptions, candleFetcher: CandleFetcher) {
-            val username = requireNotNull(options.databaseUsername) { "Database username is required." }
-            val password = requireNotNull(options.databasePassword) { "Database password is required." }
+@Inject
+constructor(
+    private val runGADiscoveryFnFactory: RunGADiscoveryFnFactory,
+    private val extractFn: ExtractDiscoveredStrategiesFn,
+    private val sinkFactory: DiscoveredStrategySinkFactory,
+    private val discoveryRequestSourceFactory: DiscoveryRequestSourceFactory,
+) {
+    fun run(options: StrategyDiscoveryPipelineOptions, candleFetcher: CandleFetcher) {
+        val username = requireNotNull(options.databaseUsername) { "Database username is required." }
+        val password = requireNotNull(options.databasePassword) { "Database password is required." }
 
-            val dataSourceConfig =
-                DataSourceConfig(
-                    serverName = options.dbServerName,
-                    databaseName = options.dbDatabaseName,
-                    username = username,
-                    password = password,
-                    portNumber = options.dbPortNumber,
-                    applicationName = null,
-                    connectTimeout = null,
-                    socketTimeout = null,
-                    readOnly = null,
-                )
+        val dataSourceConfig =
+            DataSourceConfig(
+                serverName = options.dbServerName,
+                databaseName = options.dbDatabaseName,
+                username = username,
+                password = password,
+                portNumber = options.dbPortNumber,
+                applicationName = null,
+                connectTimeout = null,
+                socketTimeout = null,
+                readOnly = null,
+            )
 
-            val influxDbConfig =
-                InfluxDbConfig(
-                    url = options.influxDbUrl,
-                    token = requireNotNull(options.influxDbToken) { "InfluxDB token is required." },
-                    org = options.influxDbOrg,
-                    bucket = options.influxDbBucket,
-                )
-            val discoveryRequestSource = discoveryRequestSourceFactory.create(options)
-            val runGaFn = runGADiscoveryFnFactory.create(candleFetcher)
-            val sink = sinkFactory.create(dataSourceConfig)
+        val influxDbConfig =
+            InfluxDbConfig(
+                url = options.influxDbUrl,
+                token = requireNotNull(options.influxDbToken) { "InfluxDB token is required." },
+                org = options.influxDbOrg,
+                bucket = options.influxDbBucket,
+            )
+        
+        val discoveryRequestSource = discoveryRequestSourceFactory.create(options)
+        val runGaFn = runGADiscoveryFnFactory.create(candleFetcher)
+        val sink = sinkFactory.create(dataSourceConfig)
 
-            val pipeline = Pipeline.create(options)
+        val pipeline = Pipeline.create(options)
 
-            pipeline
-                .apply("ReadDiscoveryRequests", discoveryRequestSource)
-                .apply("RunGAStrategyDiscovery", ParDo.of(runGaFn))
-                .apply("ExtractStrategies", ParDo.of(extractFn))
-                .apply("WriteToSink", ParDo.of(sink))
+        pipeline
+            .apply("ReadDiscoveryRequests", discoveryRequestSource)
+            .apply("RunGAStrategyDiscovery", ParDo.of(runGaFn))
+            .apply("ExtractStrategies", ParDo.of(extractFn))
+            .apply("WriteToSink", ParDo.of(sink))
 
-            pipeline.run().waitUntilFinish()
-        }
-
-        companion object {
-            private val logger = FluentLogger.forEnclosingClass()
-        }
+        pipeline.run().waitUntilFinish()
     }
+
+    companion object {
+        private val logger = FluentLogger.forEnclosingClass()
+    }
+}
