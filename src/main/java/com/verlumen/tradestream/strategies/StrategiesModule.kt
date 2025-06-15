86cead3df8de5fa1ed203290c017db3a22e3ac6d
tradestreamhq <tradestream@googlegroups.com>
2025-06-15 17:38:58 -0400

    Update src/main/java/com/verlumen/tradestream/strategies/StrategiesModule.kt
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/com/verlumen/tradestream/strategies/StrategiesModule.kt
+++ src/main/java/com/verlumen/tradestream/strategies/StrategiesModule.kt
@@ -3,4 +3,4 @@ package com.verlumen.tradestream.strategies
 import com.google.common.collect.ImmutableList
 import com.google.inject.AbstractModule
 
-class StrategiesModule : AbstractModule() {}
+class StrategiesModule : AbstractModule()
