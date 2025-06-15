f5cef8676994631c8559ba3381962b395cc7a925
tradestreamhq <tradestream@googlegroups.com>
2025-06-14 21:37:29 -0400

    Update InfluxDbConfig.kt

--- src/main/java/com/verlumen/tradestream/influxdb/InfluxDbConfig.kt
+++ src/main/java/com/verlumen/tradestream/influxdb/InfluxDbConfig.kt
@@ -2,9 +2,9 @@ package com.verlumen.tradestream.influxdb
 
 import java.io.Serializable
 
-data class InfluxDbConfig:Serializable(
+data class InfluxDbConfig(
     val url: String,
     val token: String,
     val org: String,
-    val bucket: String,
-)
+    val bucket: String
+) : Serializable
