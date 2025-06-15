6a73cd6da6899bdd391ec57d0b8a288d9453b52d
tradestreamhq <tradestream@googlegroups.com>
2025-06-14 21:28:08 -0400

    Update InfluxDbClientFactory.kt

--- src/main/java/com/verlumen/tradestream/influxdb/InfluxDbClientFactory.kt
+++ src/main/java/com/verlumen/tradestream/influxdb/InfluxDbClientFactory.kt
@@ -2,12 +2,15 @@ package com.verlumen.tradestream.influxdb
 
 import com.influxdb.client.InfluxDBClient
 import com.influxdb.client.InfluxDBClientFactory
+import java.io.Serializable
 
 interface InfluxDbClientFactory {
     fun create(config: InfluxDbConfig): InfluxDBClient
 }
 
-class InfluxDbClientFactoryImpl : InfluxDbClientFactory {
+class InfluxDbClientFactoryImpl :
+    InfluxDbClientFactory,
+    Serializable {
     override fun create(config: InfluxDbConfig): InfluxDBClient =
         com.influxdb.client.InfluxDBClientFactory.create(
             config.url,
@@ -15,4 +18,8 @@ class InfluxDbClientFactoryImpl : InfluxDbClientFactory {
             config.org,
             config.bucket,
         )
+
+    companion object {
+        private const val serialVersionUID: Long = 1L
+    }
 }
