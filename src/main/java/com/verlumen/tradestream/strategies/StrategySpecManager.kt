b74897cc1925cedd016ec8989f5e927c99c1f199
tradestreamhq <tradestream@googlegroups.com>
2025-06-15 10:52:44 -0400

    Update StrategySpecManager.kt

--- src/main/java/com/verlumen/tradestream/strategies/StrategySpecManager.kt
+++ src/main/java/com/verlumen/tradestream/strategies/StrategySpecManager.kt
@@ -1,16 +1,19 @@
-package com.verlumen.tradestream.strategyspecs
+package com.verlumen.tradestream.strategies
 
-import com.google.inject.Inject
+import javax.inject.Inject
 
 /**
  * Manages and provides StrategySpec instances for different strategy types.
  */
-class StrategySpecManager
-    @Inject
-    constructor(
-        private val specs: Map<StrategyType, StrategySpec>,
-    ) {
-        fun getSpec(strategyType: StrategyType): StrategySpec =
-            specs[strategyType]
-                ?: throw IllegalArgumentException("No spec found for strategy type: $strategyType")
+class StrategySpecManager @Inject constructor(
+    private val specs: Map<StrategyType, StrategySpec>
+) {
+    fun getSpec(strategyType: StrategyType): StrategySpec {
+        return specs[strategyType]
+            ?: throw IllegalArgumentException("No spec found for strategy type: $strategyType")
     }
+
+    fun getSupportedTypes(): Set<StrategyType> {
+        return specs.keys
+    }
+}
