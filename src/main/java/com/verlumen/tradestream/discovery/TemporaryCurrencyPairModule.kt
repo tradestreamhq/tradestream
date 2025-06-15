44b62a28a6f7ba5d3528ab51e15e42f3bb00225e
tradestreamhq <tradestream@googlegroups.com>
2025-06-14 20:32:18 -0400

    Update src/main/java/com/verlumen/tradestream/discovery/TemporaryCurrencyPairModule.kt
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/com/verlumen/tradestream/discovery/TemporaryCurrencyPairModule.kt
+++ src/main/java/com/verlumen/tradestream/discovery/TemporaryCurrencyPairModule.kt
@@ -9,8 +9,10 @@ import java.util.function.Supplier
 // TODO: delete this module as soon as we remove all remaining dependencies
 class TemporaryCurrencyPairModule : AbstractModule() {
     override fun configure() {
-        bind(object : TypeLiteral<Supplier<java.util.List<CurrencyPair>>>() {}).toInstance(Supplier {
-            ImmutableList.of<CurrencyPair>() as java.util.List<CurrencyPair>
-        })
+        bind(object : TypeLiteral<Supplier<java.util.List<CurrencyPair>>>() {}).toInstance(
+            Supplier {
+                ImmutableList.of<CurrencyPair>() as java.util.List<CurrencyPair>
+            },
+        )
     }
 }
