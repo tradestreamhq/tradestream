c482bd610e4771e1479461b501d6b895bae00116
tradestreamhq <tradestream@googlegroups.com>
2025-06-15 22:37:00 -0400

    Delete src/main/java/com/verlumen/tradestream/discovery/ParamConfigManager.java

--- src/main/java/com/verlumen/tradestream/discovery/ParamConfigManager.java
+++
@@ -1,8 +0,0 @@
-package com.verlumen.tradestream.discovery;
-
-import com.verlumen.tradestream.strategies.StrategyType;
-import java.io.Serializable;
-
-public interface ParamConfigManager extends Serializable {
-  ParamConfig getParamConfig(StrategyType strategyType);
-}
