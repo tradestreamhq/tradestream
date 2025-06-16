9a95fea0f1bce0e6222b3e37c1a03fa35fdc0f3c
tradestreamhq <tradestream@googlegroups.com>
2025-06-16 18:58:44 -0400

    Update StrategiesModule.kt

--- src/main/java/com/verlumen/tradestream/strategies/StrategiesModule.kt
+++ src/main/java/com/verlumen/tradestream/strategies/StrategiesModule.kt
@@ -6,6 +6,8 @@ import com.google.inject.TypeLiteral
 
 class StrategiesModule : AbstractModule() {
     override fun configure() {
+        bind(object : TypeLiteral<ImmutableList<StrategyFactory<*>>>() {})
+            .toInstance(ImmutableList.of())
         bind(StrategyManager::class.java).to(StrategyManagerImpl::class.java)
     }
 }
