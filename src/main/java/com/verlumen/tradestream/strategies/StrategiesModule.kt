5c7b8aff1a79b514987c206ef18ca6c571729907
tradestreamhq <tradestream@googlegroups.com>
2025-06-16 20:39:04 -0400

    Delete src/main/java/com/verlumen/tradestream/strategies/StrategyFact… (#1244)

--- src/main/java/com/verlumen/tradestream/strategies/StrategiesModule.kt
+++ src/main/java/com/verlumen/tradestream/strategies/StrategiesModule.kt
@@ -2,12 +2,5 @@ package com.verlumen.tradestream.strategies
 
 import com.google.common.collect.ImmutableList
 import com.google.inject.AbstractModule
-import com.google.inject.TypeLiteral
 
-class StrategiesModule : AbstractModule() {
-    override fun configure() {
-        bind(object : TypeLiteral<ImmutableList<StrategyFactory<*>>>() {})
-            .toInstance(ImmutableList.of())
-        bind(StrategyManager::class.java).to(StrategyManagerImpl::class.java)
-    }
-}
+class StrategiesModule : AbstractModule()
