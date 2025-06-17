503769982996d9323ac28cea7f73329d722edfed
tradestreamhq <tradestream@googlegroups.com>
2025-06-16 20:25:39 -0400

    Replace `StrategyManager` with StrategyType extension methods for strategy creation (#1246)

--- src/main/java/com/verlumen/tradestream/backtesting/BacktestRunnerImpl.kt
+++ src/main/java/com/verlumen/tradestream/backtesting/BacktestRunnerImpl.kt
@@ -2,7 +2,8 @@ package com.verlumen.tradestream.backtesting
 
 import com.google.inject.Inject
 import com.google.protobuf.InvalidProtocolBufferException
-import com.verlumen.tradestream.strategies.StrategyManager
+import com.verlumen.tradestream.strategies.createStrategy
+import com.verlumen.tradestream.strategies.isSupported
 import com.verlumen.tradestream.ta4j.BarSeriesFactory
 import org.ta4j.core.AnalysisCriterion
 import org.ta4j.core.BarSeries
@@ -23,19 +24,21 @@ class BacktestRunnerImpl
     @Inject
     constructor(
         private val barSeriesFactory: BarSeriesFactory,
-        private val strategyManager: StrategyManager,
     ) : BacktestRunner {
         @Throws(InvalidProtocolBufferException::class)
         override fun runBacktest(request: BacktestRequest): BacktestResult {
             require(request.candlesList.isNotEmpty()) { "Bar series cannot be empty" }
+            require(request.strategy.type.isSupported()) { "Strategy type ${request.strategy.type} is not supported" }
+            require(
+                request.strategy.hasParameters() &&
+                    !request.strategy.parameters.typeUrl
+                        .isEmpty(),
+            ) {
+                "Strategy must have valid parameters. Use getDefaultParameters(strategyType) to get defaults for ${request.strategy.type}"
+            }
 
             val series = barSeriesFactory.createBarSeries(request.candlesList)
-            val strategy =
-                strategyManager.createStrategy(
-                    series,
-                    request.strategy.type,
-                    request.strategy.parameters,
-                )
+            val strategy = request.strategy.type.createStrategy(series, request.strategy.parameters)
 
             // Run the strategy
             val tradingRecord = runStrategy(series, strategy)
