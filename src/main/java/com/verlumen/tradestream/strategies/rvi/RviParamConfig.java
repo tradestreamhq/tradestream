518ae3028d8f5d5af908edb2819c0d96272b004e
tradestreamhq <tradestream@googlegroups.com>
2025-06-17 00:23:27 -0400

    Create RviParamConfig.java

---
+++ src/main/java/com/verlumen/tradestream/strategies/rvi/RviParamConfig.java
@@ -0,0 +1,51 @@
+package com.verlumen.tradestream.strategies.rvi;
+
+import com.google.common.collect.ImmutableList;
+import com.google.protobuf.Any;
+import com.verlumen.tradestream.discovery.ChromosomeSpec;
+import com.verlumen.tradestream.discovery.ParamConfig;
+import com.verlumen.tradestream.strategies.RviParameters;
+import com.verlumen.tradestream.strategies.StrategyType;
+import io.jenetics.IntegerChromosome;
+import io.jenetics.NumericChromosome;
+
+public final class RviParamConfig implements ParamConfig {
+  private static final ImmutableList<ChromosomeSpec<?>> SPECS =
+      ImmutableList.of(
+          ChromosomeSpec.ofInteger(5, 30)  // RVI Period
+      );
+
+  @Override
+  public ImmutableList<ChromosomeSpec<?>> getChromosomeSpecs() {
+    return SPECS;
+  }
+
+  @Override
+  public Any createParameters(ImmutableList<? extends NumericChromosome<?, ?>> chromosomes) {
+    if (chromosomes.size() != SPECS.size()) {
+      throw new IllegalArgumentException(
+          "Expected " + SPECS.size() + " chromosomes but got " + chromosomes.size());
+    }
+
+    IntegerChromosome periodChrom = (IntegerChromosome) chromosomes.get(0);
+
+    RviParameters parameters =
+        RviParameters.newBuilder()
+            .setPeriod(periodChrom.gene().allele())
+            .build();
+
+    return Any.pack(parameters);
+  }
+
+  @Override
+  public ImmutableList<? extends NumericChromosome<?, ?>> initialChromosomes() {
+    return SPECS.stream()
+        .map(ChromosomeSpec::createChromosome)
+        .collect(ImmutableList.toImmutableList());
+  }
+
+  @Override
+  public StrategyType getStrategyType() {
+    return StrategyType.RVI;
+  }
+}
