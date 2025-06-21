e3c885356879a1582d1ebc310422f5d1809bb7f1
tradestreamhq <tradestream@googlegroups.com>
2025-06-21 09:50:07 -0400

    Update VwapCrossoverStrategyFactory.java

--- src/main/java/com/verlumen/tradestream/strategies/vwapcrossover/VwapCrossoverStrategyFactory.java
+++ src/main/java/com/verlumen/tradestream/strategies/vwapcrossover/VwapCrossoverStrategyFactory.java
@@ -10,7 +10,7 @@ import org.ta4j.core.BaseStrategy;
 import org.ta4j.core.Rule;
 import org.ta4j.core.Strategy;
 import org.ta4j.core.indicators.SMAIndicator;
-import org.ta4j.core.indicators.VWAPIndicator;
+import org.ta4j.core.indicators.volume.VWAPIndicator;
 import org.ta4j.core.indicators.helpers.ClosePriceIndicator;
 import org.ta4j.core.rules.CrossedDownIndicatorRule;
 import org.ta4j.core.rules.CrossedUpIndicatorRule;
