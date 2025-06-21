10233da35632c879651b6e69934d8ebb8773a9ac
tradestreamhq <tradestream@googlegroups.com>
2025-06-21 17:38:32 -0400

    update

--- src/test/java/com/verlumen/tradestream/strategies/adxdmi/AdxDmiStrategyFactoryTest.java
+++ src/test/java/com/verlumen/tradestream/strategies/adxdmi/AdxDmiStrategyFactoryTest.java
@@ -16,22 +16,14 @@ public class AdxDmiStrategyFactoryTest {
   private AdxDmiStrategyFactory factory;
   private BarSeries series;
 
-  @Before
-  public void setUp() {
+@Before
+public void setUp() {
     factory = new AdxDmiStrategyFactory();
-    series = new BaseBarSeries();
+    series = new BaseBarSeries("test"); // Add series name
     for (int i = 0; i < 50; i++) {
-      series.addBar(
-          new BaseBar(
-              Duration.ofDays(1),
-              ZonedDateTime.now().plusDays(i),
-              100 + i,
-              100 + i,
-              100 + i,
-              100 + i,
-              100));
+        series.addBar(createBar(ZonedDateTime.now().plusDays(i), 100 + i));
     }
-  }
+}
 
   @Test
   public void testCreateStrategy() {
@@ -47,4 +39,18 @@ public class AdxDmiStrategyFactoryTest {
         AdxDmiParameters.newBuilder().setAdxPeriod(-1).setDiPeriod(14).build();
     factory.createStrategy(series, params);
   }
+  
+private BaseBar createBar(ZonedDateTime time, double price) {
+    return new BaseBar(
+        Duration.ofDays(1),
+        time.toInstant(),
+        DecimalNum.valueOf(price), // open
+        DecimalNum.valueOf(price), // high
+        DecimalNum.valueOf(price), // low
+        DecimalNum.valueOf(price), // close
+        DecimalNum.valueOf(100),   // volume
+        DecimalNum.valueOf(0),     // amount  
+        0L                         // trades
+    );
+}
 }
