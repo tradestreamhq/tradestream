2d1f5b37a2dc727011da51bfcaac81f71f481968
tradestreamhq <tradestream@googlegroups.com>
2025-06-21 15:15:19 -0400

    Update MomentumIndicator.kt

--- src/main/java/com/verlumen/tradestream/strategies/MomentumIndicator.kt
+++ src/main/java/com/verlumen/tradestream/strategies/MomentumIndicator.kt
@@ -9,17 +9,18 @@ class MomentumIndicator(
     private val closePrice: ClosePriceIndicator,
     private val period: Int,
 ) : CachedIndicator<Num>(closePrice) {
+
     override fun calculate(index: Int): Num {
         if (index < period) {
             // Not enough data yet
-            return numOf(0)
+            return series.numOf(0)
         }
         // Calculate percentage change:
         // ((Current Price - Price n periods ago) / Price n periods ago) * 100
         val currentClose = closePrice.getValue(index)
         val previousClose = closePrice.getValue(index - period)
-        return currentClose.minus(previousClose).dividedBy(previousClose).multipliedBy(numOf(100))
+        return currentClose.minus(previousClose).dividedBy(previousClose).multipliedBy(series.numOf(100))
     }
-
-    override fun getUnstableBars(): Int = period
+    
+    override fun getCountOfUnstableBars(): Int = period
 }
