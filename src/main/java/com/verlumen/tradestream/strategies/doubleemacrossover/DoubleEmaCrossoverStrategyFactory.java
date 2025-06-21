019c1b5ba7c1fb41741ee30ebfe3991d6600aa17
tradestreamhq <tradestream@googlegroups.com>
2025-06-20 23:10:58 -0400

    Update src/main/java/com/verlumen/tradestream/strategies/doubleemacrossover/DoubleEmaCrossoverStrategyFactory.java
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/com/verlumen/tradestream/strategies/doubleemacrossover/DoubleEmaCrossoverStrategyFactory.java
+++ src/main/java/com/verlumen/tradestream/strategies/doubleemacrossover/DoubleEmaCrossoverStrategyFactory.java
@@ -40,7 +40,9 @@ public final class DoubleEmaCrossoverStrategyFactory
     return new BaseStrategy(
         String.format(
             "%s (%d, %d)",
-            StrategyType.DOUBLE_EMA_CROSSOVER.name(), params.getShortEmaPeriod(), params.getLongEmaPeriod()),
+            StrategyType.DOUBLE_EMA_CROSSOVER.name(),
+            params.getShortEmaPeriod(),
+            params.getLongEmaPeriod()),
         entryRule,
         exitRule,
         params.getLongEmaPeriod());
