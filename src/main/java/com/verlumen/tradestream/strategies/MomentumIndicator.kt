3fb46698a69d175931a5fda10e35dadb9aec8f6f
tradestreamhq <tradestream@googlegroups.com>
2025-06-21 17:00:02 -0400

    Update MomentumIndicator.kt

--- src/main/java/com/verlumen/tradestream/strategies/MomentumIndicator.kt
+++ src/main/java/com/verlumen/tradestream/strategies/MomentumIndicator.kt
@@ -3,6 +3,7 @@ package com.verlumen.tradestream.strategies
 import org.ta4j.core.indicators.CachedIndicator
 import org.ta4j.core.indicators.helpers.ClosePriceIndicator
 import org.ta4j.core.num.Num
+import org.ta4j.core.num.PrecisionNum
 
 /** Momentum indicator that measures price changes over a specified period as a percentage. */
 class MomentumIndicator(
@@ -11,7 +12,7 @@ class MomentumIndicator(
 ) : CachedIndicator<Num>(closePrice) {
     
     override fun calculate(index: Int): Num = 
-        if (index < period) getBarSeries().numOf(0.0) // Not enough data yet
+        if (index < period) PrecisionNum.valueOf(0.0) // Not enough data yet
         else calculatePercentageChange(index)
     
     override fun getCountOfUnstableBars(): Int = period
@@ -20,6 +21,6 @@ class MomentumIndicator(
         // ((Current Price - Price n periods ago) / Price n periods ago) * 100
         val currentClose = closePrice.getValue(index)
         val previousClose = closePrice.getValue(index - period)
-        return currentClose.minus(previousClose).dividedBy(previousClose).multipliedBy(getBarSeries().numOf(100))
+        return currentClose.minus(previousClose).dividedBy(previousClose).multipliedBy(PrecisionNum.valueOf(100))
     }
 }
