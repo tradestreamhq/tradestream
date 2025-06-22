9ed7adceef84ce847c06c4163d5cb9d9cea9bd34
Patrick Selamy <pselamy@gmail.com>
2025-06-22 14:42:17 -0400

    update

--- src/test/java/com/verlumen/tradestream/strategies/StrategySpecsTest.kt
+++ src/test/java/com/verlumen/tradestream/strategies/StrategySpecsTest.kt
@@ -176,7 +176,7 @@ class StrategySpecsTest {
 
         // Assert
         assertThat(result).isNotNull()
-        assertThat(result).hasSize(25)
+        assertThat(result).hasSize(26)
     }
 
     @Test
