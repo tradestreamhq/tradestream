8d95d7e0b35b09a819ca83b2ed8171e5105d09e2
Trader Joe <tradestream@googlegroups.com>
2025-06-23 07:18:36 +0200

    add new stratgy

--- src/test/java/com/verlumen/tradestream/strategies/StrategySpecsTest.kt
+++ src/test/java/com/verlumen/tradestream/strategies/StrategySpecsTest.kt
@@ -176,7 +176,7 @@ class StrategySpecsTest {
 
         // Assert
         assertThat(result).isNotNull()
-        assertThat(result).hasSize(51)
+        assertThat(result).hasSize(52)
     }
 
     @Test
