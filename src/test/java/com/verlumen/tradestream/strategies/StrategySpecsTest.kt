5f3a9045fac6f4da58210913f96bd8fae18c5dbf
Trader Joe <tradestream@googlegroups.com>
2025-06-23 02:43:01 +0200

    remove it

--- src/test/java/com/verlumen/tradestream/strategies/StrategySpecsTest.kt
+++ src/test/java/com/verlumen/tradestream/strategies/StrategySpecsTest.kt
@@ -176,7 +176,7 @@ class StrategySpecsTest {
 
         // Assert
         assertThat(result).isNotNull()
-        assertThat(result).hasSize(38)
+        assertThat(result).hasSize(37)
     }
 
     @Test
