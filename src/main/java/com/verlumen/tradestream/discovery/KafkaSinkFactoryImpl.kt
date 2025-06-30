308b73b3db16a8cd8f6e4c95e84253635d1d782d
Trader Joe <tradestream@googlegroups.com>
2025-06-30 07:09:55 +0200

    refactor: simplify DiscoveredStrategySinkFactory using Guice modules
    - Delete monolithic DiscoveredStrategySinkFactoryImpl
    - Add focused KafkaSinkFactoryImpl and DryRunSinkFactoryImpl
    - Add DryRunDiscoveredStrategySinkFactory interface
    - Update Guice module bindings to use new factories
    - Move sink parameter creation to pipeline runner
    - Update BUILD file dependencies
    - All tests passing

---
+++ src/main/java/com/verlumen/tradestream/discovery/KafkaSinkFactoryImpl.kt
@@ -0,0 +1,12 @@
+package com.verlumen.tradestream.discovery
+
+import com.google.inject.Inject
+
+class KafkaSinkFactoryImpl @Inject constructor(
+    private val writeDiscoveredStrategiesToKafkaFactory: WriteDiscoveredStrategiesToKafkaFactory,
+) : DiscoveredStrategySinkFactory {
+    override fun create(params: DiscoveredStrategySinkParams): DiscoveredStrategySink {
+        val kafkaParams = params as DiscoveredStrategySinkParams.Kafka
+        return writeDiscoveredStrategiesToKafkaFactory.create(kafkaParams.bootstrapServers, kafkaParams.topic)
+    }
+} 
