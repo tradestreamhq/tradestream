ad1586254879eb0fc432cb04f765ecf4c9601174
tradestreamhq <tradestream@googlegroups.com>
2025-07-03 14:39:48 -0400

    Migrate MarketDataModule from Java to Kotlin (#1417)
    * Update BUILD
    
    * Delete src/main/java/com/verlumen/tradestream/marketdata/ExchangeClientUnboundedSourceImpl.kt
    
    * Delete src/main/java/com/verlumen/tradestream/marketdata/ExchangeClientUnboundedSource.kt
    
    * Delete src/main/java/com/verlumen/tradestream/marketdata/ExchangeStreamingClient.java
    
    * Delete src/main/java/com/verlumen/tradestream/marketdata/FillForwardCandlesFn.kt
    
    * Delete src/main/java/com/verlumen/tradestream/marketdata/FillForwardCandles.kt
    
    * Delete src/main/java/com/verlumen/tradestream/marketdata/TiingoCryptoFetcherFn.kt
    
    * Delete src/main/java/com/verlumen/tradestream/marketdata/TiingoResponseParser.kt
    
    * Delete src/main/java/com/verlumen/tradestream/marketdata/TiingoCryptoCandleTransform.kt
    
    * Delete src/main/java/com/verlumen/tradestream/marketdata/TradeCheckpointMark.kt
    
    * Update BUILD
    
    * Delete src/test/java/com/verlumen/tradestream/marketdata/LastCandlesFnTest.java
    
    * Update BUILD
    
    * Delete src/test/java/com/verlumen/tradestream/marketdata/TiingoResponseParserTest.kt
    
    * Delete src/test/java/com/verlumen/tradestream/marketdata/TiingoCryptoFetcherFnTest.kt
    
    * Delete src/test/java/com/verlumen/tradestream/marketdata/TiingoCryptoCandleTransformTest.kt
    
    * Update BUILD
    
    * Update MarketDataModule.java
    
    * Update MarketDataModule.java
    
    * Update BUILD
    
    * Rename MarketDataModule.java to MarketDataModule.kt
    
    * Update StrategyDiscoveryPipelineRunner.kt
    
    * Update src/main/java/com/verlumen/tradestream/marketdata/MarketDataModule.kt
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Create MarketDataModule.java
    
    * Delete src/main/java/com/verlumen/tradestream/marketdata/MarketDataModule.java
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/main/java/com/verlumen/tradestream/marketdata/MarketDataModule.kt
@@ -0,0 +1,14 @@
+package com.verlumen.tradestream.marketdata
+
+import com.google.inject.AbstractModule
+import com.google.inject.assistedinject.FactoryModuleBuilder
+
+class MarketDataModule : AbstractModule() {
+    override fun configure() {
+        install(
+            FactoryModuleBuilder()
+                .implement(InfluxDbCandleFetcher::class.java, InfluxDbCandleFetcher::class.java)
+                .build(InfluxDbCandleFetcher.Factory::class.java),
+        )
+    }
+}
